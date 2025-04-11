58e08fc86fcee37a5f5ced2c50d51afebbc0fcda
Kguo-cs <u3006612@connect.hku.hk>
2025-04-11 13:33:04 +0800

    rkl_detach_ratio0_reward01_alpha1

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -146,7 +146,7 @@ class IQ_SoftQ(LightningModule):
             alpha=1
             reward_loss= alpha*(torch.log(alpha/reward)-1)
         elif div == "rkl":
-            alpha=10
+            alpha=1
             reward_loss= (-reward/alpha-1).exp() * alpha
             reward_loss= reward_loss.detach()*reward
         else:
