4b410af341641e83222352c03d9c9f410aa68fee
Kguo-cs <u3006612@connect.hku.hk>
2025-04-11 11:49:37 +0800

    rkl_ratio0_buffer100_alpha1

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -144,7 +144,7 @@ class IQ_SoftQ(LightningModule):
             alpha=1
             reward_loss= alpha*(torch.log(alpha/reward)-1)
         elif div == "rkl":
-            alpha=2
+            alpha=1
             reward_loss= (-reward/alpha-1).exp() * alpha
         else:
             alpha = 0.01
