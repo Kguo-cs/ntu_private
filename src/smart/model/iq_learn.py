27ae66fdfe40a1f8e619c852c0ed206aa474bf77
Kguo-cs <u3006612@connect.hku.hk>
2025-04-11 14:53:07 +0800

    sh_ratio0_reward01_alpha01

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -149,7 +149,7 @@ class IQ_SoftQ(LightningModule):
             alpha=1
             reward_loss= (-reward/alpha-1).exp() * alpha
            # reward_loss= reward_loss.detach()*reward
-        elif div=="hellinger":
+        elif div=="sh":
             alpha=0.1
             reward_loss= -1/(1/reward+1/alpha)
         elif div =='js':
