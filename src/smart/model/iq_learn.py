34f6f9dd3367ac40b485a18a4cf868b58370b072
Kguo-cs <u3006612@connect.hku.hk>
2025-04-14 15:15:42 +0800

    kl1e-3_rewardw1

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -227,7 +227,7 @@ class IQ_SoftQ(LightningModule):
 
             self.log("train/reward_mean", reward_mean.item(), on_step=True, batch_size=1)
 
-            critic_loss=self.reward_w*(reward_loss+reward_mean+expert_constraint_loss+agent_constraint_loss)
+            critic_loss=self.reward_w*(reward_loss+reward_mean)#+expert_constraint_loss+agent_constraint_loss
 
             self.log("train/critic_loss", critic_loss.item(), on_step=True, batch_size=1)
 
