642a58bee029c6953a715bf46e4db15113770242
Kguo-cs <u3006612@connect.hku.hk>
2025-04-15 10:16:59 +0800

    OnlyKl_pretrain

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -34,7 +34,7 @@ class IQ_SoftQ(LightningModule):
         else:
             self.replay_buffer = deque(maxlen=100)
 
-        self.reward_w= 0
+        self.reward_w= 1
 
     def rollout(self, tokenized_map, tokenized_agent):
         pred = self.encoder.inference(
@@ -230,7 +230,7 @@ class IQ_SoftQ(LightningModule):
 
             self.log("train/critic_loss", critic_loss.item(), on_step=True, batch_size=1)
 
-            loss = expert_nll+ critic_loss
+            loss =  critic_loss#expert_nll+
 
         return loss
 
