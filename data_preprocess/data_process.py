f5b409f967700add8975e16b1fa472f5dc6477c1
Kguo-cs <u3006612@connect.hku.hk>
2025-04-16 20:57:41 +0800

    BC_64

--- data_preprocess/data_process.py
+++ data_preprocess/data_process.py
@@ -351,9 +351,10 @@ print(len(scenarios))
 #
 # with Pool(28) as pool:
 #     results = pool.starmap(process_scenario, zip(scenarios))
-with Pool(28) as pool:
-    results = list(tqdm(pool.imap_unordered(process_scenario, scenarios), total=len(scenarios)))
-
+# with Pool(28) as pool:
+#     results = list(tqdm(pool.imap_unordered(process_scenario, scenarios), total=len(scenarios)))
+for scenario in scenarios:
+    process_scenario(scenario)
 
 # # Submit tasks in parallel
 # futures = [process_scenario.remote(scenario) for scenario in scenarios]
