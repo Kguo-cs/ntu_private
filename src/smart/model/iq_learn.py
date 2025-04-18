e5a0e8ab96c693df860bfbc75d923719493d58c9
Kguo-cs <u3006612@connect.hku.hk>
2025-04-18 17:49:53 +0800

    rkl1_r1e3_len10

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -28,7 +28,7 @@ class IQ_SoftQ(LightningModule):
         if self.batch_replay:
             self.replay_buffer = deque(maxlen=4000)
         else:
-            self.replay_buffer = deque(maxlen=100)
+            self.replay_buffer = deque(maxlen=10)
 
         self.reward_w= 1e-3
         self.use_target_q=True
@@ -135,7 +135,7 @@ class IQ_SoftQ(LightningModule):
             reward_loss= -alpha*((reward/alpha).log()+1)
         elif div == "rkl":
             alpha=1
-            reward=torch.clamp_min(reward,min=alpha*(-1+np.log(1e-4)))
+            reward=torch.clamp_min(reward,min=alpha*(-1+np.log(1e-3)))
             reward_loss= alpha*(-reward/alpha-1).exp()
             # with torch.no_grad():
             #     phi_grad = torch.exp(-reward)
