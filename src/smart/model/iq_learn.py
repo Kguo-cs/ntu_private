c9aaf4cb0d0be91db46a2febdad83143defabd17
Kguo-cs <u3006612@connect.hku.hk>
2025-04-19 15:25:40 +0800

    kl1_r1_soft1e3

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -123,12 +123,12 @@ class IQ_SoftQ(LightningModule):
 
         constraint_loss = torch.relu(-reward).mean()
 
-        div = 'rkl'
+        div = 'kl'
         #TO DO: detach gradient, clip reward, gmm, refine by KL constrained
 
         if div=="kl":
             alpha=1#*(self.global_step/10000+1e-2)
-            reward=torch.clamp_min(reward,min=alpha*1e-3)
+            reward=torch.clamp(reward,max=alpha*1e3,min=alpha*1e-3)
             reward_loss= -alpha*((reward/alpha).log()+1)
         elif div == "rkl":
             alpha=1
