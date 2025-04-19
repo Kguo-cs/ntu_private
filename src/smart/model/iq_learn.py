15f071cb36887567af398525fed5353abdd46a42
Kguo-cs <u3006612@connect.hku.hk>
2025-04-19 10:00:59 +0800

    rkl1_r1e3_soft001

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -39,7 +39,7 @@ class IQ_SoftQ(LightningModule):
             self.target_net.load_state_dict(self.encoder.state_dict())
 
             if self.soft_update:
-                self.critic_tau = 0.001
+                self.critic_tau = 0.01
                 self.critic_target_update_frequency = 1
             else:
                 self.critic_target_update_frequency = 4
