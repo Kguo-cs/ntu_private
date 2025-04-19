81402ad4ce9ef2b32c8504041bcb3b64c8cc84d4
Kguo-cs <u3006612@connect.hku.hk>
2025-04-19 16:58:51 +0800

    only_js_r1_soft1e4

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -7,6 +7,7 @@ from collections import deque
 import torch.nn as nn
 import torch
 import numpy as np
+
 from src.smart.tokens.my_token_processor import TokenProcessor
 from src.smart.modules.smart_decoder import SMARTDecoder
 
@@ -39,7 +40,7 @@ class IQ_SoftQ(LightningModule):
             self.target_net.load_state_dict(self.encoder.state_dict())
 
             if self.soft_update:
-                self.critic_tau = 1e-5
+                self.critic_tau = 1e-4
                 self.critic_target_update_frequency = 1
             else:
                 self.critic_target_update_frequency = 4
@@ -248,7 +249,7 @@ class IQ_SoftQ(LightningModule):
 
             self.log("train/critic_loss", critic_loss.item(), on_step=True, batch_size=1)
 
-            loss = expert_nll+ critic_loss
+            loss =  critic_loss #expert_nll+
 
         return loss
 
