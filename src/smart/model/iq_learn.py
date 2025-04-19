44d331c676203c4c7b282a3870c008831ee731f0
Kguo-cs <u3006612@connect.hku.hk>
2025-04-19 16:00:43 +0800

    js_r1_soft1e4

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -123,7 +123,7 @@ class IQ_SoftQ(LightningModule):
 
         constraint_loss = torch.relu(-reward).mean()
 
-        div = 'kl'
+        div = 'js'
         #TO DO: detach gradient, clip reward, gmm, refine by KL constrained
 
         if div=="kl":
