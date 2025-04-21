f9043e7fb5c5ceedb7a47500db71baa5e1ffc8ce
Kguo-cs <u3006612@connect.hku.hk>
2025-04-21 09:45:48 +0800

    LSIF05_soft1e4

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -251,12 +251,12 @@ class IQ_SoftQ(LightningModule):
 
             # critic_loss=self.reward_w*(reward_loss+reward_mean)#self.global_step/10000*+expert_constraint_loss+agent_constraint_loss
 
-            #alpha=2
+            alpha=0.5
 
-            #critic_loss=(-expert_reward/alpha).exp().mean()+1/2*(2*agent_reward/alpha).exp().mean()
+            critic_loss=(-expert_reward/alpha).exp().mean()+1/2*(2*agent_reward/alpha).exp().mean()
 
             # critic_loss=-expert_reward.mean()+agent_reward.exp().mean()
-            critic_loss=-expert_reward.mean()+1/2*agent_reward.square().mean()
+            # critic_loss=-expert_reward.mean()+1/2*agent_reward.square().mean()
 
 
             self.log("train/critic_loss", critic_loss.item(), on_step=True, batch_size=1)
