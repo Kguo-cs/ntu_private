060531bd3da22191152b17b3705c9d287b0a5626
Kguo-cs <u3006612@connect.hku.hk>
2025-04-22 10:05:03 +0800

    BCE1_soft1e4_01BC_rollout1_maxlen1

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -42,7 +42,7 @@ class IQ_SoftQ(LightningModule):
             self.target_net.load_state_dict(self.encoder.state_dict())
 
             if self.soft_update:
-                self.critic_tau = 1e-5
+                self.critic_tau = 1e-4
                 self.critic_target_update_frequency = 1
             else:
                 self.critic_target_update_frequency = 4
@@ -265,7 +265,7 @@ class IQ_SoftQ(LightningModule):
 
             self.log("train/critic_loss", critic_loss.item(), on_step=True, batch_size=1)
 
-            loss =  expert_nll+critic_loss # #*0.1
+            loss =  expert_nll*0.1+critic_loss # #
 
         return loss
 
