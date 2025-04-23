06d9f1c826fe4efaea93c7e93c1e98d1e3a98979
Kguo-cs <u3006612@connect.hku.hk>
2025-04-23 17:21:25 +0800

    onlyBCE1_1e3_drop0_rollout1_maxlen1

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -260,7 +260,7 @@ class IQ_SoftQ(LightningModule):
 
             #critic_loss=-(expert_reward/alpha).exp().mean()+1/2*(2*agent_reward/alpha).exp().mean()
             alpha=1
-            critic_loss=((-expert_reward).exp()+1).log().mean()+0.05*((agent_reward/alpha).exp()+1).log().mean() #((-expert_reward/alpha).exp()+1).log().mean()
+            critic_loss=((-expert_reward).exp()+1).log().mean()+((agent_reward/alpha).exp()+1).log().mean() #((-expert_reward/alpha).exp()+1).log().mean()
             # alpha=0.1
             #
             # critic_loss= (-expert_reward+expert_reward.square()/(4*alpha)).mean()+ (agent_reward+agent_reward.square()/(4*alpha)).mean()
