af3a3690eb49a753e6d2608ad43749ccc865237e
Kguo-cs <u3006612@connect.hku.hk>
2025-04-24 18:57:20 +0800

    TV_notarget1e4_10SV_movingV1e3

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -359,7 +359,7 @@ class IQ_SoftQ(LightningModule):
         if self.reward_w!=0 and self.use_target_q and self.global_step % self.critic_target_update_frequency == 0  :
 
             if self.soft_update:
-                tau=1e-4 #self.critic_tau/(self.global_step+1)
+                tau=1e-3 #self.critic_tau/(self.global_step+1)
                 soft_update(self.encoder,self.target_net,tau)
             else:
                 hard_update(self.encoder,self.target_net)
