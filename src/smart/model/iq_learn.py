b679fd8c9ff76481982c4690176691c907aff6bb
Kguo-cs <u3006612@connect.hku.hk>
2025-04-25 10:38:19 +0800

    BCE_soft_SnextV1_nodetach

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -31,7 +31,7 @@ class IQ_SoftQ(LightningModule):
 
         self.reward_w = 1
         self.use_target_q=True
-        self.soft_update=False
+        self.soft_update=True
 
         self.rollout_freq=1
         self.target_net = SMARTDecoder(
@@ -288,7 +288,7 @@ class IQ_SoftQ(LightningModule):
 
             #constraint_loss=2*(torch.clamp_min(expert_V,min=0).square().mean()+torch.clamp_max(agent_V,max=0).square().mean() )
             #constraint_loss=0.5*((expert_V/2).exp().mean()+(-agent_V/2).exp().mean() )#expert_next_V.mean()(torch.clamp_min(expert_V,min=0).exp().mean()+torch.clamp_min(-agent_V,min=0).exp().mean() )
-            constraint_loss=100*(expert_current_next_V_diff.square().mean()+agent_current_next_V_diff.square().mean() )
+            constraint_loss=1*(expert_current_next_V_diff.square().mean()+agent_current_next_V_diff.square().mean() )
 
             #constraint_loss=10*((expert_V-expert_current_target_V).square().mean()+(agent_V-agent_current_target_V).square().mean() )
             self.log("train/constraint_loss", constraint_loss.item(), on_step=True, batch_size=1)
