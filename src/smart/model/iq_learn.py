b4220f3e949863bf3015a2430b1edf9d9b990cff
Kguo-cs <u3006612@connect.hku.hk>
2025-04-25 18:20:43 +0800

    kl1_soft2e4_SV2_nodetach

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -268,7 +268,7 @@ class IQ_SoftQ(LightningModule):
 
             #critic_loss=self.reward_w*(reward_loss+reward_mean)#self.global_step/10000*+expert_constraint_loss+agent_constraint_loss
 
-            div='js'
+            div='kl'
             alpha=1
             eps=1e-3
 
@@ -279,13 +279,15 @@ class IQ_SoftQ(LightningModule):
             elif div=='ukl':
                 critic_loss = -expert_reward.mean() + agent_reward.exp().mean()
             elif div=='rkl':
-                critic_loss= (-expert_reward  - 1).exp().mean()+agent_reward.mean()
+                critic_loss= alpha *(-expert_reward / alpha  - 1).exp().mean()+agent_reward.mean()
             elif div=='tv':
                 critic_loss= (-expert_reward ).mean()+agent_reward.mean()
             elif div=='x2':
-                critic_loss= (-expert_reward +expert_reward.square()/4).mean()+agent_reward.mean()
+                critic_loss= (-expert_reward +expert_reward.square()/ (4 * alpha)).mean()+agent_reward.mean()
+            elif div=='kl':
+                expert_reward = torch.clamp_min(expert_reward, min=alpha * eps)
+                critic_loss = -alpha * ((expert_reward / alpha).log() + 1)+agent_reward.mean()
             elif div=='js':
-                alpha = 1
                 expert_reward = torch.clamp_min(expert_reward, min=alpha * (np.log(1 / 2 + eps)))  # ,max=alpha*(np.log(1/2+1/eps))
                 critic_loss = -(2 - (-expert_reward / alpha).exp()).log().mean()+agent_reward.mean()
             else:
