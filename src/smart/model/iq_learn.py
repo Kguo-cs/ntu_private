e46a7f8b6b1260bf681f1453668cf75d6fbbf1f7
Kguo-cs <u3006612@connect.hku.hk>
2025-04-26 17:16:57 +0800

    RKLe1_soft1e4_SV100

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -17,7 +17,7 @@ class IQ_SoftQ(LightningModule):
     def __init__(self, model_config) -> None:
         super(IQ_SoftQ, self).__init__(model_config)
 
-        self.gamma = 0.8
+        self.gamma = 0.99
         self.alpha = self.encoder.agent_encoder.alpha
 
         self.logsoftmax = nn.LogSoftmax(dim=-1)
@@ -297,7 +297,7 @@ class IQ_SoftQ(LightningModule):
 
             #constraint_loss=2*(torch.clamp_min(expert_V,min=0).square().mean()+torch.clamp_max(agent_V,max=0).square().mean() )
             #constraint_loss=0.5*((expert_V/2).exp().mean()+(-agent_V/2).exp().mean() )#expert_next_V.mean()(torch.clamp_min(expert_V,min=0).exp().mean()+torch.clamp_min(-agent_V,min=0).exp().mean() )
-            constraint_loss=(expert_current_V_diff.square().mean()+agent_current_V_diff.square().mean() )#10*
+            constraint_loss=100*(expert_current_V_diff.square().mean()+agent_current_V_diff.square().mean() )#10*
 
             #constraint_loss=10*((expert_V-expert_current_target_V).square().mean()+(agent_V-agent_current_target_V).square().mean() )
             self.log("train/constraint_loss", constraint_loss.item(), on_step=True, batch_size=1)
