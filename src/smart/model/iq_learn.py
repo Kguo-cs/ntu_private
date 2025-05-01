304e70270cdfa0f249225e4bac2f6057aaabd484
Kguo-cs <u3006612@connect.hku.hk>
2025-05-01 13:18:18 +0800

    rkl_temp1_2e4_SV5_nearest20

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -273,7 +273,7 @@ class IQ_SoftQ(LightningModule):
 
             #critic_loss=self.reward_w*(reward_loss+reward_mean)#self.global_step/10000*+expert_constraint_loss+agent_constraint_loss
 
-            div='expx'
+            div='rkl'
             alpha=1
 
             if div=="lsif":
