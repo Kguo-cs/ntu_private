52a09fcb705c5806f9447eec311ad926bcbd5345
Kguo-cs <u3006612@connect.hku.hk>
2025-05-03 22:00:11 +0800

    x2_4_plr20pl10_pla40max10_allV_finaldone

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -101,7 +101,7 @@ class IQ_SoftQ(LightningModule):
 
         #done[~action_mask] = 1
 
-        #done[:, -1] = 1
+        done[:, -1] = 1
 
         next_V = (1 - done) * next_V
 
@@ -278,7 +278,7 @@ class IQ_SoftQ(LightningModule):
 
             self.log("train/critic_loss", critic_loss.item(), on_step=True, batch_size=1)
 
-            constraint_loss=5*(expert_current_V_diff.square().mean()+agent_current_V_diff.square().mean() )#10*000/(self.global_step+1)10*
+            constraint_loss=5*(expert_V_diff.square().mean()+agent_V_diff.square().mean() )#10*000/(self.global_step+1)10*
 
             constraint_ratio=critic_loss/constraint_loss
 
