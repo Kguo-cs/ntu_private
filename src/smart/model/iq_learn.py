0ce0775fb859c20eb0ef455b53d3335ad3735406
Kguo-cs <u3006612@connect.hku.hk>
2025-05-05 15:45:40 +0800

    rkl05_allV5_adam_noactiondone

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -226,7 +226,7 @@ class IQ_SoftQ(LightningModule):
             agent_reward,agent_V,agent_Q,agent_next_V,agent_V_diff,agent_current_V_diff,agent_next_V_diff,agent_Q_diff ,_,agent_entropy= self.get_QV(tokenized_map_rollout,tokenized_agent_rollout, key='agent')
 
             div='rkl'
-            alpha=10
+            alpha=0.5
             eps=1e-3
 
             if div=="lsif":
