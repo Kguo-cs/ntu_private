f90a571f8534d0c21b32475ab672c386d84b471f
Kguo-cs <u3006612@connect.hku.hk>
2025-05-05 10:04:34 +0800

    rkl5_allV5_adam_noactiondone

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -240,8 +240,8 @@ class IQ_SoftQ(LightningModule):
 
             #critic_loss=self.reward_w*(reward_loss+reward_mean)#self.global_step/10000*+expert_constraint_loss+agent_constraint_loss
 
-            div='x2'
-            alpha=4
+            div='rkl'
+            alpha=5
             eps=1e-3
 
             if div=="lsif":
@@ -283,7 +283,7 @@ class IQ_SoftQ(LightningModule):
 
             self.log("train/critic_loss", critic_loss.item(), on_step=True, batch_size=1)
 
-            constraint_loss=1*(expert_V_diff.square().mean()+agent_V_diff.square().mean() )#10*000/(self.global_step+1)10*
+            constraint_loss=5*(expert_V_diff.square().mean()+agent_V_diff.square().mean() )#10*000/(self.global_step+1)10*
 
             constraint_ratio=critic_loss/constraint_loss
 
