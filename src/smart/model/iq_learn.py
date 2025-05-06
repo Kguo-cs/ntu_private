9e46b21f8c5c7ced159b154238a30e6dc83aae5e
Kguo-cs <u3006612@connect.hku.hk>
2025-05-06 10:13:06 +0800

    x2_allV4_adam_noactiondone_midlane_return

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -27,7 +27,7 @@ class IQ_SoftQ(LightningModule):
             self.replay_buffer = deque(maxlen=1)
 
         self.reward_w = 1
-        self.use_target_q=True
+        self.use_target_q=False
         self.soft_update=True
 
         self.rollout_freq=1
@@ -158,6 +158,8 @@ class IQ_SoftQ(LightningModule):
             target_current_V=target_V[:,:-1]
             target_next_V=target_V[:,1:]
 
+            reward= current_Q - self.gamma * target_next_V
+
         reward = reward[state_action_mask]
 
         current_Q=current_Q[state_action_mask]
@@ -271,7 +273,7 @@ class IQ_SoftQ(LightningModule):
 
             self.log("train/critic_loss", critic_loss.item(), on_step=True, batch_size=1)
 
-            constraint_loss=5*(expert_V_diff.square().mean()+agent_V_diff.square().mean() )#10*000/(self.global_step+1)10*
+            constraint_loss=0*(expert_V_diff.square().mean()+agent_V_diff.square().mean() )#10*000/(self.global_step+1)10*
 
             constraint_ratio=critic_loss/constraint_loss
 
