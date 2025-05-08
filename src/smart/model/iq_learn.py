4223b70b1b0b9fbc4516384130304649a4a6855a
Kguo-cs <u3006612@connect.hku.hk>
2025-05-08 18:45:47 +0800

    x2_4_nofinaldone_notarget_replace_vreg5

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -206,7 +206,7 @@ class IQ_SoftQ(LightningModule):
 
         last_V=V[:,-1][valid_mask[:,-1]]
 
-        V_diff=last_V#(V-target_V)[cumulative_mask]#last_V#(V-target_V)[:,-1][valid_mask[:,-1]]
+        V_diff=(V-target_V)[cumulative_mask]#last_V#(V-target_V)[:,-1][valid_mask[:,-1]]
 
         current_V=current_V[state_mask]
 
