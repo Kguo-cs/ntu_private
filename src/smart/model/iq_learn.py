95a310f32648b5a4d19c0777395f92a4fe19603d
Kguo-cs <u3006612@connect.hku.hk>
2025-05-09 18:09:57 +0800

    x2_4_expertV20

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -305,7 +305,7 @@ class IQ_SoftQ(LightningModule):
 
             self.log("train/critic_loss", critic_loss.item(), on_step=True, batch_size=1)
 
-            constraint_loss=10*(expert_V_diff.square().mean()+agent_V_diff.square().mean() )#10*000/(self.global_step+1)10*
+            constraint_loss=20*(expert_V_diff.square().mean() )#+agent_V_diff.square().mean()10*000/(self.global_step+1)10*
 
             constraint_ratio=critic_loss/constraint_loss
 
