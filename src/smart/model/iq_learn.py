1eba3b9de9dc3614aa3c6c1f9b369f29f29e152e
Kguo-cs <u3006612@connect.hku.hk>
2025-05-11 17:03:09 +0800

    x2_4_experttarget_diff02_V10

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -27,7 +27,7 @@ class IQ_SoftQ(LightningModule):
             self.replay_buffer = deque(maxlen=1)
 
         self.reward_w = 1
-        self.use_target_q=False
+        self.use_target_q=True
         self.soft_update=True
 
         self.rollout_freq=1
@@ -167,7 +167,7 @@ class IQ_SoftQ(LightningModule):
                 target_q, target_current_Q, target_V,target_current_V,target_next_V, target_reward,_ = self.get_network_QV(self.target_net, tokenized_map, tokenized_agent,action,key,cumulative_mask)
         else:
             target_V = returns#cannot .detach()
-            reward= current_Q - self.gamma * next_returns
+            #reward= current_Q - self.gamma * next_returns
 
         reward = reward[cumulative_mask[:,1:]]
 
@@ -285,7 +285,7 @@ class IQ_SoftQ(LightningModule):
 
             self.log("train/critic_loss", critic_loss.item(), on_step=True, batch_size=1)
 
-            constraint_loss=10*((expert_V_diff-0.1).square()).mean() #(expert_V_diff.square().mean() +agent_V_diff.square().mean())#10*000/(self.global_step+1)10*
+            constraint_loss=10*((expert_V_diff-0.2).square()).mean() #(expert_V_diff.square().mean() +agent_V_diff.square().mean())#10*000/(self.global_step+1)10*
 
             constraint_ratio=critic_loss/constraint_loss
 
