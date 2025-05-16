52d63cfb6b9e16d0e6173762b7bf4698e78c5c40
Kguo-cs <u3006612@connect.hku.hk>
2025-05-16 17:46:01 +0800

    bc_inter10_pla15

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -296,7 +296,7 @@ class IQ_SoftQ(LightningModule):
 
             self.log("train/constraint_loss", constraint_loss.item(), on_step=True, batch_size=1)
 
-            loss = critic_loss+constraint_loss #-0.01*agent_entropy.mean() #expert_nll+expert_nll+expert_nll+.square().square()expert_nll++(expert_target_loss+agent_target_loss) # #*0.1
+            loss = expert_nll #critic_loss+constraint_loss #-0.01*agent_entropy.mean() #expert_nll+expert_nll+expert_nll+.square().square()expert_nll++(expert_target_loss+agent_target_loss) # #*0.1
 
         return loss
 
