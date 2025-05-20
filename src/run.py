e12cbe8002b27601455c9ac5b57be81bd68fef94
Kguo-cs <u3006612@connect.hku.hk>
2025-05-20 14:51:56 +0800

    bc

--- src/run.py
+++ src/run.py
@@ -33,6 +33,7 @@ sys.path.append('/home/ke/code/sim')
 sys.path.append('/home/users/ntu/ke.guo/scratch/sim')
 sys.path.append('/home/ke/code/catk')
 sys.path.append('/home/users/ntu/zhangshu/scratch/sim')
+sys.path.append('/home/users/ntu/shanhelo/scratch/keguo_projects/sim')
 
 working_dir=os.getcwd()
 
