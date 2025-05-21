29e0ebd3cec8a56c34d2ef229924bca516a91c7b
Kguo-cs <u3006612@connect.hku.hk>
2025-05-21 16:16:04 +0800

    bc_pla8_light_all_lg2lg_shareapt40

--- src/smart/modules/agent_decoder.py
+++ src/smart/modules/agent_decoder.py
@@ -610,7 +610,6 @@ class SMARTAgentDecoder(nn.Module):
                 mask=mask,  # [n_agent, n_step]
                 batch_s=batch_s,  # [n_agent*n_step]
                 batch_pl=batch_lg,  # [n_pl*n_step]
-                pl2a_radius=100
             )
             #
             # predicted_tokens = light_idx.clone()
@@ -1002,7 +1001,6 @@ class SMARTAgentDecoder(nn.Module):
                     mask=inference_mask[:, -hist_step:],  # [n_agent, hist_step]
                     batch_s=batch_s,  # [n_agent*hist_step]
                     batch_pl=batch_lg,  # [n_pl*hist_step]
-                    pl2a_radius=100
                 )
 
             pt_token=map_feature["pt_token"]
