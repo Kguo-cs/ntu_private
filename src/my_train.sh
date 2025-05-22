c1e748b037ad3fdf290198aef52dd95c531a394a
Kguo-cs <u3006612@connect.hku.hk>
2025-05-22 19:54:30 +0800

    bc_shareaptemp100_pt10_route10_stateTrue_layer24

--- src/my_train.sh
+++ src/my_train.sh
@@ -2,7 +2,7 @@
 #PBS -N std
 
 #PBS -l select=1:ncpus=112:ngpus=8:mem=320gb:container_engine=enroot
-#PBS -l walltime=12:00:00
+#PBS -l walltime=16:00:00
 #PBS -q normal
 #PBS -P 12002486
 #PBS -j oe
@@ -12,4 +12,4 @@ source "/home/users/ntu/shanhelo/miniconda3/bin/activate"
 cd /home/users/ntu/shanhelo/scratch/keguo_projects/sim/src
 conda activate catk
 
-python -m torch.distributed.run --nproc_per_node=2 --master_port=29502 run.py > cat.log  2>&1
+python -m torch.distributed.run --nproc_per_node=8 --master_port=29502 run.py > cat.log  2>&1
