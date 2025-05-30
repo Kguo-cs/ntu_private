f79115e3879c1ddfecaf775200dc635ee57b06a5
Kguo-cs <u3006612@connect.hku.hk>
2025-05-30 15:47:54 +0800

    [200~bc_noattnmask~

--- src/smart/modules/map_decoder.py
+++ src/smart/modules/map_decoder.py
@@ -41,7 +41,7 @@ class SMARTMapDecoder(nn.Module):
         super(SMARTMapDecoder, self).__init__()
         self.pl2pl_radius = pl2pl_radius
         self.num_layers = num_layers
-        self.use_map=False
+        self.use_map=True
 
         if self.use_map:
             self.type_pt_emb = nn.Embedding(10, hidden_dim)
