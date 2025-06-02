f18ff7cbee59cd2d36431083303fdcd1fd12bf89
Kguo-cs <u3006612@connect.hku.hk>
2025-06-02 19:49:49 +0800

    bc_mixed_pt10_planearest100

--- src/smart/modules/roformer_agent_decoder.py
+++ src/smart/modules/roformer_agent_decoder.py
@@ -107,7 +107,7 @@ class SMARTAgentDecoder(nn.Module):
             
             self.pt2a_roformer = RoFormerBlock(hidden_dim=hidden_dim, num_heads=num_heads, dropout=dropout)
 
-            self.use_gnn=False
+            self.use_gnn=True
             input_dim_r_a2a = 3
 
             if self.use_gnn:
