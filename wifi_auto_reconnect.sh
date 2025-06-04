8973c6603c340e455f96a89adf6b5bdb3ceaee5a
Kguo-cs <u3006612@connect.hku.hk>
2025-06-04 10:26:00 +0800

    bc64_nomap_near40

--- wifi_auto_reconnect.sh
+++ wifi_auto_reconnect.sh
@@ -9,7 +9,7 @@ SSID="NTUSECURE"
 # Check if Wi-Fi is connected
 if ! nmcli -t -f DEVICE,STATE dev | grep -q "${WIFI_INTERFACE}:connected"; then
     echo "$(date): Wi-Fi is disconnected. Attempting to reconnect..."
-    nmcli dev wifi connect "$SSID" ifname "$WIFI_INTERFACE"
+    sudo nmcli dev wifi connect "$SSID" ifname "$WIFI_INTERFACE"
 else
     echo "$(date): Wi-Fi is connected."
 fi
