aacee19430624f74fa42c3249b752ffbe8281f0c
Kguo-cs <u3006612@connect.hku.hk>
2025-06-05 20:51:36 +0800

    bc64_gnn

--- src/run.py
+++ src/run.py
@@ -27,16 +27,16 @@ import torch
 import numpy as np
 import random
 
-seed = 42
-random.seed(seed)
-np.random.seed(seed)
-torch.manual_seed(seed)
-torch.cuda.manual_seed(seed)
-torch.cuda.manual_seed_all(seed)
-torch.use_deterministic_algorithms(True)
-torch.backends.cudnn.deterministic = True
-torch.backends.cudnn.benchmark = False
-torch.cuda.synchronize()
+# seed = 42
+# random.seed(seed)
+# np.random.seed(seed)
+# torch.manual_seed(seed)
+# torch.cuda.manual_seed(seed)
+# torch.cuda.manual_seed_all(seed)
+# torch.use_deterministic_algorithms(True)
+# torch.backends.cudnn.deterministic = True
+# torch.backends.cudnn.benchmark = False
+# torch.cuda.synchronize()
 
 os.environ["WANDB_SILENT"] = "true"
 
