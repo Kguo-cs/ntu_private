3b204331ea789215779fece88f55baa1798c79ce
Kguo-cs <u3006612@connect.hku.hk>
2025-06-05 16:14:36 +0800

    bc32_expertmask_nomap_30_done_mix2x2

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -267,7 +267,7 @@ class IQ_SoftQ(LightningModule):
                 tokenized_map_rollout, tokenized_agent_rollout, key='agent')
 
             div = 'x2'
-            alpha =0.5
+            alpha =2
             eps = 1e-3
 
             if div == "lsif":
@@ -285,8 +285,9 @@ class IQ_SoftQ(LightningModule):
             elif div=='x2':
                 value_loss=(agent_value_loss.mean()+expert_value_loss.mean())/2
 
-                chi2_loss=(expert_reward.square().mean()/ (4 * alpha)+agent_reward.square().mean()/ (4 * alpha)) /2
+                #chi2_loss=(expert_reward.square().mean()/ (4 * alpha)+agent_reward.square().mean()/ (4 * alpha)) /2
 
+                chi2_loss =expert_reward.square().mean()/ (4 * alpha)
                 critic_loss= -expert_reward.mean()+value_loss +chi2_loss
 
                 #(agent_value_loss.mean()/2+expert_value_loss.mean()/2)#agent_value_loss.mean()
