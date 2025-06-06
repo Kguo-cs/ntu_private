82e0802d93e7bb7b952a8fe1b1f434416d66e571
Kguo-cs <u3006612@connect.hku.hk>
2025-06-06 13:57:06 +0800

    bc64_pt8_map_share_mixx2

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -26,7 +26,7 @@ class IQ_SoftQ(LightningModule):
         else:
             self.replay_buffer = deque(maxlen=1)
 
-        self.finetune = False#model_config.finetune
+        self.finetune = True#model_config.finetune
         self.use_target_q=False
         self.soft_update=True
 
@@ -266,7 +266,7 @@ class IQ_SoftQ(LightningModule):
             agent_reward, agent_value_loss, agent_V_diff, _, agent_entropy = self.get_QV(
                 tokenized_map_rollout, tokenized_agent_rollout, key='agent')
 
-            div = 'rkl'
+            div = 'x2'
             alpha = 1
             eps = 1e-3
 
