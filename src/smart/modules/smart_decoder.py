29eb6975e548ce24a4b0c693c0811f0e6265ae90
Kguo-cs <u3006612@connect.hku.hk>
2025-06-07 23:10:55 +0800

    bc32_pt30_share_map_mix05x2_freq5_len50

--- src/smart/modules/smart_decoder.py
+++ src/smart/modules/smart_decoder.py
@@ -201,8 +201,8 @@ class SMARTDecoder(nn.Module):
             map_feature = self.map_encoder(tokenized_map)
             map_feature_dict={}
             for key in map_feature.keys():
-                map_feature[key] = map_feature[key].detach()
-            tokenized_map["map_feature"] = map_feature
+                map_feature_dict[key] = map_feature[key].detach()
+            tokenized_map["map_feature"] = map_feature_dict
 
         pred_dict = self.agent_encoder(tokenized_agent, map_feature)
 
