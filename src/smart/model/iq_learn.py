a5d5afef9038428542ec768e2b2f4326db850ee1
Kguo-cs <u3006612@connect.hku.hk>
2025-06-12 21:08:37 +0800

    bc64_pt20_map_col_fut

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -281,7 +281,9 @@ class IQ_SoftQ(LightningModule):
         future_valid[:, -1] = True  # No future after last step
 
         # ---- Final step mask ----
-        train_mask =future_valid.to(bool) & valid_mask #valid_mask.all(-1) #past_valid & future_valid  # [N, T]
+        train_mask = future_valid.to(bool) & valid_mask #valid_mask.all(-1) #past_valid & future_valid  # [N, T]
+
+        train_mask= train_mask[:,1:] & train_mask[:,:-1]
 
         #train_mask=valid_mask.all(-1)
 
