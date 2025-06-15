b0655eceb97b6e8bb58bd12a24505bb655c170cc
Kguo-cs <u3006612@connect.hku.hk>
2025-06-15 21:16:34 +0800

    bc24_gmm_cov01_x2V0_nll

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -408,7 +408,7 @@ class IQ_SoftQ(LightningModule):
                     tokenized_agent[key] = agent[key]
                 pred=self.token_processor.my_match_agent_token(agent["gt_valid_raw"],agent["gt_pos_raw"],
                                                                 agent["gt_head_raw"],
-                                                                agent_shape,token_traj,not self.encoder.iq_learn
+                                                                agent_shape,token_traj,True
                                                                 )
 
                 tokenized_agent["valid_mask"]=pred["valid_mask"]
