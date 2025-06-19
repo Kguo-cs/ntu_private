55e46f41566fa2c227c624fae0c9c7c35ece9a89
Kguo-cs <u3006612@connect.hku.hk>
2025-06-19 09:51:27 +0800

    bc128_pt20_map9

--- src/smart/modules/map_decoder.py
+++ src/smart/modules/map_decoder.py
@@ -133,7 +133,7 @@ class SMARTMapDecoder(nn.Module):
                 x_pt = self.pt2pt_layers[i](x_pt, r_pt2pt, edge_index_pt2pt)
 
         # mask=torch.isin(tokenized_map["type"],torch.tensor([0,1,2,3,4,5]).to(batch.device))#9,,10
-        mask=tokenized_map["type"]<11
+        mask=tokenized_map["type"]<9
         # self.lane_style = [
         #     (COLOR_WHITE, 6),  # FREEWAY = 0
         #     (COLOR_ALUMINIUM_2, 6),  # SURFACE_STREET = 1
