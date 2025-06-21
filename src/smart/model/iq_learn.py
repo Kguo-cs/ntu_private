bb8ba276d00f0136c5db8d23dbe2c9690af5067e
Kguo-cs <u3006612@connect.hku.hk>
2025-06-21 22:17:51 +0800

    bc64_map_light5maskgnn100_nonoise

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -133,7 +133,7 @@ class IQ_SoftQ(LightningModule):
 
             logpi= torch.log(pi+ 1e-10)#.clamp_min(min=1e-10)
 
-            log_pi_stack=torch.log(torch.softmax(all_q_value[:, :-1]/ self.alpha, dim=-1)+ 1e-10)
+            log_pi_stack=torch.log_softmax(all_q_value[:, :-1]/ self.alpha, dim=-1)
 
             rolling_action = torch.stack([
                         torch.roll(action, shifts=-i, dims=1)
