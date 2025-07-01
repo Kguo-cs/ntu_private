07b3c63399bb8d991948325a550da364baa90839
Kguo-cs <u3006612@connect.hku.hk>
2025-07-01 14:15:10 +0800

    bc15_5hz_nolight

--- src/smart/tokens/trajectory_process.py
+++ src/smart/tokens/trajectory_process.py
@@ -6,6 +6,7 @@ import torch
 import datetime
 from torch_geometric.data import HeteroData
 import numpy as np
+import matplotlib.pyplot as plt
 
 #
 # data_directory = "/home/ke/code/catk/src/waymo_data/full/training_inter10_a91/"
@@ -45,15 +46,77 @@ import numpy as np
 
 
 
-traj=torch.load("/home/ke/code/catk/src/waymo_data/traj.pt")
-type=torch.load("/home/ke/code/catk/src/waymo_data/type.pt")
+traj=torch.load("/home/ke/code/catk/src/waymo_data/traj.pt").cuda()
+type=torch.load("/home/ke/code/catk/src/waymo_data/type.pt").cuda()
 
-veh_traj=traj[type==0]
-ped_traj=traj[type==1]
-cyc_traj=traj[type==2]
+# ped_traj=traj[type==1]
+# cyc_traj=traj[type==2]
+#
+# print(veh_traj.size())#8559983
+# print(ped_traj.size())#585226
+# print(cyc_traj.size())#60093
 
-print(veh_traj.size())#8559983
-print(ped_traj.size())#585226
-print(cyc_traj.size())#60093
+for type_id in [0,1,2]:
+    veh_traj=traj[type==type_id][:,::5]
+
+    veh_vel=(veh_traj[:,1:,:2]-veh_traj[:,:-1,:2])/0.5
+
+    veh_speed=torch.norm(veh_vel,dim=-1)
+    veh_acc=(veh_speed[:,1:]-veh_speed[:,:-1])/0.5
+
+    print((veh_acc>-5).to(torch.float).mean())
+    print((veh_acc<5).to(torch.float).mean())
 
 
+    # yaw
+
+    veh_rate=(veh_traj[:,1:,2]-veh_traj[:,:-1,2])/0.5
+
+    print((veh_rate>-1.5).to(torch.float).mean())
+    print((veh_rate<1.5).to(torch.float).mean())
+
+
+# tensor(0.9996, device='cuda:0')
+# tensor(0.9996, device='cuda:0')
+# tensor(0.9999, device='cuda:0')
+# tensor(0.9999, device='cuda:0')
+# tensor(1.0000, device='cuda:0')
+# tensor(1.0000, device='cuda:0')
+# tensor(0.9909, device='cuda:0')
+# tensor(0.9906, device='cuda:0')
+# tensor(0.9999, device='cuda:0')
+# tensor(0.9999, device='cuda:0')
+# tensor(0.9978, device='cuda:0')
+# tensor(0.9978, device='cuda:0')
+
+#yaw rate
+
+#-1 tensor(0.9999, device='cuda:0')
+#1 tensor(0.9999, device='cuda:0')
+
+# veh_speed_99 = torch.quantile(veh_speed.to(torch.float32).flatten(), 0.99)
+# print(f"99th percentile of vehicle speed: {veh_speed_99.item():.2f} m/s")
+
+
+# # veh_acc=(veh_speed[:,1:]-veh_speed[:,:-1])/0.1
+# bincount=torch.bincount(veh_speed.to(torch.int32).flatten())
+#
+# # Prepare x and y
+# x = torch.arange(len(bincount)).numpy()
+# y = bincount.numpy()
+# # Plot
+# plt.figure(figsize=(10, 5))
+# plt.bar(x, y)
+# plt.yscale('log')  # <-- Log scale here
+# plt.xlabel("Speed (m/s, integer)")
+# plt.ylabel("Count (log scale)")
+# plt.title("Vehicle Speed Distribution (Log Scale)")
+# plt.grid(True, which="both", linestyle="--", linewidth=0.5)
+# plt.tight_layout()
+# plt.show()
+
+#print("max_acc",veh_acc.max())
+#print("min_acc",veh_acc.min())
+
+#print(veh_vel.())
+#print(veh_acc.size())
