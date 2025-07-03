c222fb5ed593ad47a3ba3cc47de184fcbc351e09
Kguo-cs <u3006612@connect.hku.hk>
2025-07-03 20:44:20 +0800

    bc64_embed_internosys

--- src/smart/tokens/trajectory_select.py
+++ src/smart/tokens/trajectory_select.py
@@ -39,14 +39,14 @@ for type_id in [0,1,2]:#
     #
     # veh_traj[...,2][mask]=-veh_traj[...,2][mask]
     # veh_traj[...,1][mask]=-veh_traj[...,1][mask]
-    veh_traj[...,1]=veh_traj[...,1].abs()
-    veh_traj[...,2]=veh_traj[...,2].abs()
+    # veh_traj[...,1]=veh_traj[...,1].abs()
+    # veh_traj[...,2]=veh_traj[...,2].abs()
 
     if type_id == 0:
         x_min, x_max = -5, 20
         y_max = 1.5
-        x_interval = 0.1
-        y_interval = 0.05
+        x_interval =0.2 #0.1
+        y_interval =0.1 # 0.05
     elif type_id == 1:
         x_min, x_max = -1.5 , 4.5
         y_max=2
@@ -57,10 +57,12 @@ for type_id in [0,1,2]:#
         y_max=1
         x_interval = 0.05
         y_interval = 0.05
+        
+    y_min= -y_max
 
     final_pos = veh_traj[..., -1, :2]
 
-    mask=(final_pos[:,0]>x_min) & (final_pos[:,0]<x_max) & (final_pos[:,1]<y_max)
+    mask=(final_pos[:,0]>x_min) & (final_pos[:,0]<x_max) & (final_pos[:,1]<y_max) & (final_pos[:,1]>y_min)
 
     veh_traj=veh_traj[mask]
     #print(len(veh_traj_in)/len(veh_traj))
@@ -68,13 +70,13 @@ for type_id in [0,1,2]:#
     final_pos = veh_traj[..., -1, :2]
 
     x_bin=(x_max - x_min) / x_interval
-    y_bin= y_max / y_interval
+    y_bin= ( y_max-y_min) / y_interval
 
     x_bin = int(x_bin)
     y_bin = int(y_bin)
 
     x_idx= ((final_pos[:, 0] -x_min) /x_interval).long()
-    y_idx= (final_pos[:, 1]/ y_interval).long()
+    y_idx= ((final_pos[:, 1]-y_min)/ y_interval).long()
 
     x_idx = x_idx.clamp(0, x_bin-1)
     y_idx = y_idx.clamp(0, y_bin-1)
@@ -84,7 +86,7 @@ for type_id in [0,1,2]:#
     joint_hist = torch.bincount(joint_idx, minlength=x_bin * y_bin)#.reshape(250, 30)
 
     # Top-k
-    cluster_n=1024
+    cluster_n=2048
 
     top_k_value, top_k_flat_idx = torch.topk(joint_hist, k=cluster_n)#.flatten()
 
@@ -104,16 +106,16 @@ for type_id in [0,1,2]:#
         idx = top_k_flat_idx[i]
         traj2 = veh_traj[joint_idx == idx]#[:10000000]
 
-        mean_traj=torch.mean(traj2[:,-1,:2], dim=0)
+        # mean_traj=torch.mean(traj2[:,-1,:2], dim=0)
 
-        dist=torch.norm(traj2[:,-1,:2]-mean_traj[None],dim=-1)#.mean(-1) #.argmin()
+        # dist=torch.norm(traj2[:,-1,:2]-mean_traj[None],dim=-1)#.mean(-1) #.argmin()
 
-        choice_index=torch.argmin(dist)
+        # choice_index=torch.argmin(dist)
 
-        #choice_index = torch.randint(0, traj2.shape[0], (1,)).item()
+        # #choice_index = torch.randint(0, traj2.shape[0], (1,)).item()
 
-        meaning_traj=traj2[choice_index]
-        #meaning_traj= traj2.mean(dim=0) #.numpy()
+        # meaning_traj=traj2[choice_index]
+        meaning_traj= traj2.mean(dim=0) #.numpy()
         traj_list.append(meaning_traj.cpu().to(torch.float32))
 
         # traj2=torch.cat([torch.zeros_like(traj2[:,:1]),traj2], dim=1)
@@ -139,18 +141,18 @@ for type_id in [0,1,2]:#
     #     plt.plot(meaning_traj[:,0],meaning_traj[:,1])#, alpha=0.1, color='C0'
     #
     # plt.show()
-    traj_list = torch.stack(traj_list, dim=0)
+    codebook = torch.stack(traj_list, dim=0)
 
     # inverse_contour = traj_list.clone()
     # inverse_contour[:, :, 1] = -inverse_contour[:, :, 1]
     #
     # contour = torch.cat([traj_list, inverse_contour], dim=0)
 
-    inverse_traj = traj_list.clone()
-    inverse_traj[:, :, 1] = -inverse_traj[:, :, 1]
-    inverse_traj[:, :, 2] = -inverse_traj[:, :, 2]
+    # inverse_traj = traj_list.clone()
+    # inverse_traj[:, :, 1] = -inverse_traj[:, :, 1]
+    # inverse_traj[:, :, 2] = -inverse_traj[:, :, 2]
 
-    codebook = torch.cat([traj_list, inverse_traj], dim=0)
+    # codebook = torch.cat([traj_list, inverse_traj], dim=0)
 
     codebook=torch.cat([torch.zeros_like(codebook[:,:1]),codebook], dim=1)
 
@@ -204,10 +206,11 @@ with open("my2048.pkl", "wb") as f:
 # torch.Size([1608651, 5, 3])
 # tensor(0.9901, device='cuda:0') 0.9964125220448686 tensor(0.9937, device='cuda:0') tensor(18, device='cuda:0')
 
-#2048
+
+# 0.1,0.2
 # torch.Size([215685024, 5, 3])
-# tensor(0.9995, device='cuda:0') 0.9998906507296492 tensor(0.9996, device='cuda:0') tensor(113, device='cuda:0')
+# tensor(0.9999, device='cuda:0') 0.9998906507296492 tensor(1.0000, device='cuda:0') tensor(26, device='cuda:0')
 # torch.Size([16594332, 5, 3])
-# tensor(0.9996, device='cuda:0') 0.999764979994374 tensor(0.9998, device='cuda:0') tensor(4, device='cuda:0')
+# tensor(0.9989, device='cuda:0') 0.999764979994374 tensor(0.9991, device='cuda:0') tensor(36, device='cuda:0')
 # torch.Size([1608651, 5, 3])
-# tensor(0.9954, device='cuda:0') 0.9964125220448686 tensor(0.9990, device='cuda:0') tensor(4, device='cuda:
+# tensor(0.9901, device='cuda:0') 0.9964125220448686 tensor(0.9937, device='cuda:0') tensor(18, device='cuda:0')
