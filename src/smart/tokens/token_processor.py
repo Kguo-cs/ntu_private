4c210892e2dee1efc68afbcf85ad4615bf9ac567
Kguo-cs <u3006612@connect.hku.hk>
2025-07-07 10:11:47 +0800

    bc40_8192_iq

--- src/smart/tokens/token_processor.py
+++ src/smart/tokens/token_processor.py
@@ -66,7 +66,7 @@ class TokenProcessor(torch.nn.Module):
 
         self.light_type=5
 
-        self.use_light=True
+        self.use_light=False
 
         self.pred_proposal=False
 
@@ -80,7 +80,7 @@ class TokenProcessor(torch.nn.Module):
         if self.pred_all_token:
             self.n_token_agent=self.agent_token_all_veh.shape[0]
             
-        self.pred_res= True
+        self.pred_res= False
         if self.pred_res:
             self.n_token_agent+=1
 
