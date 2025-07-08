6036b2727d55dea260bc531fd87dfea79fa31c38
Kguo-cs <u3006612@connect.hku.hk>
2025-07-08 12:25:06 +0800

    bc64_nonoise_diff05_sa

--- src/smart/modules/map_decoder.py
+++ src/smart/modules/map_decoder.py
@@ -58,11 +58,11 @@ class SMARTMapDecoder(nn.Module):
             # map_token_traj_src: [n_token, 11, 2].flatten(0,1)
             # self.my_map=False
 
-            # if self.my_map:
-            #     self.token_emb = MLPEmbedding(input_dim=27, hidden_dim=hidden_dim)
-            # else:
-            #     self.token_emb = MLPEmbedding(input_dim=22, hidden_dim=hidden_dim)
-            self.token_emb = nn.Embedding(token_processor.n_token_map, hidden_dim)
+            if self.my_map:
+                self.token_emb = MLPEmbedding(input_dim=27, hidden_dim=hidden_dim)
+            else:
+                self.token_emb = MLPEmbedding(input_dim=22, hidden_dim=hidden_dim)
+            #self.token_emb = nn.Embedding(token_processor.n_token_map, hidden_dim)
 
             if self.gnn:
                 input_dim_r_pt2pt = 3
@@ -99,9 +99,9 @@ class SMARTMapDecoder(nn.Module):
         if "orientation" in tokenized_map.keys():
             pos_pt = tokenized_map["position"]
             orient_pt = tokenized_map["orientation"]
-            # pt_token_emb_src = self.token_emb(self.token_processor.map_token_traj_src)
-            # x_pt = pt_token_emb_src[tokenized_map["token_idx"].long()]
-            x_pt = self.token_emb(tokenized_map["token_idx"].long())
+            pt_token_emb_src = self.token_emb(self.token_processor.map_token_traj_src)
+            x_pt = pt_token_emb_src[tokenized_map["token_idx"].long()]
+            #x_pt = self.token_emb(tokenized_map["token_idx"].long())
         else:
             traj_pos= tokenized_map["traj_pos"]
             pos_pt=traj_pos[:,0,:2]
