959435253b99090634ffb1f3250f6e5d9d287111
Kguo-cs <u3006612@connect.hku.hk>
2025-07-08 19:23:24 +0800

    bc64_allres05_diff_sa_nodetach

--- src/my_train.sh
+++ src/my_train.sh
@@ -12,4 +12,4 @@ source "/home/users/ntu/shanhelo/miniconda3/bin/activate"
 cd /home/users/ntu/shanhelo/scratch/keguo_projects/sim/src
 conda activate catk
 
-python  run.py > bc64_allres05_diff_sa.log  2>&1
+python  run.py > bc64_allres05_diff_sa_nodetach.log  2>&1
