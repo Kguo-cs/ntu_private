ccbfcecfaa041645d5a40aaece6d0a1bac3c7cb4
Kguo-cs <u3006612@connect.hku.hk>
2025-07-09 18:00:57 +0800

    bc72_my_residx

--- src/smart/modules/agent_decoder.py
+++ src/smart/modules/agent_decoder.py
@@ -355,11 +355,11 @@ class SMARTAgentDecoder(nn.Module):
 
                     next_token_traj_all = token_local_traj[torch.arange(n_agent)[:,None], next_token_idx]
 
-                    token_diff = torch.index_select(self.token_processor.max_diff, dim=0, index=tokenized_agent["type"].long())
-
-                    proposal_max_diff = token_diff[torch.arange(n_agent)[:,None], next_token_idx]
-
-                    proposal=torch.tanh(proposal)*proposal_max_diff[:,:,None]
+                    # token_diff = torch.index_select(self.token_processor.max_diff, dim=0, index=tokenized_agent["type"].long())
+                    #
+                    # proposal_max_diff = token_diff[torch.arange(n_agent)[:,None], next_token_idx]
+                    #
+                    # proposal=torch.tanh(proposal)*proposal_max_diff[:,:,None]
 
                     proposal=proposal+next_token_traj_all[:,:,None]
 
@@ -552,11 +552,11 @@ class SMARTAgentDecoder(nn.Module):
                         token_diff = torch.index_select(self.token_processor.max_diff, dim=0,
                                                         index=tokenized_agent["type"].long())
 
-                        proposal_max_diff = token_diff[torch.arange(n_agent), next_token_idx]
-
-                        proposal = torch.tanh(proposal) * proposal_max_diff
-
-                        next_token_traj_all = token_local_traj[torch.arange(n_agent), next_token_idx]
+                        # proposal_max_diff = token_diff[torch.arange(n_agent), next_token_idx]
+                        #
+                        # proposal = torch.tanh(proposal) * proposal_max_diff
+                        #
+                        # next_token_traj_all = token_local_traj[torch.arange(n_agent), next_token_idx]
 
                         proposal=proposal+next_token_traj_all
 
