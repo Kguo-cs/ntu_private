c1143474313aa40d33331daed036afafb00537b7
Kguo-cs <u3006612@connect.hku.hk>
2025-07-09 13:35:12 +0800

    bc72_mydist_residx_nodetach

--- src/my_train.sh
+++ src/my_train.sh
@@ -12,4 +12,4 @@ source "/home/users/ntu/shanhelo/miniconda3/bin/activate"
 cd /home/users/ntu/shanhelo/scratch/keguo_projects/sim/src
 conda activate catk
 
-python  run.py > bc72_mydist_diff.log  2>&1
+python  run.py > bc72_mydist_residx_nodetach.log  2>&1
