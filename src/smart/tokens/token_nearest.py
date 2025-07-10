92cdd950c17baa861832123b69df2b01fb327994
Kguo-cs <u3006612@connect.hku.hk>
2025-07-10 11:34:52 +0800

    bc62_mykdist4_residx_nodetach

--- src/smart/tokens/token_nearest.py
+++ src/smart/tokens/token_nearest.py
@@ -19,18 +19,18 @@ sys.path.append('/mnt/d/code/sim')
 from src.smart.utils import cal_polygon_contour, transform_to_local, wrap_angle
 
 
-res = pickle.load(open("/home/ke/code/catk/src/smart/tokens/my_kdist.pkl", "rb"))
-agent_token_data=res['token_all']   
+agent_token_data = pickle.load(open("/home/ke/code/catk/src/smart/tokens/my_kdist.pkl", "rb"))['token_all']
+
 diff_list=[]
 
 for type_id in [0,1,2]:#
     
-    all_v=torch.load("/home/ke/code/catk/src/waymo_data/"+str(type_id)+".pt").cuda() #[:,-1:]
-    all_idx= torch.load("/home/ke/code/catk/src/waymo_data/nearest_token_"+str(type_id)+".pt").cuda() #[:,-1:]
+    all_v=torch.load("/home/ke/code/catk/src/waymo_data/"+str(type_id)+".pt")[:,-1:]
+    
     
     k= ["veh", "ped", "cyc"][type_id]
 
-    token_traj_all=torch.FloatTensor(agent_token_data[k]).to(torch.float16)[:,1:].cuda() #[1, n_token,4, 2]
+    token_traj_all=torch.FloatTensor(agent_token_data[k]).to(torch.float16)[:,-1].cuda() #[1, n_token,4, 2]
 
     print(all_v.shape,token_traj_all.shape)
 
@@ -41,38 +41,38 @@ for type_id in [0,1,2]:#
     elif k == "cyc":
         width_length = torch.tensor([1.0, 2.0])
 
-    pred_pos = token_traj_all.mean(2)
-    diff_xy = token_traj_all[:, :, 0] - token_traj_all[:, :, 3]
-    pred_head = torch.arctan2(diff_xy[:, :, 1], diff_xy[ :, :, 0])
+    nearsest_token = []
+        
+    for i in range(2000):
+        v= all_v[i*110000:(i+1)*110000].cuda()  # [1, 1, 4]
 
-    token_local_traj = torch.cat([pred_pos, pred_head[:, :,None]], dim=-1)
+        gt_contour = cal_polygon_contour(
+            pos=v[:, :, :2],  # [N, 1, 2]
+            head=v[:, :, 2],  # [N, 1]
+            width_length=width_length.unsqueeze(0).cuda(),
+        )# [n_agent, 1, 4, 2]
+
+        token_idx_gt = torch.argmin(
+            torch.norm(token_traj_all[None] - gt_contour, dim=-1).sum(-1), dim=-1
+        )  # [n_agent]
+        
+        nearsest_token.append(token_idx_gt)
+        
+        # print(i)
+        
+    nearsest_token = torch.cat(nearsest_token).cpu()
     
-    traj_diff=[]
+    print(nearsest_token.shape)
     
-    for i in range(len(token_local_traj)):
-        
-        traj2 = all_v[all_idx==i]#.cuda()
-        
-        meaning_traj= token_local_traj[i]#traj2.mean(dim=0) #.numpy()
+    torch.save(nearsest_token, "/home/ke/code/catk/src/waymo_data/nearest_token_"+str(type_id)+".pt")
+    
+    
+    # pred_pos = token_traj_all.mean(2)
+    # diff_xy = token_traj_all[:, :, 0] - token_traj_all[:, :, 3]
+    # pred_head = torch.arctan2(diff_xy[:, :, 1], diff_xy[ :, :, 0])
 
-        diff=traj2-meaning_traj[None]
+    # token_local_traj = torch.cat([pred_pos, pred_head[:, :,None]], dim=-1)[:,-1]
 
-        max_diff=diff.abs().amax(dim=0)#torch.minimum(diff.amax(dim=0), -diff.amin(dim=0))
-        
-        diff[:,2]=wrap_angle(diff[:,2])
-        
-        std_diff = diff.std(dim=0)*4
-        
-        max_diff = torch.minimum(max_diff, std_diff)
-        
-        traj_diff.append(max_diff.cpu())
-
-    diff_list.append(torch.stack(traj_diff))
-
-
-res["max_diff"]=torch.stack(diff_list)
-with open("my_kdist.pkl", "wb") as f:
-    pickle.dump(res, f)
 
     # all_diff=[]
 
