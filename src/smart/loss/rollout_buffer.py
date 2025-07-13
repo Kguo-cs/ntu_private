566807bfdd9ea02fb06e492907e3df4b1f131a2e
Kguo-cs <u3006612@connect.hku.hk>
2025-07-13 15:39:21 +0800

    bc20_high2noise_Bce_h800

--- src/smart/loss/rollout_buffer.py
+++ src/smart/loss/rollout_buffer.py
@@ -92,6 +92,8 @@ def rollout(encoder, tokenized_map, tokenized_agent,post_sampling=False):
     encoder.train()
 
     tokenized_agent.update(pred)
+    for key in ["sampled_pos", "sampled_heading"]:
+        tokenized_agent[key] = tokenized_agent[key] + 1e-3 * torch.randn_like(tokenized_agent[key])
 
     # tokenized_agent_rollout = tokenized_agent
     # tokenized_agent_rollout['num_graphs'] = tokenized_agent['num_graphs']
