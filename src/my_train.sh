f1237c826728fde013cfa79a2f3de11a6d90f88e
Kguo-cs <u3006612@connect.hku.hk>
2025-07-20 13:42:17 +0800

    GAIL64_map5_11_airl_normR

--- src/my_train.sh
+++ src/my_train.sh
@@ -12,4 +12,4 @@ source "/home/users/ntu/shanhelo/miniconda3/bin/activate"
 cd /home/users/ntu/shanhelo/scratch/keguo_projects/sim/src
 conda activate catk
 
-python  run.py > bc64_map5_11.log  2>&1
+python  run.py > GAIL64_map5_11_airl_normR.log  2>&1
