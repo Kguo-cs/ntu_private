558341d913f39595e19099864c00203cd628598b
Kguo-cs <u3006612@connect.hku.hk>
2025-07-23 11:09:26 +0800

    AIRL64_map10

--- src/smart/modules/map_decoder.py
+++ src/smart/modules/map_decoder.py
@@ -98,16 +98,16 @@ class SMARTMapDecoder(nn.Module):
         map_type[map_type>9] = 9
         
         #mask = torch.zeros_like(map_type, dtype=bool)
-        mask = torch.ones_like(map_type, dtype=bool)
-
-        type4_indices=torch.where((map_type==4) |(map_type==5))[0]
-
-        sampled_indices = type4_indices[::2]
-
-        mask[sampled_indices] = True
-
-        mask[(map_type!=4)&(map_type!=5)] = True
-
+        # mask = torch.ones_like(map_type, dtype=bool)
+        #
+        # type4_indices=torch.where((map_type==4) |(map_type==5))[0]
+        #
+        # sampled_indices = type4_indices[::2]
+        #
+        # mask[sampled_indices] = True
+        #
+        # mask[(map_type!=4)&(map_type!=5)] = True
+        #
         batch = tokenized_map["batch"]#[mask]
 
         if self.my_map:
