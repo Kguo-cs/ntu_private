d52e0f3eea43e6ec5d0ea46ab8c207bc20ee19eb
Kguo-cs <u3006612@connect.hku.hk>
2025-07-24 17:12:47 +0800

    AIRL64_map2_agentall_anorm_sa16_notrans

--- src/smart/modules/interative_decoder.py
+++ src/smart/modules/interative_decoder.py
@@ -129,10 +129,10 @@ class InterativeDecoder(nn.Module):
         all_features=[feat.flatten(0, 1) for feat in all_features ]#.transpose(0, 1)
         feat_a,pos_s, head_s, head_vector_s,mask_s, batch_s,batch_s_repeat,batch_pl=all_features
 
-        batch_pl = batch_pl #map_feature["batch"]
-        pos_pl = map_feature["position"][:,None].repeat( 1,n_step,1).flatten(0, 1)
-        orient_pl = map_feature["orientation"][:,None].repeat(1,n_step).flatten(0, 1)
-        feat_map = map_feature["pt_token"].unsqueeze(1).expand( -1,n_step, -1).flatten(0, 1)
+        #batch_pl = map_feature["batch"]
+        pos_pl = map_feature["position"][:,None].repeat( 1,n_step,1).flatten(0, 1)#.transpose(0, 1)
+        orient_pl = map_feature["orientation"][:,None].repeat(1,n_step).flatten(0, 1)#.transpose(0, 1)
+        feat_map = map_feature["pt_token"].unsqueeze(1).expand( -1,n_step, -1).flatten(0, 1)#.transpose(0, 1)
 
         edge_index_a2a, r_a2a = self.edge_encoder.build_interaction_edge(
             pos_s=pos_s,  # [n_agent, n_step, 2]
@@ -169,9 +169,7 @@ class InterativeDecoder(nn.Module):
 
             feat_a = self.pt2a_attn_layers[layer_i]((feat_map, feat_a), r_pl2a, edge_index_pl2a)
 
-        feat_a = feat_a.view(n_agent,-1,  self.hidden_dim)#.transpose(0, 1)
-#tensor(7.992, device='cuda:0', grad_fn=<MaxBackward1>)
-#tensor(-0.050, device='cuda:0', grad_fn=<MeanBackward0>)
+        feat_a = feat_a.view(-1, n_agent, self.hidden_dim)#.transpose(0, 1)
         proposal=None
 
         if self.pred_last_res:
