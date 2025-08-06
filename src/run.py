c454c751d357529ff449c7af49557e62f9e9f6a1
Kguo-cs <u3006612@connect.hku.hk>
2025-08-06 16:30:18 +0800

    AIRL64_finenotoken_density1

--- src/run.py
+++ src/run.py
@@ -39,7 +39,7 @@ sys.path.append('/home/ke/code/catk')
 sys.path.append('/home/users/ntu/zhangshu/scratch/sim')
 sys.path.append('/home/users/ntu/shanhelo/scratch/keguo_projects/sim')
 sys.path.append('/mnt/d/code/sim')
-sys.path.append('home/guoke/sim')
+sys.path.append('/home/guoke/sim')
 
 working_dir=os.getcwd()
 
