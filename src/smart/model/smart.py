bd2cb4c35b0fcd8302559ab5ff7a6ae98220f77b
Kguo-cs <u3006612@connect.hku.hk>
2025-08-06 17:33:52 +0800

    AIRL64_finenotoken_density1

--- src/smart/model/smart.py
+++ src/smart/model/smart.py
@@ -231,8 +231,7 @@ class SMART(LightningModule):
                     # )
                     # self.logger.log_metrics(epoch_wosac_metrics)
                     for key, value in epoch_wosac_metrics.items():
-                        # print(key, value)
-                        self.log(key, value, on_step=False, on_epoch=True, prog_bar=True, sync_dist=False)
+                        self.log(key, value, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True, rank_zero_only=True)
 
                 self.wosac_metrics.reset()
                 self.minADE.reset()
