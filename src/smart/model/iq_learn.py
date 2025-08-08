8cd93753a55d0b2ffe76de3ba618b50c3bc59fc1
Kguo-cs <u3006612@connect.hku.hk>
2025-08-08 11:30:15 +0800

    AIRL64_disnot_novalue_entropy01

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -452,7 +452,7 @@ class IQ_SoftQ(LightningModule):
 
                 self.log("train/agent_density", agent_density.item(), on_step=True, batch_size=1)
 
-                expert_nll = expert_nll + gail_weight*agent_wNLL + value_loss-0.1*agent_entropy.mean()  # - 0.01 * agent_entropy.mean()
+                expert_nll = expert_nll + gail_weight*agent_wNLL + value_loss+0.1*agent_entropy.mean()  # - 0.01 * agent_entropy.mean()
 
             else:
                 agent_reward, agent_value_loss, agent_V_diff, agent_nll,agent_Q,agent_proposal_loss = self.get_QV(
