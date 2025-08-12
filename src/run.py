d8774fe7305331c29219b89cc1d0cf81c2dfbe6a
Kguo-cs <u3006612@connect.hku.hk>
2025-08-12 10:45:49 +0800

    AIRL64_disallrollAll_shape_1e4_nei5

--- src/run.py
+++ src/run.py
@@ -121,7 +121,7 @@ def run(cfg: DictConfig) -> None:
     elif cfg.action == "finetune":
         log.info("Starting finetuning!")
         model.load_state_dict(torch.load(cfg.ckpt_path, weights_only=False)["state_dict"], strict=False)
-        model.target_net.load_state_dict(model.encoder.agent_encoder.state_dict())
+        #model.target_net.load_state_dict(model.encoder.agent_encoder.state_dict())
         trainer.fit(model=model, datamodule=datamodule)
     elif cfg.action == "validate":
         log.info("Starting validating!")
