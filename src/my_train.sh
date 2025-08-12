825f7958508f77550e64bbf31c05be9bd706037d
Kguo-cs <u3006612@connect.hku.hk>
2025-08-12 14:00:34 +0800

    AIRL64_disallrollAll_shape_1e4_dist

--- src/my_train.sh
+++ src/my_train.sh
@@ -12,7 +12,7 @@ source "/home/users/ntu/shanhelo/miniconda3/bin/activate"
 cd /home/users/ntu/shanhelo/scratch/keguo_projects/sim/src
 conda activate catk
 
-python  run.py > AIRL64_disallrollAll_shape_kl01.log  2>&1
+python  run.py > AIRL64_disallrollAll_shape_1e4_dist.log  2>&1
 #torchrun --nproc_per_node=2  -m run trainer=ddp  > AIRL128_disnot_running_edge.log  2>&1
 
 ##python -m torch.distributed.run --nproc_per_node=4 --master_port=29502 run.py > pad064_32_noshare.log  2>&1
