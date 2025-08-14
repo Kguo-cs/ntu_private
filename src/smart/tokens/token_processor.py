f76fbe6939214a917d24c6d3e2dfadc35b5c68aa
Kguo-cs <u3006612@connect.hku.hk>
2025-08-14 16:27:31 +0800

    bc32_histdrop01_05

--- src/smart/tokens/token_processor.py
+++ src/smart/tokens/token_processor.py
@@ -378,11 +378,11 @@ class TokenProcessor(torch.nn.Module):
             # [n_agent, 4, 2]
             token_contour_gt = token_world_gt[range_a, token_idx_gt]
 
-            if  self.pred_last_res:
-                token_valid=min_dist<0.5
-                # token_idx_gt[~token_valid]=self.agent_token_all_veh.shape[0]
-                # _valid_mask=token_valid & _valid_mask
-                _valid_mask[~token_valid]=False
+            #if  self.pred_last_res:
+            token_valid=min_dist<0.5
+            # token_idx_gt[~token_valid]=self.agent_token_all_veh.shape[0]
+            # _valid_mask=token_valid & _valid_mask
+            _valid_mask[~token_valid]=False
 
             if self.pred_all_res and self.max_diff is not None:
                 token_local_traj= self.token_local_traj[torch.arange(n_agent), token_idx_gt][:,-1:]  # [n_agent, 5,3]
