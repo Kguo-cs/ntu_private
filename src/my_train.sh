ed3a504ae8a1ec2a1c1b4eb59635c98889e61ed0
Kguo-cs <u3006612@connect.hku.hk>
2025-08-16 13:35:37 +0800

    AIRL128_dist_clean

--- src/my_train.sh
+++ src/my_train.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #PBS -N std
 
-#PBS -l select=1:ncpus=112:ngpus=1:mem=320gb:container_engine=enroot
-#PBS -l walltime=12:00:00
+#PBS -l select=1:ncpus=112:ngpus=2:mem=320gb:container_engine=enroot
+#PBS -l walltime=24:00:00
 #PBS -q normal
 #PBS -P 12002486
 #PBS -j oe
@@ -12,7 +12,7 @@ source "/home/users/ntu/shanhelo/miniconda3/bin/activate"
 cd /home/users/ntu/shanhelo/scratch/keguo_projects/sim/src
 conda activate catk
 
-python  run.py > training_map2_clean.log  2>&1
-#torchrun --nproc_per_node=2  -m run trainer=ddp  > AIRL128_disnot_running_edge.log  2>&1
+#python  run.py > training_map2_clean.log  2>&1
+torchrun --nproc_per_node=2  -m run trainer=ddp  > training_map2_clean.log  2>&1
 
 ##python -m torch.distributed.run --nproc_per_node=4 --master_port=29502 run.py > pad064_32_noshare.log  2>&1
