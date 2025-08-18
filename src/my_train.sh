791ec24be223444eee71a8d7d7d48c6532ae2f3b
Kguo-cs <u3006612@connect.hku.hk>
2025-08-18 11:34:29 +0800

    AIRL128_1e4_03_Return05_normt

--- src/my_train.sh
+++ src/my_train.sh
@@ -13,6 +13,6 @@ cd /home/users/ntu/shanhelo/scratch/keguo_projects/sim/src
 conda activate catk
 
 #python  run.py > training_map2_clean.log  2>&1
-torchrun --nproc_per_node=2  -m run trainer=ddp  > training_map2_clean.log  2>&1
+torchrun --nproc_per_node=2  -m run trainer=ddp  > AIRL128_1e4_03_Return05_normt.log  2>&1
 
 ##python -m torch.distributed.run --nproc_per_node=4 --master_port=29502 run.py > pad064_32_noshare.log  2>&1
