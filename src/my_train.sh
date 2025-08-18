adc2a946fe2cc8fcd7df0cbd36f9dfdf6a176231
Kguo-cs <u3006612@connect.hku.hk>
2025-08-18 11:35:56 +0800

    AIRL64_1e4_03_Return05_normt

--- src/my_train.sh
+++ src/my_train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -N std
 
-#PBS -l select=1:ncpus=112:ngpus=2:mem=320gb:container_engine=enroot
+#PBS -l select=1:ncpus=112:ngpus=1:mem=320gb:container_engine=enroot
 #PBS -l walltime=24:00:00
 #PBS -q normal
 #PBS -P 12002486
@@ -12,7 +12,7 @@ source "/home/users/ntu/shanhelo/miniconda3/bin/activate"
 cd /home/users/ntu/shanhelo/scratch/keguo_projects/sim/src
 conda activate catk
 
-#python  run.py > training_map2_clean.log  2>&1
-torchrun --nproc_per_node=2  -m run trainer=ddp  > AIRL128_1e4_03_Return05_normt.log  2>&1
+python  run.py > AIRL64_1e4_03_Return05_normt.log  2>&1
+#torchrun --nproc_per_node=1  -m run trainer=ddp  > AIRL128_1e4_03_Return05_normt.log  2>&1
 
 ##python -m torch.distributed.run --nproc_per_node=4 --master_port=29502 run.py > pad064_32_noshare.log  2>&1
