bdad5b31961908b1cc7741066aade6a336866a76
Kguo-cs <u3006612@connect.hku.hk>
2025-08-21 19:16:33 +0800

    AIRL64_dis05_inter1detach_nextA

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -454,7 +454,7 @@ class IQ_SoftQ(LightningModule):
         self.log("train/"+key+"_return", returns.mean().item(), on_step=True, batch_size=1)
         self.log("train/"+key+"_rewards", rewards.mean().item(), on_step=True, batch_size=1)
 
-        return bce_loss+bottleneck_loss-0.03*entropy,rewards,returns,disc_val
+        return bce_loss+bottleneck_loss,rewards,returns,disc_val
 
     def iq_update(self, tokenized_map, tokenized_agent):
         valid_mask= tokenized_agent["valid_mask"][:, self.start_step:]
