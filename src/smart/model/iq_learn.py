ff35fbff0e5af0d838fd4c7590512167f84d9977
Kguo-cs <u3006612@connect.hku.hk>
2025-08-25 22:09:32 +0800

    AIRL64_learntoken_dist_tanh10

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -317,6 +317,8 @@ class IQ_SoftQ(LightningModule):
                                                         tokenized_agent["light_idx"],
                                                         None)[0]
 
+        logit=torch.tanh(logit)*10
+
 
         disc_val = self.get_d(logit[:, :, 0],log_prob)
         # svo=torch.sigmoid(logit[:,:,1])
