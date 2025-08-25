a10d3b74ab8ce1883959975d024f1ebf2ac153ff
Kguo-cs <u3006612@connect.hku.hk>
2025-08-25 12:41:50 +0800

    AIRL64_distoken

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -68,7 +68,7 @@ class IQ_SoftQ(LightningModule):
 
         self.use_lcf=self.encoder.agent_encoder.use_lcf
 
-        self.dis_loss="wgan"
+        self.dis_loss="gail"
 
         self.learn_lcf=self.encoder.learn_lcf
 
