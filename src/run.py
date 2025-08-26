2c934137bf8efd50203a70c4b6a713af0fa5f5d5
Kguo-cs <u3006612@connect.hku.hk>
2025-08-26 16:11:03 +0800

    bc32_histdrop01_03_formert

--- src/run.py
+++ src/run.py
@@ -43,7 +43,7 @@ sys.path.append('/home/ke/code/catk')
 sys.path.append('/home/users/ntu/zhangshu/scratch/sim')
 sys.path.append('/home/users/ntu/shanhelo/scratch/keguo_projects/sim')
 sys.path.append('/mnt/d/code/sim')
-sys.path.append('/home/keguo/sim')
+sys.path.append('/home/ke/keguo/sim')
 
 working_dir=os.getcwd()
 
