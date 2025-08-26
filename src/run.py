c96fd8bc64fd1ba586390c07b19d6c2dabf091b2
Kguo-cs <u3006612@connect.hku.hk>
2025-08-26 16:23:42 +0800

    AIRL128_learntokenmap_dist_latent4

--- src/run.py
+++ src/run.py
@@ -44,7 +44,7 @@ sys.path.append('/home/users/ntu/zhangshu/scratch/sim')
 sys.path.append('/home/users/ntu/shanhelo/scratch/keguo_projects/sim')
 sys.path.append('/mnt/d/code/sim')
 sys.path.append('/home/ke/keguo/sim')
-
+sys.path.append('/home/guoke/sim')
 working_dir=os.getcwd()
 
 print('keguo' in working_dir or "guoke" in working_dir)
