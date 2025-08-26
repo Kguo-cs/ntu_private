477c8bb0f3e03dc69afcef03f6ecaf042e108ecb
Kguo-cs <u3006612@connect.hku.hk>
2025-08-26 16:10:07 +0800

    bc32_histdrop01_03_formert

--- src/run.py
+++ src/run.py
@@ -34,6 +34,7 @@ wandb.login(key='7eba71eb2539f241fbf502af503ea5dd098168ae')
 wandb.require("service")  # forces the new service backend
 # Optional: use thread start (very robust in multiprocess settings)
 settings = wandb.Settings(start_method="thread")
+os.environ["WANDB__SERVICE_WAIT"] = "3000"
 
 sys.path.append('/home/users/ntu/lyuchen/scratch/keguo_projects/ntu/sim')
 sys.path.append('/home/ke/code/sim')
