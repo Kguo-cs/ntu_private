f203dc5d44977c8b2be850d27ca3a3ad76ebac04
Kguo-cs <u3006612@connect.hku.hk>
2025-08-28 23:03:49 +0800

    bc32_map1_inter2_pt20

--- src/smart/modules/map_decoder.py
+++ src/smart/modules/map_decoder.py
@@ -83,7 +83,7 @@ class SMARTMapDecoder(nn.Module):
     def forward(self, tokenized_map: Dict):
 
         map_type=tokenized_map["type"].long()
-        #map_type[map_type>9] = 9
+        map_type[map_type>9] = 9
         
         # mask = torch.zeros_like(map_type, dtype=bool)
         # #mask = torch.ones_like(map_type, dtype=bool)
