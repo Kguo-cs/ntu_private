8ebac6039308d16ebf81af847e720728aee32b78
Kguo-cs <u3006612@connect.hku.hk>
2025-09-01 23:36:09 +0800

    AIRL64_value0001distclip10displr20a5_predmask

--- src/smart/modules/smart_decoder.py
+++ src/smart/modules/smart_decoder.py
@@ -172,7 +172,7 @@ class SMARTDecoder(nn.Module):
                     hist_drop_prob=0,
                     n_token_agent=1,
                     pt2a_neighbor=10,
-                    a2a_neighbor=10,
+                    a2a_neighbor=5,
                     token_processor=token_processor,
                     alpha=self.alpha,
                     output_gmm=False,
