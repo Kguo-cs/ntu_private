d648665723a40cb75bb654404bf2794d3b13bbec
Kguo-cs <u3006612@connect.hku.hk>
2025-09-02 16:23:39 +0800

    AIRL64_value0001_nofilter

--- src/smart/modules/interative_decoder.py
+++ src/smart/modules/interative_decoder.py
@@ -205,14 +205,14 @@ class InterativeDecoder(nn.Module):
        # a2a_list=[]
 
         for layer_i in range(self.num_layers):
-            if self.num_layers>1 and layer_i == self.num_layers - 1 and train_mask is not None:
-                end_mask=train_repeat_mask[edge_index_a2a[1]]
-                edge_index_a2a = edge_index_a2a[:, end_mask]
-                r_a2a=r_a2a[end_mask]
-
-                end_pt_mask=train_repeat_mask[edge_index_pl2a[1]]
-                edge_index_pl2a = edge_index_pl2a[:, end_pt_mask]
-                r_pl2a=r_pl2a[end_pt_mask]
+            # if self.num_layers>1 and layer_i == self.num_layers - 1 and train_mask is not None:
+            #     end_mask=train_repeat_mask[edge_index_a2a[1]]
+            #     edge_index_a2a = edge_index_a2a[:, end_mask]
+            #     r_a2a=r_a2a[end_mask]
+            #
+            #     end_pt_mask=train_repeat_mask[edge_index_pl2a[1]]
+            #     edge_index_pl2a = edge_index_pl2a[:, end_pt_mask]
+            #     r_pl2a=r_pl2a[end_pt_mask]
 
             feat_a,a2a_attn = self.a2a_attn_layers[layer_i](feat_a, r_a2a, edge_index_a2a)
 
