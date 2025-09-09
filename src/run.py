e813667a9c796c4e80ae9f74b33939971a05d2e0
Kguo-cs <u3006612@connect.hku.hk>
2025-09-09 12:51:49 +0800

    AIRL64_map2_token1050

--- src/run.py
+++ src/run.py
@@ -124,7 +124,7 @@ def run(cfg: DictConfig) -> None:
         trainer.fit(model=model, datamodule=datamodule, ckpt_path=cfg.get("ckpt_path"))
     elif cfg.action == "finetune":
         log.info("Starting finetuning!")
-        model.load_state_dict(torch.load(cfg.ckpt_path, weights_only=False)["state_dict"], strict=True)
+        model.load_state_dict(torch.load(cfg.ckpt_path, weights_only=False)["state_dict"], strict=False)
         if model.encoder.agent_encoder.use_kl_penalty:
             model.bc_net.load_state_dict(model.encoder.agent_encoder.state_dict())
             if model.bc_map_net is not None:
