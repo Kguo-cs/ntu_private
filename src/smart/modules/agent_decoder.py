16b56ae8d4bf60d71cbdbe395b1f87d2df9015a7
Kguo-cs <u3006612@connect.hku.hk>
2025-09-10 09:38:20 +0800

    bc_map2_gnn_drop01

--- src/smart/modules/agent_decoder.py
+++ src/smart/modules/agent_decoder.py
@@ -105,7 +105,7 @@ class SMARTAgentDecoder(nn.Module):
                         hidden_dim=hidden_dim,
                         num_heads=num_heads,
                         head_dim=head_dim,
-                        dropout=dropout,
+                        dropout=hist_drop_prob,
                         bipartite=False,
                         has_pos_emb=True,
                     )
