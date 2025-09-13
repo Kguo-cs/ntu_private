c8be1f255204b8a28060a0d33e9b853ce7959304
Kguo-cs <u3006612@connect.hku.hk>
2025-09-13 18:17:40 +0800

    AIRL64_edgesum_lcf_wewightloss_a6020weight51_pt4020

--- src/smart/modules/smart_decoder.py
+++ src/smart/modules/smart_decoder.py
@@ -160,7 +160,7 @@ class SMARTDecoder(nn.Module):
                     num_historical_steps=num_historical_steps,
                     num_future_steps=num_future_steps,
                     time_span=10,
-                    pl2a_radius=10,
+                    pl2a_radius=40,
                     a2a_radius=60,#20 bad
                     num_freq_bands=num_freq_bands,
                     num_layers=1,
@@ -169,7 +169,7 @@ class SMARTDecoder(nn.Module):
                     dropout=0,
                     hist_drop_prob=0,
                     n_token_agent=1,
-                    pt2a_neighbor=10,
+                    pt2a_neighbor=20,
                     a2a_neighbor=20,
                     token_processor=token_processor,
                     alpha=self.alpha,
