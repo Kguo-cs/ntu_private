4842e35e8a9f33bf31b4a42421340656c301e7fc
Kguo-cs <u3006612@connect.hku.hk>
2025-09-13 13:00:09 +0800

    AIRL64_edgesum_lcf_seploss_a60weight10

--- src/smart/modules/interative_decoder.py
+++ src/smart/modules/interative_decoder.py
@@ -406,7 +406,7 @@ class InterativeDecoder(nn.Module):
             if self.use_edge_feature:
                 end_idx = edge_index_a2a[1]  # shape: [E]
 
-                weight=torch.exp(-dist/10)*10
+                weight=torch.exp(-dist/10)
 
                 interact_logits=next_token_logits.detach()*weight[:,None,None]
 
