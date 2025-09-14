e1986cbe591997d7ca5dae1c1c8867d0b30f448c
Kguo-cs <u3006612@connect.hku.hk>
2025-09-14 10:21:15 +0800

    AIRL64_edgesum52_lcf_t30_ata

--- src/smart/modules/interative_decoder.py
+++ src/smart/modules/interative_decoder.py
@@ -229,7 +229,7 @@ class InterativeDecoder(nn.Module):
                 start_index=edge_index_a2a[0]
                 end_index=edge_index_a2a[1]
 
-                start_edge_feature=feat_a[start_index]
+                start_edge_feature=feat_a_token[start_index]
                 end_edge_feature=feat_a[end_index]
 
                 if  train_mask is not None and self.num_layers==1:
