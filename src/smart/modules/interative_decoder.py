a95dcd89aa3b41a280c9efa4171ff9c9b546cfd2
Kguo-cs <u3006612@connect.hku.hk>
2025-09-16 10:43:03 +0800

    AIRL96_lcfweight351

--- src/smart/modules/interative_decoder.py
+++ src/smart/modules/interative_decoder.py
@@ -437,7 +437,7 @@ class InterativeDecoder(nn.Module):
 
                 all_rewards[train_mask]=rewards
 
-                weight2=torch.exp(-dist/3)*2
+                weight2=torch.exp(-dist/5)
 
                 flatten_reward=all_rewards.transpose(0, 1).flatten(0,1)
 
