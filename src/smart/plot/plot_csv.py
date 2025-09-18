0fd9b1634dd63e661acf8e502822af2de01a2e6b
Kguo-cs <u3006612@connect.hku.hk>
2025-09-18 16:53:23 +0800

    AIRL80_learnmap_lcf336_policygnn_type_val40_noedge

--- src/smart/plot/plot_csv.py
+++ src/smart/plot/plot_csv.py
@@ -8,27 +8,29 @@ df = pd.read_csv("/home/ke/code/catk/src/waymo_data/wandb_export_2025-09-18T16_2
 plt.figure(figsize=(8,5))
 
 # --- Run 1: AIRL80_lcf356_noedge ---
-x = df["trainer/global_step"]
-y = df["AIRL80_lcf356_policygnn_type_val40_noedge - train/agent_disc_val"]
-ymin = df["AIRL80_lcf356_policygnn_type_val40_noedge - train/agent_disc_val__MIN"]
-ymax = df["AIRL80_lcf356_policygnn_type_val40_noedge - train/agent_disc_val__MAX"]
+x = df["trainer/global_step"][:300]
+y = df["AIRL80_lcf356_policygnn_type_val40_noedge - train/agent_disc_val"][:300]
+# ymin = df["AIRL80_lcf356_policygnn_type_val40_noedge - train/agent_disc_val__MIN"]
+# ymax = df["AIRL80_lcf356_policygnn_type_val40_noedge - train/agent_disc_val__MAX"]
 
-plt.plot(x, y, label="AIRL80_lcf356_noedge", color="tab:blue")
-plt.fill_between(x, ymin, ymax, alpha=0.2, color="tab:blue")
+plt.plot(x, y, label="PS-GAIL", color="tab:blue")
+# plt.fill_between(x, ymin, ymax, alpha=0.2, color="tab:blue")
 
 # --- Run 2: AIRL80_lcf336 ---
-y2 = df["AIRL80_lcf336_policygnn_type_val40 - train/agent_disc_val"]
-y2min = df["AIRL80_lcf336_policygnn_type_val40 - train/agent_disc_val__MIN"]
-y2max = df["AIRL80_lcf336_policygnn_type_val40 - train/agent_disc_val__MAX"]
+y2 = df["AIRL80_lcf336_policygnn_type_val40 - train/agent_disc_val"][:300]
+#y2min = df["AIRL80_lcf336_policygnn_type_val40 - train/agent_disc_val__MIN"][:len(x)]
+#y2max = df["AIRL80_lcf336_policygnn_type_val40 - train/agent_disc_val__MAX"][:len(x)]
 
-plt.plot(x, y2, label="AIRL80_lcf336", color="tab:orange")
-plt.fill_between(x, y2min, y2max, alpha=0.2, color="tab:orange")
+plt.plot(x, y2, label="DecompGAIL", color="tab:orange")
+# plt.fill_between(x, y2min, y2max, alpha=0.2, color="tab:orange")
 
 # Labels and formatting
 plt.xlabel("Global Step")
-plt.ylabel("Agent Disc Val")
-plt.title("Agent Discriminator Value Comparison")
-plt.legend()
+plt.ylabel("Policy Discriminator Value")
+# plt.title("Policy Discriminator Value Comparison")
+plt.legend(loc="upper right")
 plt.grid(True)
+
+plt.ylim(0.18,0.6)
 plt.tight_layout()
-plt.show()
+plt.savefig('1.pdf')
