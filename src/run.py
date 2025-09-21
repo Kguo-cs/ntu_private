27daaea19b86e44a9fa7c9c25126e7b1ede93a56
Kguo-cs <u3006612@connect.hku.hk>
2025-09-21 21:40:20 +0800

    AIRL80_lcf336_val40_learnmap

--- src/run.py
+++ src/run.py
@@ -125,12 +125,12 @@ def run(cfg: DictConfig) -> None:
         trainer.fit(model=model, datamodule=datamodule, ckpt_path=cfg.get("ckpt_path"))
     elif cfg.action == "finetune":
         log.info("Starting finetuning!")
-        model.load_state_dict(torch.load(cfg.ckpt_path, weights_only=False)["state_dict"], strict=False)
+       # model.load_state_dict(torch.load(cfg.ckpt_path, weights_only=False)["state_dict"], strict=False)
         if model.encoder.use_kl_penalty:
             model.bc_net.load_state_dict(model.encoder.agent_encoder.state_dict())
             if model.bc_map_net is not None:
                 model.bc_map_net.load_state_dict(model.encoder.map_encoder.state_dict())
-        trainer.fit(model=model, datamodule=datamodule)#, ckpt_path=cfg.get("ckpt_path")
+        trainer.fit(model=model, datamodule=datamodule, ckpt_path=cfg.get("ckpt_path"))#, ckpt_path=cfg.get("ckpt_path")
     elif cfg.action == "validate":
         log.info("Starting validating!")
         trainer.validate(
