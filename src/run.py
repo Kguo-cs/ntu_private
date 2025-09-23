325568b33ca008069ccc46960b4284a6bdd7abd7
Kguo-cs <u3006612@connect.hku.hk>
2025-09-23 23:43:18 +0800

    AIRL160_kl4_learnmap

--- src/run.py
+++ src/run.py
@@ -133,8 +133,9 @@ def run(cfg: DictConfig) -> None:
         trainer.fit(model=model, datamodule=datamodule)#, ckpt_path=cfg.get("ckpt_path")
     elif cfg.action == "validate":
         log.info("Starting validating!")
+        model.load_state_dict(torch.load(cfg.ckpt_path, weights_only=False)["state_dict"], strict=False)
         trainer.validate(
-            model=model, datamodule=datamodule, ckpt_path=cfg.get("ckpt_path")
+            model=model, datamodule=datamodule#)##, ckpt_path=cfg.get("ckpt_path")
         )
     elif cfg.action == "test":
         log.info("Starting testing!")
