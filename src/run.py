17416a45fdf9ca0224c4e5561e1b8077f80d5070
Kguo-cs <u3006612@connect.hku.hk>
2025-09-24 09:02:38 +0800

    AIRL56_kl4_learnmap

--- src/run.py
+++ src/run.py
@@ -130,7 +130,7 @@ def run(cfg: DictConfig) -> None:
             model.bc_net.load_state_dict(model.encoder.agent_encoder.state_dict())
             if model.bc_map_net is not None:
                 model.bc_map_net.load_state_dict(model.encoder.map_encoder.state_dict())
-        trainer.fit(model=model, datamodule=datamodule, ckpt_path=cfg.get("ckpt_path"))
+        trainer.fit(model=model, datamodule=datamodule)#, ckpt_path=cfg.get("ckpt_path"))
     elif cfg.action == "validate":
         log.info("Starting validating!")
         trainer.validate(
