9e6108fe10281d49803f2716958a8932735e775b
Kguo-cs <u3006612@connect.hku.hk>
2025-10-02 11:42:04 +0800

    AIRL112_learnmap_mlp3_all13

--- src/smart/modules/interative_decoder.py
+++ src/smart/modules/interative_decoder.py
@@ -169,7 +169,7 @@ class InterativeDecoder(nn.Module):
                 if self.use_iteract:
 
                     self.token_predict_head = MLPLayer(
-                        input_dim=hidden_dim * 3, hidden_dim=hidden_dim, output_dim=n_token_agent
+                        input_dim=hidden_dim * 3, hidden_dim=hidden_dim*3, output_dim=n_token_agent
                     )
 
                 # self.token_predict_head = nn.Sequential(
