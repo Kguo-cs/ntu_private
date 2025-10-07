cd1032730d8125761ef3b779e8ddf2a2b9b929cb
Kguo-cs <u3006612@connect.hku.hk>
2025-10-07 19:17:44 +0800

    AIRL152_learnmap_a10

--- src/smart/modules/interative_decoder.py
+++ src/smart/modules/interative_decoder.py
@@ -449,7 +449,7 @@ class InterativeDecoder(nn.Module):
 
                     #interact_logits=-torch.log(1-torch.sigmoid(next_token_logits[:,:,:1]))*weight[:,None]
 
-                    interact_logits=dirl(next_token_logits[:,:,:1])*weight[:,None]
+                    interact_logits=next_token_logits[:,:,:1]*weight[:,None]
 
                     interact_logits_sum = scatter_sum(interact_logits, end_idx, dim=0, dim_size=mask_a.shape[0]*n_step)#[0]
 
@@ -459,7 +459,7 @@ class InterativeDecoder(nn.Module):
                     rewards=interact_logits_sum.view( n_step,  -1).transpose(0, 1)
 
                     if not self.use_ego_loop:
-                        ego_rewards=dirl(ego_logits).view(n_step,  -1).transpose(0, 1)
+                        ego_rewards=ego_logits.view(n_step,  -1).transpose(0, 1)
                         #ego_rewards=-torch.log(1-torch.sigmoid(ego_logits)).view(n_step,  -1).transpose(0, 1)
 
                         rewards=ego_rewards+rewards#torch.minimum(rewards,ego_rewards)#rewards+ego_rewards#+torch.zeros_like(torch.minimum(ego_rewards,rewards)#)#rewards+ego_rewards#
