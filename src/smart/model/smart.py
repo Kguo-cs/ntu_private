7aed2a7ff32c601e4a3e50ca46e84c950dd8a543
Kguo-cs <u3006612@connect.hku.hk>
2025-10-10 14:52:27 +0800

    bc32_nuplan_noiseagentmap4679_drop01Val

--- src/smart/model/smart.py
+++ src/smart/model/smart.py
@@ -116,38 +116,38 @@ class SMART(LightningModule):
 
     def validation_step(self, data, batch_idx):
 
-        tokenized_map, tokenized_agent = self.token_processor(data)
 
-        # ! open-loop vlidation
-        if self.val_open_loop:
-            pred = self.encoder(tokenized_map, tokenized_agent)
-            loss = self.training_loss(
-                **pred,
-                token_agent_shape=tokenized_agent["token_agent_shape"],  # [n_agent, 2]
-                token_traj=tokenized_agent["token_traj"],  # [n_agent, n_token, 4, 2]
-            )
-
-            self.TokenCls.update(
-                # action that goes from [(10->15), ..., (85->90)]
-                pred=pred["next_token_logits"],  # [n_agent, 16, n_token]
-                pred_valid=pred["next_token_valid"],  # [n_agent, 16]
-                target=tokenized_agent["gt_idx"][:, 2:],
-                target_valid=tokenized_agent["valid_mask"][:, 2:],
-            )
-            self.log(
-                "val_open/acc",
-                self.TokenCls,
-                on_epoch=True,
-                sync_dist=True,
-                batch_size=1,
-            )
-            self.log("val_open/loss", loss, on_epoch=True, sync_dist=True, batch_size=1)
+        # # ! open-loop vlidation
+        # if self.val_open_loop:
+        #     pred = self.encoder(tokenized_map, tokenized_agent)
+        #     loss = self.training_loss(
+        #         **pred,
+        #         token_agent_shape=tokenized_agent["token_agent_shape"],  # [n_agent, 2]
+        #         token_traj=tokenized_agent["token_traj"],  # [n_agent, n_token, 4, 2]
+        #     )
+        #
+        #     self.TokenCls.update(
+        #         # action that goes from [(10->15), ..., (85->90)]
+        #         pred=pred["next_token_logits"],  # [n_agent, 16, n_token]
+        #         pred_valid=pred["next_token_valid"],  # [n_agent, 16]
+        #         target=tokenized_agent["gt_idx"][:, 2:],
+        #         target_valid=tokenized_agent["valid_mask"][:, 2:],
+        #     )
+        #     self.log(
+        #         "val_open/acc",
+        #         self.TokenCls,
+        #         on_epoch=True,
+        #         sync_dist=True,
+        #         batch_size=1,
+        #     )
+        #     self.log("val_open/loss", loss, on_epoch=True, sync_dist=True, batch_size=1)
 
         # ! closed-loop vlidation
         if self.global_rank == 0 and self.val_closed_loop:
             pred_traj, pred_z, pred_head = [], [], []
-            #tokenized_map,tokenized_agent = self.encoder.preprocess(tokenized_map, tokenized_agent)
-            map_feature = self.encoder.map_encoder(tokenized_map)
+            # tokenized_map, tokenized_agent = self.token_processor(data)
+            #
+            # map_feature = self.encoder.map_encoder(tokenized_map)
 
             if self.encoder.use_vae:
                 # logits = self.encoder.prior_net.predict_agent(tokenized_agent["sampled_idx"][:, :2],
@@ -186,6 +186,9 @@ class SMART(LightningModule):
             #t1=time.time()
 
             for _ in range(self.n_rollout_closed_val):
+                tokenized_map, tokenized_agent = self.token_processor(data)
+
+                map_feature = self.encoder.map_encoder(tokenized_map)
 
                 if self.encoder.use_vae:
                     latent_z = mu + torch.randn_like(std) * std
--- src/smart/tokens/token_processor.py
+++ src/smart/tokens/token_processor.py
@@ -208,7 +208,7 @@ class TokenProcessor(torch.nn.Module):
 
         self.noise=True
 
-        if self.training and self.noise:
+        if  self.noise:
             topk_indices = torch.argsort(dist, dim=1)[:, :8]
             sample_topk = torch.randint(0, topk_indices.shape[-1], size=(topk_indices.shape[0], 1), device=topk_indices.device)
             token_idx = torch.gather(topk_indices, 1, sample_topk).squeeze(-1)
@@ -382,7 +382,7 @@ class TokenProcessor(torch.nn.Module):
             out_dict["gt_idx"].append(token_idx_gt)
 
 
-            if  self.training and self.noise:
+            if   self.noise:
                 topk_indices = torch.argsort( all_dist,dim=-1)[:, :5]
                 sample_topk = np.random.choice(range(0, topk_indices.shape[1]), topk_indices.shape[0])
                 token_idx_gt = topk_indices[np.arange(topk_indices.shape[0]), sample_topk]
