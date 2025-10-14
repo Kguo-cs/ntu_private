82f308c6e06ccd8217f8ba95d01fde4006be43a2
Kguo-cs <u3006612@connect.hku.hk>
2025-10-14 15:22:12 +0800

    airl64_nomapnoise_inter2_03_route_10nei05token

--- src/smart/modules/interative_decoder.py
+++ src/smart/modules/interative_decoder.py
@@ -438,7 +438,7 @@ class InterativeDecoder(nn.Module):
                     if self.learn_weight:
                         weight =1
                     else:
-                        weight=torch.exp(-dist[:,None]/3)*0.5
+                        weight=torch.exp(-dist[:,None]/3)*3
                         # agent_num=scatter_sum(torch.ones_like(dist), end_idx, dim=0, dim_size=len(train_repeat_mask))#[0] #torch.exp(-dist[:,None]/3)*3
                         #
                         # weight=agent_num[end_idx]
@@ -477,7 +477,7 @@ class InterativeDecoder(nn.Module):
                     all_rewards = torch.zeros_like(head_a)
                     all_rewards[train_mask]=rewards
 
-                weight2=torch.exp(-dist/3)*1
+                weight2=torch.exp(-dist/3)*6
 
                 flatten_reward=all_rewards.transpose(0, 1).flatten(0,1)
 
@@ -523,4 +523,4 @@ class InterativeDecoder(nn.Module):
         else:
             rewards=torch.tensor(0.0),torch.tensor(0.0)
 
-        return next_token_logits,feat_a,proposal,rewards,weight
+        return next_token_logits,feat_a_all,proposal,rewards,weight
