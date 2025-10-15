87b8d13e5d54bb3f249536a457541f25e806ae82
Kguo-cs <u3006612@connect.hku.hk>
2025-10-15 18:32:57 +0800

    1

--- TrafficManager/waymo/waymo_gui.py
+++ TrafficManager/waymo/waymo_gui.py
@@ -15,6 +15,10 @@ from TrafficManager.LimSim.simModel.DataQueue import (
 )
 import importlib.util
 
+import matplotlib as mpl
+
+mpl.rcParams['toolbar'] = 'None'
+
 COLOR_BLACK = (0, 0, 0, 255)
 COLOR_WHITE = (255, 255, 255, 255)
 COLOR_RED = (255, 0, 0, 255)
