336790cc01fbf6969f29dde5875250d240d52a4a
Kguo-cs <u3006612@connect.hku.hk>
2025-10-16 19:37:13 +0800

    bc32_bothnoise004_1467_lane2

--- nuplan_preprocess/data_process.py
+++ nuplan_preprocess/data_process.py
@@ -725,10 +725,10 @@ def process_scenario(scenario):
     data['map_save']['traj_pos']=data['map_save']['traj_pos'][mask]
     data['map_save']['traj_theta']=data['map_save']['traj_theta'][mask]
     data['pt_token']['type']=data['pt_token']['type'][mask]
-    data['pt_token']['pl_type']=data['pt_token']['pl_type'][mask]
     data['pt_token']['num_nodes']=len(data['pt_token']['pl_type'])
 
     del data['pt_token']['light_type']
+    del data['pt_token']['pl_type']
     # traj_pos = data['map_save']['traj_pos']
     #
     # # type=data['pt_token']['type']
