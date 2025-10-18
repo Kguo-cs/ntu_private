1256a284532bf471f1ed53e63469e0881206bdf2
Kguo-cs <u3006612@connect.hku.hk>
2025-10-18 14:41:04 +0800

    AIRL64_learnmap_arn10_nolcf_r2

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -90,7 +90,7 @@ class IQ_SoftQ(LightningModule):
 
         self.use_lcf=self.encoder.use_lcf
 
-        self.dis_loss="wgan"
+        self.dis_loss="gail"
 
         self.learn_lcf=self.encoder.learn_lcf
 
