8fa89d6ead963cd6d4725c3f380fcfdfbff7a6f0
Kguo-cs <u3006612@connect.hku.hk>
2025-10-24 22:13:59 +0800

    AIRL152_learnmap_arn10_predrollout355

--- src/smart/modules/interative_decoder.py
+++ src/smart/modules/interative_decoder.py
@@ -440,7 +440,7 @@ class InterativeDecoder(nn.Module):
                     if self.learn_weight:
                         weight =1
                     else:
-                        weight=torch.exp(-dist[:,None]/3)*3#torch.clamp_max(*0.2,1)
+                        weight=torch.exp(-dist[:,None]/3)*5#torch.clamp_max(*0.2,1)
                         # agent_num=scatter_sum(torch.ones_like(dist), end_idx, dim=0, dim_size=len(train_repeat_mask))#[0] #torch.exp(-dist[:,None]/3)*3
                         #
                         # weight=agent_num[end_idx]
@@ -479,7 +479,7 @@ class InterativeDecoder(nn.Module):
                     all_rewards = torch.zeros_like(head_a)
                     all_rewards[train_mask]=rewards
 
-                weight2=torch.exp(-dist/3)*3
+                weight2=torch.exp(-dist/3)*5
 
                 flatten_reward=all_rewards.transpose(0, 1).flatten(0,1)
 
