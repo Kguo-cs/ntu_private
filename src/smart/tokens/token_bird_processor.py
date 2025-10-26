d9d67f8ca00ac291cbf3f652cdfc5c747f56e6a6
Kguo-cs <u3006612@connect.hku.hk>
2025-10-26 23:08:48 +0800

    bc_pt30_14679_tokemask

--- src/smart/tokens/token_bird_processor.py
+++ src/smart/tokens/token_bird_processor.py
@@ -259,7 +259,7 @@ class TokenProcessor(torch.nn.Module):
             # [n_agent, 4, 2]
             token_contour_gt = token_world_gt[range_a, token_idx_gt]#next_pos
 
-            token_valid=min_dist<0.5
+            token_valid=min_dist<1
             _valid_mask[~token_valid]=False
 
 
