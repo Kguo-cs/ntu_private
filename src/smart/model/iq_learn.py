8fbcbe76a25ffe452209979fe9fbb748de7b7911
Kguo-cs <u3006612@connect.hku.hk>
2025-11-11 15:49:53 +0800

    AIRL152_learnmap_arn10_predvalidrollout355_ego

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -491,7 +491,7 @@ class IQ_SoftQ(LightningModule):
         #
         # self.log("train/"+key+"_gp", gp, on_step=True, batch_size=1)
 
-        return bce_loss,rewards,nei_sum_rewards, disc_val,0#gp*10#torch.sigmoid(logit[:,:,-1]) #-0.03*entropy
+        return bce_loss,ego_rewards,nei_sum_rewards, disc_val,0#gp*10#torch.sigmoid(logit[:,:,-1]) #-0.03*entropy
 
     def iq_update(self, tokenized_map, tokenized_agent):
         valid_mask= tokenized_agent["valid_mask"][:, self.start_step:]
