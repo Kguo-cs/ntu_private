cc03f244057d7ed9830cc65e9b5d7fb43d94cc70
Kguo-cs <u3006612@connect.hku.hk>
2025-11-13 10:00:01 +0800

    iq28_w11_lcf12_value1000_head1mean_exitmask

--- src/smart/modules/interative_decoder.py
+++ src/smart/modules/interative_decoder.py
@@ -223,7 +223,7 @@ class InterativeDecoder(nn.Module):
         mask_ta=mask_a.transpose(0, 1)
         mask_ta_flatten=mask_ta.flatten(0,1)
 
-        if not self.discriminator:
+        if not self.discriminator and not self.token_processor.use_bird:
             feat_a_t = torch.zeros([n_step, n_agent, self.hidden_dim], device=feat_a.device)
 
             feat_a_t[mask_ta] = feat_a
@@ -332,6 +332,26 @@ class InterativeDecoder(nn.Module):
 
             for i in range(self.t_num_layers):
                 feat_a = self.t_attn_layers[i](feat_a, r_t, edge_index_t)
+
+
+        if  self.discriminator or self.token_processor.use_bird:
+            feat_a_t = torch.zeros([n_step, n_agent, self.hidden_dim], device=feat_a.device)
+
+            feat_a_t[mask_ta] = feat_a
+
+            if n_current == 0:
+                self.feat_a_cache = feat_a_t
+            else:
+                self.feat_a_cache = torch.cat((self.feat_a_cache, feat_a_t), dim=0)[-self.agent_hist:]  # t,a
+
+                feat_a = self.feat_a_cache[self.mask_cache.transpose(0, 1)]
+
+            for i in range(self.t_num_layers):
+                feat_a = self.t_attn_layers[i](feat_a, r_t, edge_index_t)
+
+            if n_current != 0:
+                current_len = mask_a.sum()
+                feat_a = feat_a[-current_len:]
 
         if not (self.use_edge_feature and self.discriminator) and (self.num_layers>1 and train_mask is not None):
             feat_a_all = feat_a.view( n_step,  -1,self.hidden_dim).transpose(0, 1)
