e7af753ad54e36a4314101babe06a9c92b0f5a05
Kguo-cs <u3006612@connect.hku.hk>
2025-11-21 10:39:23 +0800

    AIRL80_4025_00_dis60_1m_r2

--- src/my_train.sh
+++ src/my_train.sh
@@ -13,6 +13,6 @@ cd /home/users/ntu/shanhelo/scratch/keguo_projects/sim/src
 conda activate catk
 
 #torchrun --nproc_per_node=2  -m run trainer=ddp  > AIRL160_kl3_learnmap.log  2>&1
-python  run.py > AIRL80_4025_00_dis60_1m_r2.log  2>&1
+python  run.py > AIRL80_lcf11_sum5_3.log  2>&1
 
 ##python -m torch.distributed.run --nproc_per_node=4 --master_port=29502 run.py > pad064_32_noshare.log  2>&1
--- src/run.py
+++ src/run.py
@@ -131,7 +131,7 @@ def run(cfg: DictConfig) -> None:
                 model.bc_net.load_state_dict(model.encoder.agent_encoder.state_dict())
                 if model.bc_map_net is not None:
                     model.bc_map_net.load_state_dict(model.encoder.map_encoder.state_dict())
-        trainer.fit(model=model, datamodule=datamodule, ckpt_path='/home/ke/code/catk/src/waymo_data/epoch=2-step=15220.ckpt')
+        trainer.fit(model=model, datamodule=datamodule)#, ckpt_path='/home/ke/code/catk/src/waymo_data/epoch=2-step=15220.ckpt')
     elif cfg.action == "validate":
         log.info("Starting validating!")
         trainer.validate(
