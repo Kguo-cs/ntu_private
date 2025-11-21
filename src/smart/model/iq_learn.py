8a93cf1ff2d6b00684c1a2e1cfe5dd0f02fac781
Kguo-cs <u3006612@connect.hku.hk>
2025-11-21 17:42:36 +0800

    AIRL80_4025_11_09_dis60_1m

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -357,9 +357,9 @@ class IQ_SoftQ(LightningModule):
 
             nei_advantages, nei_value_loss = compute_advantages(nei_rewards, nei_value, agent_present_mask)
 
-            value_loss = nei_value_loss + value_loss
+            value_loss =0.9 * value_loss+0.1 *nei_value_loss
 
-            advantages = 1 / 2 * advantages + 1 / 2 * nei_advantages
+            advantages = 0.9 * advantages + 0.1 * nei_advantages
 
         self.log("train/value_loss", value_loss.item(), on_step=True, batch_size=1)
 
