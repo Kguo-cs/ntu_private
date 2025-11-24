b204cc6dba95ca9c04d34fcef55c1cd4e88e8293
Kguo-cs <u3006612@connect.hku.hk>
2025-11-24 13:18:50 +0800

    AIRL80_roformershare_4025

--- wifi_auto_reconnect.sh
+++ wifi_auto_reconnect.sh
@@ -11,6 +11,7 @@ if ! curl -s --max-time 5 https://www.google.com >/dev/null 2>&1; then
     echo "$(date): Cannot reach Google. Attempting to reconnect Wi-Fi..."
     sudo nmcli radio wifi on
     sudo systemctl restart NetworkManager
+    sleep 5
     sudo nmcli dev wifi connect "$SSID" ifname "$WIFI_INTERFACE"
 else
     echo "$(date): Google is reachable."
