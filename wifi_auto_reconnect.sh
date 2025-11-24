94c30ae513bbf163e109beb0f8b97aedd74af166
Kguo-cs <u3006612@connect.hku.hk>
2025-11-24 13:15:48 +0800

    AIRL80_roformershare_4025

--- wifi_auto_reconnect.sh
+++ wifi_auto_reconnect.sh
@@ -9,7 +9,7 @@ SSID="NTUSECURE"
 # Check if Wi-Fi is connected
 if ! curl -s --max-time 5 https://www.google.com >/dev/null 2>&1; then
     echo "$(date): Cannot reach Google. Attempting to reconnect Wi-Fi..."
-    nmcli radio wifi on
+    sudo nmcli radio wifi on
     sudo systemctl restart NetworkManager
     sudo nmcli dev wifi connect "$SSID" ifname "$WIFI_INTERFACE"
 else
