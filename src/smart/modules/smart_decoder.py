fa4ea93cb232e201f7cda5e241344a71e525fff6
Kguo-cs <u3006612@connect.hku.hk>
2025-11-25 13:12:02 +0800

    iq3_noedge_distemp32

--- src/smart/modules/smart_decoder.py
+++ src/smart/modules/smart_decoder.py
@@ -136,7 +136,7 @@ class SMARTDecoder(nn.Module):
 
         if self.gail:
             self.discriminator = SMARTAgentDecoder(
-                hidden_dim=hidden_dim,
+                hidden_dim=hidden_dim//2,
                 num_historical_steps=num_historical_steps,
                 num_future_steps=num_future_steps,
                 time_span=10,
@@ -145,7 +145,7 @@ class SMARTDecoder(nn.Module):
                 num_freq_bands=num_freq_bands,
                 num_layers=1,
                 num_heads=num_heads,
-                head_dim=head_dim,
+                head_dim=head_dim//2,
                 dropout=0,
                 hist_drop_prob=0,
                 n_token_agent=1,
