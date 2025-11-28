d0507c6e72f467b5fcf4dd9ee7fc8a00863a9bcf
Kguo-cs <u3006612@connect.hku.hk>
2025-11-28 22:48:12 +0800

    bc40_apt30_tokenexitpred1_14679_newattn_randmask

--- src/smart/modules/interative_decoder.py
+++ src/smart/modules/interative_decoder.py
@@ -219,7 +219,7 @@ class InterativeDecoder(nn.Module):
 
                 #a2a_mask_i= (dist>layer_i*20) & (dist<(layer_i+1)*20)
 
-                feat_a = self.a2a_attn_layers[layer_i](feat_a, r_a2a, edge_index_a2a)
+                feat_a = self.a2a_attn_layers[layer_i](feat_a, r_a2a[layer_i::3], edge_index_a2a[:,layer_i::3])
 
                 if  agent_train_mask is not None and self.num_layers==1:
                     feat_a=feat_a[train_repeat_mask]
