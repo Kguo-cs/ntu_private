ceeab4bd9f65416c923143948a2bd4998f968fa5
Kguo-cs <u3006612@connect.hku.hk>
2025-11-28 10:00:31 +0800

    iq3_roformergate_sa

--- src/smart/modules/agent_token_encoder.py
+++ src/smart/modules/agent_token_encoder.py
@@ -58,7 +58,7 @@ class AgentTokenEncoder(nn.Module):
 
         self.discriminator=discriminator
         self.use_state_action=True
-        self.use_counterfactual=True
+        self.use_counterfactual=False
 
         if self.discriminator:
             if self.use_state_action:
