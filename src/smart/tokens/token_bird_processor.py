0dd0cc01d10f9da3bd313b82bd4bc1ff5ea0880c
Kguo-cs <u3006612@connect.hku.hk>
2025-11-30 09:58:35 +0800

    iq3_roformer_sa15

--- src/smart/tokens/token_bird_processor.py
+++ src/smart/tokens/token_bird_processor.py
@@ -532,7 +532,7 @@ class TokenProcessor(torch.nn.Module):
         if len(entry_token_invalid_mask)>0:
             out_dict["entry_token_invalid_mask"]=torch.cat(entry_token_invalid_mask, dim=0)
 
-        if self.autoregressive_entry:
+        if len(entry_idx_list):
             # entry_length=out_dict['sampled_idx'].shape[1]-1
             out_dict["entry_idx"]=pad_sequence(entry_idx_list, batch_first=True, padding_value=self.n_token_entry-1)#.reshape(entry_length,batch_num,-1)
             out_dict["entry_head_idx"]=pad_sequence(entry_head_idx_list, batch_first=True, padding_value=32)#.reshape(entry_length,batch_num,-1)
