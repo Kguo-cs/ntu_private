09f63673282ef7c09a7758d4eafaa720ac399e50
Kguo-cs <u3006612@connect.hku.hk>
2025-12-05 10:02:07 +0800

    bird6_noentryreplace_63_maxrange

--- src/smart/tokens/HierarchicalStateTokenizer.py
+++ src/smart/tokens/HierarchicalStateTokenizer.py
@@ -30,7 +30,7 @@ class HierarchicalStateTokenizer(nn.Module):
         z_range=( 0  ,64),
         h_range=(-math.pi, math.pi),
         num_levels=3,
-        base=5,
+        base=6,
     ):
         super().__init__()
 
