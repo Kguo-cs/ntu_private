672e2a0b781e52cb593e33085582e87905ffb478
Kguo-cs <u3006612@connect.hku.hk>
2025-12-07 15:34:32 +0800

    bird6_nonautoregressive_entry_noreplace

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -288,7 +288,7 @@ class IQ_SoftQ(LightningModule):
             expert_nll, expert_log_prob,exert_entry_nll,expert_entry_head_nll= self.get_QV(tokenized_map, tokenized_agent, expert_train_mask)
 
         if not self.gail:
-            return expert_nll.mean()+exert_entry_nll.mean()
+            return expert_nll.mean()+exert_entry_nll.mean()+entry_head_nll.mean()
 
         tokenized_agent["train_mask"]=tokenized_agent["pred_mask"] #& expert_train_mask.all(0)
 
