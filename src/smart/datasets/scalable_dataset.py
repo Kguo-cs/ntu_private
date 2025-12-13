ae066cd5aad766532f22845947b51a0ab449e954
Kguo-cs <u3006612@connect.hku.hk>
2025-12-13 18:26:45 +0800

    bird6_auto1024

--- src/smart/datasets/scalable_dataset.py
+++ src/smart/datasets/scalable_dataset.py
@@ -189,7 +189,6 @@ class MultiDataset(Dataset):
         #     with open('./waymo_data/full/validation_map2/'+self.selected_files[idx//device_number], "rb") as handle:
         #         data = pickle.load(handle)
         # else:
-        print(self.raw_paths[idx])
 
         idx = idx // num_gpus
 
