f51492f18034d7793b6e9c335f4c3a55796144f1
Kguo-cs <u3006612@connect.hku.hk>
2025-12-14 11:06:49 +0800

    bird5_auto512_time_dist_head

--- src/smart/tokens/token_bird_processor.py
+++ src/smart/tokens/token_bird_processor.py
@@ -418,7 +418,8 @@ class TokenProcessor(torch.nn.Module):
                     entry_heading = heading[:, i][entry_agent]
                     entry_batch=batch[entry_agent]
 
-                    sort_idx=torch.argsort(torch.linalg.norm(entry_pos,dim=-1))
+                    # sort_idx=torch.argsort(torch.linalg.norm(entry_pos,dim=-1))
+                    sort_idx=torch.argsort(entry_pos[...,0])
 
                     entry_pos = entry_pos[sort_idx]
                     entry_heading=entry_heading[sort_idx]
