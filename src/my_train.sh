92466bbebe4ab23ed76a984b66f3b8253aeee08c
Kguo-cs <u3006612@connect.hku.hk>
2025-12-16 16:40:39 +0800

    bird6_auto512_allpos_detach01_nocross

--- src/my_train.sh
+++ src/my_train.sh
@@ -8,9 +8,12 @@
 #PBS -j oe
 #PBS -M ke.guo@staff.main.ntu.edu.sg
 
-source "/home/users/ntu/shanhelo/miniconda3/bin/activate"
-cd /home/users/ntu/shanhelo/scratch/keguo_projects/sim/src
-conda activate catk
+export CUDA_HOME=/home/users/ntu/lyuchen/scratch/keguo_projects/cuda-12.2
+export PATH=/home/users/ntu/lyuchen/scratch/keguo_projects/cuda12.2/bin:$PATH
+export LD_LIBRARY_PATH=/home/users/ntu/lyuchen/scratch/keguo_projects/cuda12.2/lib64:$LD_LIBRARY_PATH
+source "/home/users/ntu/lyuchen/miniconda3/bin/activate"
+cd /home/users/ntu/lyuchen/scratch/keguo_projects/sim/src
+conda activate sim
 
 #torchrun --nproc_per_node=2  -m run trainer=ddp  > AIRL160_kl3_learnmap.log  2>&1
 python  run.py > AIRL80_noedge_1m_Prevroformer_allvalid_notoken_gp.log  2>&1
