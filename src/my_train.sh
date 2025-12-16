edfb565e55d833a1bf16ad1854a44f70a3d4cc11
Kguo-cs <u3006612@connect.hku.hk>
2025-12-16 16:52:38 +0800

    bird6_auto512_allpos_detach01_nocross

--- src/my_train.sh
+++ src/my_train.sh
@@ -7,8 +7,7 @@
 #PBS -P 12002486
 #PBS -j oe
 #PBS -M ke.guo@staff.main.ntu.edu.sg
-#PBS -l container_image=~/images/nvidia+pytorch+23.10-py3.sqsh
-#PBS -l container_name=nvidia+pytorch+23.10-py3
+#PBS -l container_name=my_container_name
 
 export CUDA_HOME=/home/users/ntu/lyuchen/scratch/keguo_projects/cuda-12.2
 export PATH=/home/users/ntu/lyuchen/scratch/keguo_projects/cuda12.2/bin:$PATH
