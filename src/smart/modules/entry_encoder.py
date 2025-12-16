f18e53734ee3bd4e2e9aa636033df0b4ad6b903c
Kguo-cs <u3006612@connect.hku.hk>
2025-12-16 10:41:24 +0800

    bird6_auto512_offsethead_drop01_allpos_detach

--- src/smart/modules/entry_encoder.py
+++ src/smart/modules/entry_encoder.py
@@ -79,7 +79,7 @@ class EntryDecoder(nn.Module):
                 #     ]
                 # )
 
-            self.attr_former = RoFormerBlock(hidden_dim=hidden_dim, num_heads=num_heads, dropout=0, hist_len=self.entry_his_len)
+            self.attr_former = RoFormerBlock(hidden_dim=hidden_dim, num_heads=num_heads, dropout=0.1, hist_len=self.entry_his_len)
 
 
             self.pos_embedding = nn.Embedding(self.n_token_entry+1, hidden_dim)
