0b2d662bb6820d984626741e32e38d0246edfab8
Kguo-cs <u3006612@connect.hku.hk>
2025-12-16 18:59:58 +0800

    bc40_apt30_exitpred1_14679_token10clone150

--- src/smart/datamodules/target_builder.py
+++ src/smart/datamodules/target_builder.py
@@ -24,15 +24,15 @@ class WaymoTargetBuilderTrain(BaseTransform):
 
     def __call__(self, data) -> HeteroData:
 
-        # if "agent" in data.keys():
-        # #     _,  data["pt_token"]["ln_id"] = torch.unique( data["pt_token"]["ln_id"], return_inverse=True)
+        if "agent" in data.keys() and "role" in data["agent"].keys():
+        #     _,  data["pt_token"]["ln_id"] = torch.unique( data["pt_token"]["ln_id"], return_inverse=True)
+
+            pos = data["agent"]["position"]
+            av_index = torch.where(data["agent"]["role"][:, 0])[0].item()
+            distance = torch.norm(pos - pos[av_index], dim=-1)
         #
-        #     pos = data["agent"]["position"]
-        #     av_index = torch.where(data["agent"]["role"][:, 0])[0].item()
-        #     distance = torch.norm(pos - pos[av_index], dim=-1)
-        # #
-        # #     # we do not believe the perception out of range of 150 meters
-        #     data["agent"]["valid_mask"][:,self.step_current:] = data["agent"]["valid_mask"] [:,self.step_current:]& (distance[:,self.step_current:] < 150)
+        #     # we do not believe the perception out of range of 150 meters
+            data["agent"]["valid_mask"][:,self.step_current:] = data["agent"]["valid_mask"] [:,self.step_current:]& (distance[:,self.step_current:] < 150)
             #data["agent"]["dist_mask"] =distance < 150
         #     # we do not predict vehicle too far away from ego car
         #     role_train_mask = data["agent"]["role"].any(-1)
