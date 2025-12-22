6dbfca77f1e599838b9af5c223b275bdb28206cc
Kguo-cs <u3006612@connect.hku.hk>
2025-12-22 23:07:17 +0800

    bc40_para_typeshapepos05

--- src/smart/modules/agent_token_encoder.py
+++ src/smart/modules/agent_token_encoder.py
@@ -199,7 +199,7 @@ class AgentTokenEncoder(nn.Module):
         # if self.token_processor.use_token:
 
         if token_mask is not None:
-            feature_a[~token_mask]= 0#-10
+            feature_a[~token_mask]= -10
 
         if self.use_goal:
             if goal_pos is not None:
