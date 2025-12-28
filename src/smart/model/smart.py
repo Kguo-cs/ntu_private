be9b7b4ca8e91e7dac82fb637a70e53185fb942e
Kguo-cs <u3006612@connect.hku.hk>
2025-12-28 16:48:10 +0800

    bcgen40_feat2_token0_shape_noexit

--- src/smart/model/smart.py
+++ src/smart/model/smart.py
@@ -351,9 +351,12 @@ class SMART(LightningModule):
                     print('start metric evaluation')
                     time1=time.time()
 
-                    for i in range((len(scenario_rollouts)+1) // 32):  # 64
-                        self.wosac_metrics.update(data["tfrecord_path"][32 * i:32 * (i + 1)],
-                                                  scenario_rollouts[32 * i:32 * (i + 1)])
+                    if len(scenario_rollouts) > 32:
+                        for i in range(len(scenario_rollouts) // 32):  # 64
+                            self.wosac_metrics.update(data["tfrecord_path"][32 * i:32 * (i + 1)],
+                                                      scenario_rollouts[32 * i:32 * (i + 1)])
+                    else:
+                        self.wosac_metrics.update(data["tfrecord_path"],   scenario_rollouts)
 
                     print('end metric evaluation',time.time()-time1)#322.7260935306549/4
 
