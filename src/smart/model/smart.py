74860004570b0b1db4b083c97f28d1fe2d2e2c84
Kguo-cs <u3006612@connect.hku.hk>
2025-12-28 17:05:32 +0800

    bc40_t60_start0_map9

--- src/smart/model/smart.py
+++ src/smart/model/smart.py
@@ -79,7 +79,7 @@ class SMART(LightningModule):
             self.para_num=8
         else:
             self.challenge_type=_ChallengeType.SIM_AGENTS
-            self.para_num=64
+            self.para_num=32
 
         self.minADE = minADE()
         self.TokenCls = TokenCls(max_guesses=5)
@@ -362,7 +362,7 @@ class SMART(LightningModule):
 
                     print('end metric evaluation',time.time()-time1)
                     #322.7260935306549/4  #end metric evaluation 54.91072988510132
-                    #sim32
+                    #sim end metric evaluation para32->124.54043221473694  para64->
 
             # ! visualization
             if self.global_rank == 0 and batch_idx < self.n_vis_batch:
