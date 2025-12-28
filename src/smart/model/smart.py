d78299910bd2bcc974908bc57f4fabe8245def37
Kguo-cs <u3006612@connect.hku.hk>
2025-12-28 14:35:42 +0800

    bcgen40_feat2_token0_shape

--- src/smart/model/smart.py
+++ src/smart/model/smart.py
@@ -348,6 +348,7 @@ class SMART(LightningModule):
                         # scenario_rollouts = scenario_rollouts_from_states(
                         #     scenario, simulated_states, simulated_sizes, agent_id
                         # )
+                    print('start metric evaluation')
 
                     if len(scenario_rollouts) > 32 or self.challenge_type == _ChallengeType.SCENARIO_GEN:
                         self.wosac_metrics.update(data["tfrecord_path"][:len(scenario_rollouts)//2], scenario_rollouts[:len(scenario_rollouts)//2])
