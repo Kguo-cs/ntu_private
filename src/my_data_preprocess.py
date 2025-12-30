79da7dcd1a9ae80f35e8b32fc7e304828af6f22a
Kguo-cs <u3006612@connect.hku.hk>
2025-12-30 10:06:19 +0800

    bc40_t60_val1284

--- src/my_data_preprocess.py
+++ src/my_data_preprocess.py
@@ -200,7 +200,7 @@ def batch_process9s_transformer(input_dir, output_dir, split, num_workers):
     output_dir.mkdir(exist_ok=True, parents=True)
 
     input_dir = Path(input_dir) / split
-    packages = sorted([p.as_posix() for p in input_dir.glob("*")])
+    packages = sorted([p.as_posix() for p in input_dir.glob("*")])[750:]
     func = partial(
         wm2argo,
         split=split,
@@ -208,11 +208,11 @@ def batch_process9s_transformer(input_dir, output_dir, split, num_workers):
         output_dir_tfrecords_splitted=output_dir_tfrecords_splitted,
     )
 
-    # with multiprocessing.Pool(num_workers) as p:
-    #     r = list(tqdm(p.imap_unordered(func, packages), total=len(packages)))
+    with multiprocessing.Pool(num_workers) as p:
+        r = list(tqdm(p.imap_unordered(func, packages), total=len(packages)))
     # print(len(packages))
-    for file_path in tqdm(packages):
-        wm2argo(file_path, split, output_dir, output_dir_tfrecords_splitted)
+    # for file_path in tqdm(packages):
+    #     wm2argo(file_path, split, output_dir, output_dir_tfrecords_splitted)
 
 if __name__ == "__main__":
     parser = ArgumentParser()
