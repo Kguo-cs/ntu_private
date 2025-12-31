18d3119b74819f6e5dd9ab11c05592e3bb1132c6
Kguo-cs <u3006612@connect.hku.hk>
2025-12-31 17:55:54 +0800

    bcgen56_gtsim648

--- src/smart/tokens/token_processor.py
+++ src/smart/tokens/token_processor.py
@@ -189,26 +189,31 @@ class TokenProcessor(torch.nn.Module):
                 initial_heading = tokenized_agent["sampled_heading"][:, 0]
 
             else:
-                valid = data["agent"]["valid_mask"]  # [n_agent, n_step]
-                heading = data["agent"]["heading"]  # [n_agent, n_step]
-                pos = data["agent"]["position"][..., :2].contiguous()  # [n_agent, n_step, 2]
-                vel = data["agent"]["velocity"]  # [n_agent, n_step, 2]
-
-                first_valid_step = valid.float().argmax(dim=1)  # [n_agent]
-
-                agent_idx = torch.arange(valid.shape[0], device=valid.device)
-
-                tokenized_agent["extra_heading"] = heading[agent_idx, first_valid_step]
-                initial_vel = vel[agent_idx, first_valid_step]
-                initial_pos = pos[agent_idx, first_valid_step]
-
-                dt = 0.1
-                tokenized_agent["extra_pos"] = initial_pos - initial_vel * first_valid_step.unsqueeze(-1) * dt
+                # valid = data["agent"]["valid_mask"]  # [n_agent, n_step]
+                # heading = data["agent"]["heading"]  # [n_agent, n_step]
+                # pos = data["agent"]["position"][..., :2].contiguous()  # [n_agent, n_step, 2]
+                # vel = data["agent"]["velocity"]  # [n_agent, n_step, 2]
+                #
+                # first_valid_step = valid.float().argmax(dim=1)  # [n_agent]
+                #
+                # agent_idx = torch.arange(valid.shape[0], device=valid.device)
+                #
+                # tokenized_agent["extra_heading"] = heading[agent_idx, first_valid_step]
+                # initial_vel = vel[agent_idx, first_valid_step]
+                # initial_pos = pos[agent_idx, first_valid_step]
+                #
+                # dt = 0.1
+                # tokenized_agent["extra_pos"] = initial_pos - initial_vel * first_valid_step.unsqueeze(-1) * dt
                 initial_pos = tokenized_agent["sampled_pos"][:, 1]
                 initial_heading = tokenized_agent["sampled_heading"][:, 1]
 
-                # tokenized_agent["extra_pos"]=initial_pos
-                # tokenized_agent["extra_heading"]=initial_heading
+                for key in ["sampled_idx","token_mask","valid_mask","sampled_pos","sampled_heading"]:
+                    tokenized_agent[key]=tokenized_agent[key][:,2:]
+
+
+
+                tokenized_agent["extra_pos"]=initial_pos
+                tokenized_agent["extra_heading"]=initial_heading
 
 
             shape=tokenized_agent["shape"]
@@ -579,7 +584,7 @@ class TokenProcessor(torch.nn.Module):
         entry_shape_list=[]
 
         if not self.training:
-            n_step=11
+            n_step=11+10
 
         if self.pred_entry and not self.autoregressive_entry:
             out_dict["entry_idx"] = []
