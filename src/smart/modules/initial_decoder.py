6a1ef04498be975f4ba45bae2b355cdf17dff46b
Kguo-cs <u3006612@connect.hku.hk>
2026-01-01 16:38:59 +0800

    bcgen256_graph_alllocal

--- src/smart/modules/initial_decoder.py
+++ src/smart/modules/initial_decoder.py
@@ -47,7 +47,7 @@ class InitDecoder(nn.Module):
 
         self.use_cross_attention = True
 
-        self.use_entry_former = False
+        self.use_entry_former = True
 
         if  self.use_cross_attention:
 
