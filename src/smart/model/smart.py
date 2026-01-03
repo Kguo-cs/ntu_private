6d06ac8f164dbb0b508e4c3592541087dd605e39
Kguo-cs <u3006612@connect.hku.hk>
2026-01-03 14:38:24 +0800

    bcgen256_tokenoffsetshape_typecount_randsort

--- src/smart/model/smart.py
+++ src/smart/model/smart.py
@@ -91,7 +91,7 @@ class SMART(LightningModule):
         if self.token_processor.pred_init:
             self.challenge_type=ChallengeType.SCENARIO_GEN
             self.para_num=4
-            self.n_rollout_closed_val=2
+            self.n_rollout_closed_val=4
         else:
             self.challenge_type=ChallengeType.SIM_AGENTS
             self.para_num=32
