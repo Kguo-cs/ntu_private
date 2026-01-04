10a864f0aefb187ae0514f847413294702c99e36
Kguo-cs <u3006612@connect.hku.hk>
2026-01-04 22:25:17 +0800

    bcgen128_sort08_map100_max_01loss_nopos0

--- src/smart/modules/initial_decoder.py
+++ src/smart/modules/initial_decoder.py
@@ -104,7 +104,7 @@ class InitDecoder(nn.Module):
                     ]
                 )
 
-        self.attr_former = RoFormerBlock(hidden_dim=hidden_dim, num_heads=num_heads, dropout=0.4,
+        self.attr_former = RoFormerBlock(hidden_dim=hidden_dim, num_heads=num_heads, dropout=0.8,
                                          hist_len=self.entry_his_len)        # drop 01 is important
 
         self.use_refine=False
