45ea735f0ed1fb65f2d4bb6bcb2f3f72b221cbbe
Kguo-cs <u3006612@connect.hku.hk>
2026-01-05 10:27:58 +0800

    bcgen56_together_notoken

--- src/smart/tokens/token_processor.py
+++ src/smart/tokens/token_processor.py
@@ -584,13 +584,13 @@ class TokenProcessor(torch.nn.Module):
             pos=pos[:,10:]
             heading=heading[:,10:]
 
-            token_initial_pos, token_initial_heading,pos_token_idx,heading_token_idx,offset_idx,initial_pos,initial_heading=self.tokenize_initial(pos[:,0],heading[:,0],ego_mask,batch)
-
-            # print(torch.linalg.norm(initial_pos - pos[:, :1],dim=-1).max())
-            # print(wrap_angle(heading[:,:1]-initial_heading).max())
-
-            pos[:, :1]=initial_pos
-            heading[:, :1]=initial_heading
+            # token_initial_pos, token_initial_heading,pos_token_idx,heading_token_idx,offset_idx,initial_pos,initial_heading=self.tokenize_initial(pos[:,0],heading[:,0],ego_mask,batch)
+            #
+            # # print(torch.linalg.norm(initial_pos - pos[:, :1],dim=-1).max())
+            # # print(wrap_angle(heading[:,:1]-initial_heading).max())
+            #
+            # pos[:, :1]=initial_pos
+            # heading[:, :1]=initial_heading
 
             tokenized_agent["initial_pos"]=pos[:,0]
             tokenized_agent["initial_heading"]=heading[:,0]
