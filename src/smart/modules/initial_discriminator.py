27dc112213c324cb623c063edd254a97c67c3bf0
Kguo-cs <u3006612@connect.hku.hk>
2026-01-07 17:13:54 +0800

    gen256_realgan2_match100_trans_value80

--- src/smart/modules/initial_discriminator.py
+++ src/smart/modules/initial_discriminator.py
@@ -7,9 +7,9 @@ from src.smart.layers.relative_transformer import RoFormerBlock, padding
 from torch.distributions import Categorical
 
 from src.smart.layers import MLPLayer
-from src.smart.layers.attention_layer import AttentionLayer,CacheAttention
-from src.smart.modules.edge_encoder import EdgeEncoder,topo_rank_among_edges
-from torch_scatter import scatter_max,scatter_mean,scatter_sum
+from src.smart.layers.attention_layer import AttentionLayer, CacheAttention
+from src.smart.modules.edge_encoder import EdgeEncoder, topo_rank_among_edges
+from torch_scatter import scatter_max, scatter_mean, scatter_sum
 from src.smart.layers.relative_transformer import RoFormerBlock
 from src.smart.layers.fourier_embedding import FourierEmbedding, MLPEmbedding
 from src.smart.utils import (
@@ -30,9 +30,9 @@ class InitDiscriminator(nn.Module):
     ) -> None:
 
         super(InitDiscriminator, self).__init__()
-        self.token_processor=token_processor
+        self.token_processor = token_processor
 
-        self.hidden_dim=hidden_dim
+        self.hidden_dim = hidden_dim
 
         self.use_entry_former = True
 
@@ -43,7 +43,7 @@ class InitDiscriminator(nn.Module):
                                               hist_len=self.entry_his_len)  # replace with gnn
 
             self.attr_former = RoFormerBlock(hidden_dim=hidden_dim, num_heads=num_heads, dropout=0.2,
-                                             hist_len=self.entry_his_len)        # drop 01 is important
+                                             hist_len=self.entry_his_len)  # drop 01 is important
 
         else:
             self.edge_encoder = EdgeEncoder(hidden_dim,
@@ -90,16 +90,15 @@ class InitDiscriminator(nn.Module):
                 ]
             )
 
-
         self.shape_embedding = MLPLayer(3, hidden_dim, hidden_dim)
         self.type_embedding = nn.Embedding(3, hidden_dim)
         self.pos_embedding = MLPLayer(2, hidden_dim, hidden_dim)
         self.head_embedding = MLPLayer(1, hidden_dim, hidden_dim)
 
-        self.score_decoder = MLPLayer(hidden_dim, hidden_dim, 1 )
+        self.score_decoder = MLPLayer(hidden_dim, hidden_dim, 1)
 
-    def padding(self,pos,heading,feature,batch,batch_num):
-        lengths = torch.bincount(batch,minlength=batch_num).tolist()
+    def padding(self, pos, heading, feature, batch, batch_num):
+        lengths = torch.bincount(batch, minlength=batch_num).tolist()
 
         padding_pos_a = padding(pos, lengths, padding_value=0)  # b, n, d
         padding_heading_a = padding(heading, lengths, padding_value=0)  # b, n, d
@@ -107,35 +106,33 @@ class InitDiscriminator(nn.Module):
 
         return padding_pos_a, padding_heading_a, padding_features_a
 
-    def embed_input(self,initial_pos, initial_heading,initial_type,initial_shape,batch,batch_num ):
+    def embed_input(self, initial_pos, initial_heading, initial_type, initial_shape, batch, batch_num):
         type_embedding = self.type_embedding(initial_type)
         pos_embedding = self.pos_embedding(initial_pos)
-        heading_embedding = self.head_embedding(initial_heading[:,None])
+        heading_embedding = self.head_embedding(initial_heading[:, None])
         shape_embedding = self.shape_embedding(initial_shape)
 
-        feat_a = type_embedding  + heading_embedding + pos_embedding+ shape_embedding
+        feat_a = type_embedding + heading_embedding + pos_embedding + shape_embedding
 
-        pos_a_b, heading_a_b, feat_a_b = self.padding(initial_pos, initial_heading, feat_a, batch,batch_num)
+        pos_a_b, heading_a_b, feat_a_b = self.padding(initial_pos, initial_heading, feat_a, batch, batch_num)
 
         mask_a_b = torch.any(feat_a_b != 0, dim=-1)
 
-        return pos_a_b, heading_a_b, feat_a_b,mask_a_b
+        return pos_a_b, heading_a_b, feat_a_b, mask_a_b
 
+    def forward(self, map_feature, pos_a, head_a, shape, tokenized_agent):
 
-    def forward(self,map_feature, pos_a, head_a, shape,tokenized_agent ):
+        ego_mask = tokenized_agent["ego_mask"]
 
+        non_ego = ~ego_mask
 
-        ego_mask=tokenized_agent["ego_mask"]
-
-        non_ego=~ego_mask
-
-        batch=tokenized_agent["batch"][non_ego]
+        batch = tokenized_agent["batch"][non_ego]
         type = tokenized_agent["type"][non_ego]
-        batch_num=tokenized_agent["num_graphs"]
+        batch_num = tokenized_agent["num_graphs"]
         head_a = wrap_angle(head_a)
 
         if self.use_entry_former:
-            pos_pl, orient_pl, feat_map, map_mask=map_feature
+            pos_pl, orient_pl, feat_map, map_mask = map_feature
 
             pos_a_b, heading_a_b, feat_a_b, mask_a_b = self.embed_input(pos_a, head_a, type, shape, batch, batch_num)
 
@@ -145,16 +142,16 @@ class InitDiscriminator(nn.Module):
                                                               pos_pl,
                                                               orient_pl, map_mask)
 
-            attr_feature = self.attr_former.temporal_embed(entry_feature, pos_a_b, heading_a_b, 0, 0,  mask_a_b,use_time=False)
+            attr_feature = self.attr_former.temporal_embed(entry_feature, pos_a_b, heading_a_b, 0, 0, mask_a_b,
+                                                           use_time=False)
 
             attr_feature = attr_feature[mask_a_b]
         else:
             mask_a = None
 
-            pos_pl, orient_pl, batch_pl, feat_map=map_feature
+            pos_pl, orient_pl, batch_pl, feat_map = map_feature
 
             head_vector_a = torch.stack([head_a.cos(), head_a.sin()], dim=-1)
-
 
             edge_index_pl2a, r_pl2a = self.edge_encoder.build_map2agent_edge(
                 pos_pl=pos_pl,  # [n_pl, 2]
@@ -190,12 +187,12 @@ class InitDiscriminator(nn.Module):
             type_embedding = self.type_embedding(type)
             shape_embedding = self.shape_embedding(shape)
 
-            feat_a = type_embedding +  shape_embedding+pos_embedding+heading_embedding
+            feat_a = type_embedding + shape_embedding + pos_embedding + heading_embedding
 
             feat_a = self.a2a_attn_layers[0](feat_a, r_a2a, edge_index_a2a)
 
             attr_feature = self.pt2a_attn_layers[0]((feat_map, feat_a), r_pl2a,
-                                              edge_index_pl2a)  # edge_index_pl2a[0] is the src, edge_index_pl2a[1] is dst
+                                                    edge_index_pl2a)  # edge_index_pl2a[0] is the src, edge_index_pl2a[1] is dst
 
         score = self.score_decoder(attr_feature)
 
@@ -210,32 +207,31 @@ class InitGeneator(nn.Module):
             num_freq_bands,
             token_processor,
     ) -> None:
-
         super(InitGeneator, self).__init__()
-        self.token_processor=token_processor
+        self.token_processor = token_processor
 
-        self.hidden_dim=hidden_dim
+        self.hidden_dim = hidden_dim
 
         self.entry_his_len = 1000000
 
-        self.entry_former = RoFormerBlock(hidden_dim=hidden_dim, num_heads=num_heads, dropout=0,
-                                          hist_len=self.entry_his_len)  # replace with gnn
-
-        self.attr_former = RoFormerBlock(hidden_dim=hidden_dim, num_heads=num_heads, dropout=0,
-                                         hist_len=self.entry_his_len)        # drop 01 is important
-
-        # decoder_layer = nn.TransformerDecoderLayer(
-        #     d_model=self.hidden_dim,
-        #     nhead=num_heads,
-        #     dim_feedforward=self.hidden_dim*4,
-        #     dropout=0,
-        #     batch_first=True  # nn.Transformer uses (seq_len, batch, dim)
-        # )
+        # self.entry_former = RoFormerBlock(hidden_dim=hidden_dim, num_heads=num_heads, dropout=0,
+        #                                   hist_len=self.entry_his_len)  # replace with gnn
         #
-        # self.transformer_decoder = nn.TransformerDecoder(
-        #     decoder_layer,
-        #     num_layers=1
-        # )
+        # self.attr_former = RoFormerBlock(hidden_dim=hidden_dim, num_heads=num_heads, dropout=0,
+        #                                  hist_len=self.entry_his_len)  # drop 01 is important
+
+        decoder_layer = nn.TransformerDecoderLayer(
+            d_model=self.hidden_dim,
+            nhead=num_heads,
+            dim_feedforward=self.hidden_dim*4,
+            dropout=0,
+            batch_first=True  # nn.Transformer uses (seq_len, batch, dim)
+        )
+
+        self.transformer_decoder = nn.TransformerDecoder(
+            decoder_layer,
+            num_layers=1
+        )
 
         # self.entry_former1 = RoFormerBlock(hidden_dim=hidden_dim, num_heads=num_heads, dropout=0,
         #                                   hist_len=self.entry_his_len)  # replace with gnn
@@ -243,83 +239,80 @@ class InitGeneator(nn.Module):
         # self.attr_former1 = RoFormerBlock(hidden_dim=hidden_dim, num_heads=num_heads, dropout=0,
         #                                  hist_len=self.entry_his_len)        # drop 01 is important
 
-        self.noise_embedding = MLPLayer(6 ,hidden_dim, hidden_dim)
+        self.noise_embedding = MLPLayer(6, hidden_dim, hidden_dim)
         self.type_embedding = nn.Embedding(3, hidden_dim)
 
         self.pos_embedding = MLPLayer(2, hidden_dim, hidden_dim)
         self.head_embedding = MLPLayer(1, hidden_dim, hidden_dim)
 
-        self.count_embedding=MLPLayer(1, hidden_dim, hidden_dim)
+        self.count_embedding = MLPLayer(1, hidden_dim, hidden_dim)
 
-        self.pos_decoder = MLPLayer(hidden_dim, hidden_dim, 2 )
-        self.head_decoder = MLPLayer(hidden_dim, hidden_dim,1 )
-        self.shape_head_decoder = nn.Sequential( MLPLayer(hidden_dim, hidden_dim, 3),nn.ReLU())
+        self.pos_decoder = MLPLayer(hidden_dim, hidden_dim, 2)
+        self.head_decoder = MLPLayer(hidden_dim, hidden_dim, 1)
+        self.shape_head_decoder = nn.Sequential(MLPLayer(hidden_dim, hidden_dim, 3), nn.ReLU())
 
-    def forward(self,map_features, tokenized_agent):
-        pos_pl, orient_pl, feat_map, map_mask=map_features
+    def forward(self, map_features, tokenized_agent):
+        pos_pl, orient_pl, feat_map, map_mask = map_features
 
-        ego_mask=tokenized_agent["ego_mask"]
+        ego_mask = tokenized_agent["ego_mask"]
 
         type = tokenized_agent["type"]
 
-        batch=tokenized_agent["batch"]
+        batch = tokenized_agent["batch"]
 
-        batch_num=tokenized_agent["num_graphs"]
+        batch_num = tokenized_agent["num_graphs"]
 
-        type=type[~ego_mask]
+        type = type[~ego_mask]
 
-        batch=batch[~ego_mask]
+        batch = batch[~ego_mask]
 
-        agent_num=len(type)
+        agent_num = len(type)
 
-        z = torch.rand(agent_num, 6, device=type.device)#pos,heading and shape
+        z = torch.rand(agent_num, 6, device=type.device)  #pos,heading and shape
 
-        lengths = torch.bincount(batch,minlength=batch_num).tolist()
+        lengths = torch.bincount(batch, minlength=batch_num).tolist()
 
         padding_type = padding(type, lengths, padding_value=3)
 
         mask_a_b = padding_type != 3
 
-        count=torch.arange(padding_type.shape[1],device=type.device)[None].repeat(padding_type.shape[0],1)
+        count = torch.arange(padding_type.shape[1], device=type.device)[None].repeat(padding_type.shape[0], 1)
 
-        sort_idx=torch.argsort(padding_type,dim=-1)
+        sort_idx = torch.argsort(padding_type, dim=-1)
 
         value = torch.zeros_like(count)
 
         # Scatter count into value at sort_idx positions (row-wise)
         value.scatter_(dim=1, index=sort_idx, src=count)
 
-        value=value[mask_a_b]
+        value = value[mask_a_b]
 
-        feature =self.noise_embedding(z)+self.type_embedding(type)+self.count_embedding(value[:,None].to(z.dtype))
-
+        feature = self.noise_embedding(z) + self.type_embedding(type) + self.count_embedding(value[:, None].to(z.dtype))
 
         feat_a_b = padding(feature, lengths, padding_value=0)  # b, n, d
 
 
-        pos_a_b=torch.zeros(feat_a_b.shape[0],feat_a_b.shape[1], 2, device=type.device)
-        heading_a_b=torch.zeros(feat_a_b.shape[0],feat_a_b.shape[1],  device=type.device)
-        n_agent = feat_a_b.shape[1]
+        feat_map = feat_map + self.pos_embedding(pos_pl) + self.head_embedding(orient_pl[:, :, None])
 
-        feat_map=feat_map+self.pos_embedding(pos_pl)+self.head_embedding(orient_pl[:,:,None])
+        entry_feature = self.transformer_decoder(
+            tgt=feat_a_b,  # self-attention queries
+            memory=feat_map,  # cross-attention keys/values
+            tgt_key_padding_mask=~mask_a_b,
+            memory_key_padding_mask=~map_mask
+        )
 
-        # entry_feature = self.transformer_decoder(
-        #     tgt=feat_a_b,  # self-attention queries
-        #     memory=feat_map,  # cross-attention keys/values
-        #     tgt_key_padding_mask=~mask_a_b,
-        #     memory_key_padding_mask=~map_mask
-        # )
         # print(torch.max(entry_feature))
-
-        entry_feature = self.entry_former.cross_attention(feat_a_b, pos_a_b,
-                                                          heading_a_b, mask_a_b,
-                                                          feat_map,
-                                                          pos_pl,
-                                                          orient_pl, map_mask)
-
-
-        entry_feature = self.attr_former.temporal_embed(entry_feature, pos_a_b, heading_a_b, n_agent, 0,  mask_a_b,use_time=False,use_causal=False)#
-
+        # pos_a_b = torch.zeros(feat_a_b.shape[0], feat_a_b.shape[1], 2, device=type.device)
+        # heading_a_b = torch.zeros(feat_a_b.shape[0], feat_a_b.shape[1], device=type.device)
+        # n_agent = feat_a_b.shape[1]
+        # entry_feature = self.entry_former.cross_attention(feat_a_b, pos_a_b,
+        #                                                   heading_a_b, mask_a_b,
+        #                                                   feat_map,
+        #                                                   pos_pl,
+        #                                                   orient_pl, map_mask)
+        #
+        # entry_feature = self.attr_former.temporal_embed(entry_feature, pos_a_b, heading_a_b, n_agent, 0, mask_a_b,
+        #                                                 use_time=False, use_causal=False)  #
 
         # entry_feature = self.entry_former1.cross_attention(entry_feature, pos_a_b,
         #                                                   heading_a_b, mask_a_b,
@@ -332,10 +325,10 @@ class InitGeneator(nn.Module):
 
         attr_feature = entry_feature[mask_a_b]
 
-        pos = torch.tanh(self.pos_decoder(attr_feature))*80
+        pos = torch.tanh(self.pos_decoder(attr_feature)) * 80
 
-        heading = self.head_decoder(attr_feature)[:,0]
+        heading = self.head_decoder(attr_feature)[:, 0]
 
         shape = self.shape_head_decoder(attr_feature)
 
-        return pos,heading,shape
+        return pos, heading, shape
