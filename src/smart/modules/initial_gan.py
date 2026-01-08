6726ee6ef38cc94a832b46f78821579b4613cabc
Kguo-cs <u3006612@connect.hku.hk>
2026-01-08 20:22:23 +0800

    gen128_decompmean

--- src/smart/modules/initial_gan.py
+++ src/smart/modules/initial_gan.py
@@ -220,33 +220,33 @@ class InitGAN(nn.Module):
                 self.D.train()
                 #loss=torch.tensor(0.0, device=real_heading.device)
 
-                rows, cols = [], []
-                initial_type = tokenized_agent["type"][non_ego]
+                # rows, cols = [], []
+                # initial_type = tokenized_agent["type"][non_ego]
+                #
+                # for b in batch.unique():
+                #     for type in initial_type[batch == b].unique():
+                #         f_idx = ((batch == b) & (initial_type==type)).nonzero(as_tuple=True)[0]
+                #
+                #         dist = torch.cdist(fake_pos[f_idx], real_pos[f_idx])
+                #
+                #         cost = dist.cpu().detach().numpy()
+                #
+                #         row, col = linear_sum_assignment(cost)
+                #
+                #         rows.append(f_idx[row])
+                #         cols.append(f_idx[col])
+                #
+                # row = torch.cat(rows)
+                # col = torch.cat(cols)
+                #
+                # match_loss,pos_loss,heading_loss,shape_loss = matching_loss(
+                #     fake_pos[row], fake_heading[row], fake_shape[row],
+                #     real_pos[col], real_heading[col], real_shape[col]
+                # )
+                #
+                # match_loss=(0.1+(1-self.global_step/10000.0))*match_loss
 
-                for b in batch.unique():
-                    for type in initial_type[batch == b].unique():
-                        f_idx = ((batch == b) & (initial_type==type)).nonzero(as_tuple=True)[0]
-
-                        dist = torch.cdist(fake_pos[f_idx], real_pos[f_idx])
-
-                        cost = dist.cpu().detach().numpy()
-
-                        row, col = linear_sum_assignment(cost)
-
-                        rows.append(f_idx[row])
-                        cols.append(f_idx[col])
-
-                row = torch.cat(rows)
-                col = torch.cat(cols)
-
-                match_loss,pos_loss,heading_loss,shape_loss = matching_loss(
-                    fake_pos[row], fake_heading[row], fake_shape[row],
-                    real_pos[col], real_heading[col], real_shape[col]
-                )
-
-                match_loss=(0.1+(1-self.global_step/10000.0))*match_loss
-
-                #match_loss=pos_loss=heading_loss=shape_loss=torch.tensor(0.0, device=real_heading.device)
+                match_loss=pos_loss=heading_loss=shape_loss=torch.tensor(0.0, device=real_heading.device)
 
                 loss=(loss,match_loss,pos_loss,heading_loss,shape_loss)
 
