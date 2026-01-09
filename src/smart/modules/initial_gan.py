6282791ffb02c497f7b89183bfbd7cdcfd678f67
Kguo-cs <u3006612@connect.hku.hk>
2026-01-09 15:03:11 +0800

    gen128_gpgenr12_graph_match02

--- src/smart/modules/initial_gan.py
+++ src/smart/modules/initial_gan.py
@@ -26,7 +26,7 @@ import torch.nn.functional as F
 def matching_loss(
     fake_pos, fake_heading, fake_shape,
     real_pos, real_heading, real_shape,
-    w_pos=0.1, w_heading=0.5, w_shape=0.5
+    w_pos=0.1, w_heading=0.5, w_shape=0.2
 ):
     # Position: L1 or L2
 
