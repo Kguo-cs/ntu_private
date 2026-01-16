54a5c86d6e02aed7a7baa990b194f8c0c26281e0
Kguo-cs <u3006612@connect.hku.hk>
2026-01-16 20:03:30 +0800

    gen240_val256_latentnorm_kl02

--- src/smart/my_model/dit.py
+++ src/smart/my_model/dit.py
@@ -10,50 +10,58 @@ class DiT(nn.Module):
 
     def __init__(self, cfg):
         super(DiT, self).__init__()
-        self.cfg = cfg
-        self.cfg_model = self.cfg.model
-        self.cfg_dataset = self.cfg.dataset
+        # self.cfg = cfg
+        # self.cfg_model = self.cfg.model
+        # self.cfg_dataset = self.cfg.dataset
+        hidden_dim=128
+        self.agent_hidden_dim=hidden_dim
+        self.dropout=0.1
+        self.num_heads=8
+        self.agent_num_heads=8
+        self.num_l2l_blocks=1
+        self.num_factorized_dit_blocks=1
+        
 
-        self.emb_drop = nn.Dropout(self.cfg_model.dropout)
-        # Condition on scene type
-        self.scene_type_embedder = LabelEmbedder(self.cfg_dataset.num_map_ids * 2, self.cfg_model.hidden_dim,
-                                                 self.cfg_model.label_dropout)
+        self.emb_drop = nn.Dropout(self.dropout)
+        # # Condition on scene type
+        # self.scene_type_embedder = LabelEmbedder(self.cfg_dataset.num_map_ids * 2, hidden_dim,
+        #                                          self.cfg_model.label_dropout)
 
         # Condition on number of agents and lanes
-        self.num_agents_embedder = LabelEmbedder(self.cfg_dataset.max_num_agents + 1, self.cfg_model.hidden_dim, 0)
-        self.num_lanes_embedder = LabelEmbedder(self.cfg_dataset.max_num_lanes + 1, self.cfg_model.hidden_dim, 0)
+        # self.num_agents_embedder = LabelEmbedder(self.cfg_dataset.max_num_agents + 1, hidden_dim, 0)
+        # self.num_lanes_embedder = LabelEmbedder(self.cfg_dataset.max_num_lanes + 1, hidden_dim, 0)
 
         # Diffusion timestep embedding
-        self.t_embedder = TimestepEmbedder(self.cfg_model.hidden_dim)
+        self.t_embedder = TimestepEmbedder(hidden_dim)
         # Used because agent embedding is smaller than lane embedding
-        self.downsample_c = nn.Linear(self.cfg_model.hidden_dim, self.cfg_model.agent_hidden_dim)
+        # self.downsample_c = nn.Linear(hidden_dim, self.agent_hidden_dim)
 
         # Embed agent and lane latents
-        self.lane_embedder = TwoLayerResMLP(self.cfg_model.lane_latent_dim, self.cfg_model.hidden_dim)
-        self.agent_embedder = TwoLayerResMLP(self.cfg_model.agent_latent_dim, self.cfg_model.agent_hidden_dim)
+        # self.lane_embedder = TwoLayerResMLP(self.cfg_model.lane_latent_dim, hidden_dim)
+        self.agent_embedder = TwoLayerResMLP(self.agent_latent_dim, self.agent_hidden_dim)
 
-        # These will be overwritten by sin/cos positional encodings
-        self.pos_emb_lane = nn.Parameter(torch.zeros(self.cfg_dataset.max_num_lanes, self.cfg_model.hidden_dim),
-                                         requires_grad=False)
-        self.pos_emb_agent = nn.Parameter(torch.zeros(self.cfg_dataset.max_num_agents, self.cfg_model.agent_hidden_dim),
-                                          requires_grad=False)
+        # # These will be overwritten by sin/cos positional encodings
+        # self.pos_emb_lane = nn.Parameter(torch.zeros(self.cfg_dataset.max_num_lanes, hidden_dim),
+        #                                  requires_grad=False)
+        # self.pos_emb_agent = nn.Parameter(torch.zeros(self.cfg_dataset.max_num_agents, self.agent_hidden_dim),
+        #                                   requires_grad=False)
 
         # factorized dit blocks
         self.blocks = nn.ModuleList([
             FactorizedDiTBlock(
-                self.cfg_model.hidden_dim,
-                self.cfg_model.agent_hidden_dim,
-                self.cfg_model.num_heads,
-                self.cfg_model.agent_num_heads,
-                self.cfg_model.dropout,
+                hidden_dim,
+                self.agent_hidden_dim,
+                self.num_heads,
+                self.agent_num_heads,
+                self.dropout,
                 mlp_ratio=4,
-                num_l2l_blocks=self.cfg_model.num_l2l_blocks
-            ) for _ in range(self.cfg_model.num_factorized_dit_blocks)
+                num_l2l_blocks=self.num_l2l_blocks
+            ) for _ in range(self.num_factorized_dit_blocks)
         ])
 
         # noise prediction heads
-        self.pred_agent_noise = FinalLayer(self.cfg_model.agent_hidden_dim, self.cfg_model.agent_latent_dim)
-        self.pred_lane_noise = FinalLayer(self.cfg_model.hidden_dim, self.cfg_model.lane_latent_dim)
+        self.pred_agent_noise = FinalLayer(self.agent_hidden_dim, self.agent_latent_dim)
+        # self.pred_lane_noise = FinalLayer(hidden_dim, self.cfg_model.lane_latent_dim)
         self.initialize_weights()
 
     def initialize_weights(self):
@@ -69,20 +77,20 @@ class DiT(nn.Module):
         self.apply(_basic_init)
 
         # Initialize (and freeze) lane and agent pos_embed by sin-cos embedding:
-        pos_emb_lane = get_1d_sincos_pos_embed_from_grid(self.pos_emb_lane.shape[-1],
-                                                         np.arange(self.pos_emb_lane.shape[0]))
-        self.pos_emb_lane.data.copy_(torch.from_numpy(pos_emb_lane).float())
-        pos_emb_agent = get_1d_sincos_pos_embed_from_grid(self.pos_emb_agent.shape[-1],
-                                                          self.cfg_dataset.max_num_lanes + np.arange(
-                                                              self.pos_emb_agent.shape[0]))
-        self.pos_emb_agent.data.copy_(torch.from_numpy(pos_emb_agent).float())
+        # pos_emb_lane = get_1d_sincos_pos_embed_from_grid(self.pos_emb_lane.shape[-1],
+        #                                                  np.arange(self.pos_emb_lane.shape[0]))
+        # self.pos_emb_lane.data.copy_(torch.from_numpy(pos_emb_lane).float())
+        # pos_emb_agent = get_1d_sincos_pos_embed_from_grid(self.pos_emb_agent.shape[-1],
+        #                                                   self.cfg_dataset.max_num_lanes + np.arange(
+        #                                                       self.pos_emb_agent.shape[0]))
+        # self.pos_emb_agent.data.copy_(torch.from_numpy(pos_emb_agent).float())
 
         # Initialize label embedding table:
-        nn.init.normal_(self.scene_type_embedder.embedding_table.weight, std=0.02)
+        # nn.init.normal_(self.scene_type_embedder.embedding_table.weight, std=0.02)
 
         # Initialize num lane and num agent embedding tables:
-        nn.init.normal_(self.num_agents_embedder.embedding_table.weight, std=0.02)
-        nn.init.normal_(self.num_lanes_embedder.embedding_table.weight, std=0.02)
+        # nn.init.normal_(self.num_agents_embedder.embedding_table.weight, std=0.02)
+        # nn.init.normal_(self.num_lanes_embedder.embedding_table.weight, std=0.02)
 
         # Initialize timestep embedding MLP:
         nn.init.normal_(self.t_embedder.mlp[0].weight, std=0.02)
@@ -124,9 +132,9 @@ class DiT(nn.Module):
         agent_idx_batch = get_indices_within_scene(data['agent'].batch)
 
         # add positional embeddings
-        pos_emb_lane = self.pos_emb_lane[lane_idx_batch]
+        #pos_emb_lane = self.pos_emb_lane[lane_idx_batch]
         pos_emb_agent = self.pos_emb_agent[agent_idx_batch]
-        x_lane = self.lane_embedder(x_lane[:, 0]) + pos_emb_lane
+        # x_lane = self.lane_embedder(x_lane[:, 0]) + pos_emb_lane
         x_agent = self.agent_embedder(x_agent[:, 0]) + pos_emb_agent
 
         scene_idx = self.cfg_dataset.num_map_ids * data['lg_type'].long() + data['map_id'].long()
@@ -176,9 +184,9 @@ class DiT(nn.Module):
                 l2a_edge_index)
 
         # decode the noise as in the original DiT paper
-        c_lane = c[:x_lane.shape[0]]
+        #c_lane = c[:x_lane.shape[0]]
         c_agent = c_small[x_lane.shape[0]:]
-        x_lane = self.pred_lane_noise(x_lane, c_lane).unsqueeze(1)
+        #x_lane = self.pred_lane_noise(x_lane, c_lane).unsqueeze(1)
         x_agent = self.pred_agent_noise(x_agent, c_agent).unsqueeze(1)
 
         return x_agent, x_lane
