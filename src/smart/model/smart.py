25d282c86cb6404604650fc61f9cc22b9c2f92a7
Kguo-cs <u3006612@connect.hku.hk>
2026-01-18 22:01:25 +0800

    driectxflow128_step100_t0

--- src/smart/model/smart.py
+++ src/smart/model/smart.py
@@ -397,6 +397,8 @@ class SMART(LightningModule):
                             for i in range(len(scenario_rollouts) // self.para_num):  # 64
                                 self.wosac_metrics.update(data["tfrecord_path"][self.para_num * i:self.para_num * (i + 1)],
                                                           scenario_rollouts[self.para_num * i:self.para_num * (i + 1)])
+                                if i==7:
+                                    break
                         else:
                             self.wosac_metrics.update(data["tfrecord_path"],   scenario_rollouts)
 
