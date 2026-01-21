d879548ab51c536b065192a64daebc2b52ca65d0
Kguo-cs <u3006612@connect.hku.hk>
2026-01-21 19:34:45 +0800

    dit128_roformer1_match_initloss_gan50_tbig05

--- src/smart/modules/initial_diffusion.py
+++ src/smart/modules/initial_diffusion.py
@@ -263,13 +263,13 @@ class PDInit(nn.Module):
                         self.D.train()
                         # loss=torch.tensor(0.0, device=real_heading.device)
 
-                        match_loss,pos_loss,heading_loss,shape_loss,vel_loss=get_matching_loss(old_nonego_type_sorted, old_batch,
-                                                                                               x_pred * normal_scale + normal_mean,m_init * normal_scale + normal_mean
+                        match_loss,pos_loss,heading_loss,shape_loss,vel_loss=get_matching_loss(tokenized_agent["nonego_type_sorted"], new_batch,
+                                                                                               FakeSamples,RealSamples
                                                                                                )
 
                         # match_loss= pos_loss= heading_loss=shape_loss= vel_loss=torch.tensor(0.0, device=non_ego.device)
 
-                        loss= (loss+loss_diff_init.mean(), match_loss,pos_loss,heading_loss,shape_loss,vel_loss)
+                        loss= (loss+loss_diff_init[~low_noise_mask].mean(), match_loss,pos_loss,heading_loss,shape_loss,vel_loss)
                         #loss_diff_init,match_loss,pos_loss,heading_loss,shape_loss,vel_loss
                     self.global_step += 1
                 else:
