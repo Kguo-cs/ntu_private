ae126be47ebceb40f2153a99e962dca584c80a6e
Kguo-cs <u3006612@connect.hku.hk>
2026-01-21 14:40:32 +0800

    dit128_mode1

--- src/smart/modules/initial_diffusion.py
+++ src/smart/modules/initial_diffusion.py
@@ -63,7 +63,7 @@ class PDInit(nn.Module):
         # self.agent_latents_scale=torch.tensor([[2.951, 2.383, 3.042, 2.819, 2.614, 2.401, 2.673, 2.773]])
         # self.agent_latents_mean=torch.tensor([[-0.059,  0.043, -0.014,  0.116,  0.314,  0.155,  0.274, -0.091]])
 
-        self.use_gan=True
+        self.use_gan=False
 
         if self.use_gan:
             self.D=InitDiscriminator(hidden_dim,num_heads,num_freq_bands,token_processor)
@@ -229,8 +229,6 @@ class PDInit(nn.Module):
 
                     tokenized_agent["nonego_batch"] = new_batch
                     tokenized_agent["nonego_type_sorted"]=tokenized_agent["nonego_type_sorted"][low_noise_mask]
-                    # RealSamples[:, 2] = torch.atan2(RealSamples[:, 3], RealSamples[:, 2])  #
-                    # FakeSamples[:, 2] = torch.atan2(FakeSamples[:, 3], FakeSamples[:, 2])  #
 
                     if self.global_step %10 == 0:
 
