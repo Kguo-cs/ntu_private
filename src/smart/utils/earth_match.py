1f3684633337585ff84af171b23ccecbd39b4bb2
Kguo-cs <u3006612@connect.hku.hk>
2026-01-22 13:18:09 +0800

    dit128_roformer1_matchonlyl1

--- src/smart/utils/earth_match.py
+++ src/smart/utils/earth_match.py
@@ -20,7 +20,7 @@ def matching_loss(
     #
     # pos_loss = dist.mean()
 
-    pos_loss=F.mse_loss(fake_pos, real_pos)
+    pos_loss=F.l1_loss(fake_pos, real_pos)
 
     # Heading: periodic-safe loss
     # heading_diff = torch.atan2(
@@ -30,12 +30,12 @@ def matching_loss(
     # heading_diff=wrap_angle(fake_heading - real_heading)
     # heading_loss = heading_diff.abs().mean()
 
-    heading_loss= F.mse_loss(fake_heading, real_heading)
+    heading_loss= F.l1_loss(fake_heading, real_heading)
 
     # Shape: L1
-    shape_loss = F.mse_loss(fake_shape[:,:2], real_shape[:,:2])
+    shape_loss = F.l1_loss(fake_shape[:,:2], real_shape[:,:2])
 
-    vel_loss = F.mse_loss(fake_shape[:,2:], real_shape[:,2:])
+    vel_loss = F.l1_loss(fake_shape[:,2:], real_shape[:,2:])
 
     total_loss = (
         w_pos * pos_loss +
