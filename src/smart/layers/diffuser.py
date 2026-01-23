bb59124af692f30ae7a4ae3898b9af13946b9ec0
Kguo-cs <u3006612@connect.hku.hk>
2026-01-23 13:14:36 +0800

    dit128_roformer1_l1_pos

--- src/smart/layers/diffuser.py
+++ src/smart/layers/diffuser.py
@@ -149,6 +149,8 @@ class InitDiffusion(nn.Module):
 
         z = (1 - t[:,:, None]) * x0 + t[:,:, None] * x1 #large t, low noise
 
+        z[...,2:]=x1[...,2:]
+
         if self.x_pred:
             v_target = (x1 - z) / (1 - t[:,:, None]).clamp_min(self.t_eps)
 
@@ -166,7 +168,7 @@ class InitDiffusion(nn.Module):
 
             x_init_0_reconstructed =x0+v_pred
 
-        return F.l1_loss(x_init_0_reconstructed , x1,reduction="none") ,x_init_0_reconstructed[:,0],t_batch,t #t>0.5
+        return F.l1_loss(v_pred , v_target,reduction="none") ,x_init_0_reconstructed[:,0],t_batch,t #t>0.5 #F.l1_loss(x_init_0_reconstructed , x1,reduction="none")
 
     @torch.no_grad()
     def sample_flow(self,num_samples,tokenized_agent, scene_enc,    eval_mask):
--- src/smart/modules/initial_diffusion.py
+++ src/smart/modules/initial_diffusion.py
@@ -283,7 +283,8 @@ class PDInit(nn.Module):
                    # match_loss=(match_loss/normal_scale).mean()
                     match_loss = pos_loss = heading_loss = shape_loss = vel_loss = torch.tensor(0.0,
                                                                                                 device=non_ego.device)
-                    weight=torch.tensor([[[0.1,0.1,0.5,0.5,0.2,0.2,0.2,0.2]]],device=non_ego.device)*normal_mean[None]
+                    #weight=torch.tensor([[[0.1,0.1,0.5,0.5,0.2,0.2,0.2,0.2]]],device=non_ego.device)*normal_mean[None]
+                    weight=1
 
                     loss = ((loss_diff_init*weight).mean(),loss_diff_init.mean(), match_loss, pos_loss, heading_loss, shape_loss, vel_loss)
 
