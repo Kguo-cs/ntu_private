232e63f6f37d7f00867c714c6f8fb15a60a14bed
Kguo-cs <u3006612@connect.hku.hk>
2026-01-24 09:10:51 +0800

    dit128_roformer1_mse_heun_Rand

--- src/smart/layers/diffuser.py
+++ src/smart/layers/diffuser.py
@@ -125,7 +125,7 @@ class InitDiffusion(nn.Module):
     def sample_t(self, n: int, device=None):
         # z = torch.randn(n, device=device) * self.P_std + self.P_mean
         # z = torch.sigmoid(z)
-        timesteps = power_schedule(self.steps+1, device, alpha=2)
+        #timesteps = power_schedule(self.steps+1, device, alpha=2)
 
         z=torch.rand(n, device=device)#timesteps[torch.randint(low=0,high=self.steps,size=(n,),device=device)] #/self.steps#
         return z
@@ -215,9 +215,9 @@ class InitDiffusion(nn.Module):
 
         #dt = 1.0 / steps
         #ts = cosine_schedule(steps, z.device)
-        #timesteps=torch.linspace(0,1,steps+1,device=eval_mask.device)
+        timesteps=torch.linspace(0,1,steps+1,device=eval_mask.device)
 
-        timesteps = power_schedule(steps+1, z.device, alpha=2)
+        #timesteps = power_schedule(steps+1, z.device, alpha=2)
        # ts[0] = 1e-4
        # z[..., 0, 2:] = tokenized_agent["m_init"][..., 2:]
         # ode
