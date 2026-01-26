bdfa2836b9bf435f78c5081cbaac442c135ace45
Kguo-cs <u3006612@connect.hku.hk>
2026-01-26 17:46:16 +0800

    AIRL40_pred_allvalid

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -418,7 +418,7 @@ class IQ_SoftQ(LightningModule):
         if not self.gail:
             return expert_nll
 
-        tokenized_agent["train_mask"]=tokenized_agent["pred_mask"] #& tokenized_agent["token_mask"][:,self.start_step:].all(1)
+        tokenized_agent["train_mask"]=tokenized_agent["pred_mask"] & tokenized_agent["token_mask"][:,self.start_step:].all(1)
 
         expert_dis_loss,_,_,expert_present_mask,expert_gp,expert_dis_mask = self.get_reward(tokenized_agent, "expert")
 
