96b5d6698320adac76c4747a6b5181e5709ff823
Kguo-cs <u3006612@connect.hku.hk>
2026-02-27 13:33:31 +0800

    dit128_scale1_max512_trainLint

--- src/smart/layers/diffuser.py
+++ src/smart/layers/diffuser.py
@@ -135,8 +135,8 @@ class InitDiffusion(nn.Module):
 
         timesteps = torch.linspace(0, 1, self.steps + 1, device=device)
 
-        idx = torch.randint(0, timesteps.shape[0], (n,), device=timesteps.device)
-        z = timesteps[idx]
+        idx = torch.randint(0, timesteps.shape[0], (1,), device=timesteps.device)
+        z = timesteps[idx].repeat(n)
 
         #z=torch.rand(n, device=device)#timesteps[torch.randint(low=0,high=self.steps,size=(n,),device=device)] #/self.steps#
         return z
