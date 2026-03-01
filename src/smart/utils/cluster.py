49f86981926f1caf13dbb3870c130a92e10c2589
Kguo-cs <u3006612@connect.hku.hk>
2026-03-01 15:38:33 +0800

    dit128_airl_newcluster_coun_scedule256_lambda09

--- src/smart/utils/cluster.py
+++ src/smart/utils/cluster.py
@@ -136,7 +136,7 @@ def kmeans( padded, mask,k_per_graph,batch,pos, iters=10):
     return centroids
 
 
-def batch_increasing_schedule(N, S=256+1, gamma=1.0):
+def batch_increasing_schedule(N, S=256+1, gamma=0.9):
     """
     N: (B,) tensor of maximum levels per batch
     S: total number of steps (int)
