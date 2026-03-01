2dafe8126b57345e629912d2d5f7c5efedb6ad2d
Kguo-cs <u3006612@connect.hku.hk>
2026-03-01 13:27:26 +0800

    dit128_airl_2N_newcluster_coun_train512

--- src/smart/utils/cluster.py
+++ src/smart/utils/cluster.py
@@ -170,7 +170,7 @@ def batched_kmeans_variable_k(pos, batch,  num_graphs,iters=10):
     u = torch.rand(num_graphs, device=device)
 
     # scale per-graph and floor
-    k_per_graph = torch.floor(u * (counts*2 + 1)).long()
+    k_per_graph = torch.floor(u * (512 + 1)).long()
 
     k_per_graph = torch.minimum(k_per_graph, counts)
 
