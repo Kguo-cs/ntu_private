247aac6ca2a797c2e0f98f6a560f5f08329ac513
Kguo-cs <u3006612@connect.hku.hk>
2026-03-02 22:03:15 +0800

    dit128_128pad64_t_maxK_same

--- src/smart/utils/cluster.py
+++ src/smart/utils/cluster.py
@@ -107,9 +107,6 @@ def kmeans( padded, mask,k_per_graph,batch,pos,max_k, iters=10):
 
     return centroids
 
-
-
- # [00:48<11:03,  5.34it/s, v_num=vx1p]
 def batched_kmeans_variable_k(pos, batch,  num_graphs,iters=10):
     """
     Batched K-Means per graph, allowing variable number of clusters per graph.
@@ -163,17 +160,19 @@ def batched_kmeans_variable_k(pos, batch,  num_graphs,iters=10):
 
     centroids=kmeans(padded, mask,k_per_graph,batch,pos,max_k)
 
-    centroids1=torch.zeros_like(centroids)
+    centroids1 = kmeans(padded, mask, k1_per_graph,batch, pos,max_k)
 
-    new_k=k_per_graph!=k1_per_graph
-
-    same_batch_mask = new_k[batch]
-
-    selected_batch = batch[same_batch_mask]
-
-    _, consecutive_batch = torch.unique(selected_batch, return_inverse=True)
-
-    centroids1[new_k]=kmeans(padded[new_k], mask[new_k],k1_per_graph[new_k],consecutive_batch,pos[same_batch_mask],max_k)
+    # centroids1=torch.zeros_like(centroids)
+    #
+    # new_k=k_per_graph!=k1_per_graph
+    #
+    # same_batch_mask = new_k[batch]
+    #
+    # selected_batch = batch[same_batch_mask]
+    #
+    # _, consecutive_batch = torch.unique(selected_batch, return_inverse=True)
+    #
+    # centroids1[new_k]=kmeans(padded[new_k], mask[new_k],k1_per_graph[new_k],consecutive_batch,pos[same_batch_mask],max_k)
 
     # k1_per_graph[:10]=0
     #
@@ -311,63 +310,64 @@ def cluster_points(pos, batch, type, num_graphs,use_all_type
         ):
     if use_all_type:
         return cluster_points1(pos, batch, type, num_graphs)
-    # veh_pos=pos[type==0]
-    # veh_batch=batch[type==0]
-    #
-    # #centroids_b,centroids1_b, k_per_graph,k1_per_graph,step_idx=batched_kmeans_variable_k(veh_pos, veh_batch,num_graphs)
-    #
-    #
-    # device = pos.device
-    # less_centroids = []
-    # more_batch = []
-    # more_centroids = []
-    # more_type = []
-    #
-    # for i in range(num_graphs):
-    #     mask = (batch == i)
-    #     type_i = type[mask]
-    #
-    #     x_non_veh = pos[mask][type_i != 0]
-    #
-    #     type_non_veh = type_i[type_i != 0]
-    #
-    #     x = pos[mask][type_i == 0]
-    #     N = x.shape[0]
-    #     step = torch.randint(0, N + 1, (1,), device=device).item()
-    #     k = min(step, N)
-    #
-    #     if k == 0:
-    #         centroids = x[:k]
-    #     else:
-    #         centroids = kmeans_fast(x, k)#x[:k]#
-    #
-    #     k1 = min(k + 1, N)  # torch.randint(k+1, N+1, (1,), device=device).item()
-    #
-    #     if k1 == 0:
-    #         centroids1 = x[:k1]
-    #     else:
-    #         centroids1 =kmeans_fast(x, k1)# x[:k1] #
-    #
-    #     # k=k_per_graph[i]
-    #     # k1=k1_per_graph[i]
-    #     # centroids=centroids_b[i][:k]
-    #     # centroids1=centroids1_b[i][:k1]
-    #
-    #
-    #     padding_centers = torch.zeros_like(centroids1[k:])
-    #
-    #     less_centroids.append(torch.cat([centroids, padding_centers, x_non_veh], dim=0))
-    #
-    #     more_batch.append(torch.zeros([k1 + len(x_non_veh)], device=device, dtype=torch.long) + i)
-    #
-    #     more_centroids.append(torch.cat([centroids1, x_non_veh], dim=0))
-    #
-    #     more_type.append(torch.cat([torch.zeros([k1], device=device, dtype=torch.long), type_non_veh], dim=0))
-    #
-    # less_centroids = torch.cat(less_centroids, dim=0)
-    # more_batch = torch.cat(more_batch, dim=0)
-    # more_centroids = torch.cat(more_centroids, dim=0)
-    # more_type = torch.cat(more_type, dim=0)
+
+    veh_pos=pos[type==0]
+    veh_batch=batch[type==0]
+
+    centroids_b,centroids1_b, k_per_graph,k1_per_graph,step_idx,step_number=batched_kmeans_variable_k(veh_pos, veh_batch,num_graphs)
+
+
+    device = pos.device
+    less_centroids = []
+    more_batch = []
+    more_centroids = []
+    more_type = []
+
+    for i in range(num_graphs):
+        mask = (batch == i)
+        type_i = type[mask]
+
+        x_non_veh = pos[mask][type_i != 0]
+
+        type_non_veh = type_i[type_i != 0]
+
+        x = pos[mask][type_i == 0]
+        N = x.shape[0]
+        step = torch.randint(0, N + 1, (1,), device=device).item()
+        k = min(step, N)
+
+        # if k == 0:
+        #     centroids = x[:k]
+        # else:
+        #     centroids = kmeans_fast(x, k)#x[:k]#
+        #
+        # k1 = min(k + 1, N)  # torch.randint(k+1, N+1, (1,), device=device).item()
+        #
+        # if k1 == 0:
+        #     centroids1 = x[:k1]
+        # else:
+        #     centroids1 =kmeans_fast(x, k1)# x[:k1] #
+
+        k=k_per_graph[i]
+        k1=k1_per_graph[i]
+        centroids=centroids_b[i][:k]
+        centroids1=centroids1_b[i][:k1]
+
+
+        padding_centers = torch.zeros_like(centroids1[k:])
+
+        less_centroids.append(torch.cat([centroids, padding_centers, x_non_veh], dim=0))
+
+        more_batch.append(torch.zeros([k1 + len(x_non_veh)], device=device, dtype=torch.long) + i)
+
+        more_centroids.append(torch.cat([centroids1, x_non_veh], dim=0))
+
+        more_type.append(torch.cat([torch.zeros([k1], device=device, dtype=torch.long), type_non_veh], dim=0))
+
+    less_centroids = torch.cat(less_centroids, dim=0)
+    more_batch = torch.cat(more_batch, dim=0)
+    more_centroids = torch.cat(more_centroids, dim=0)
+    more_type = torch.cat(more_type, dim=0)
     #
     # if not (torch.all(less_centroids == less_centroids1) & torch.all(more_centroids == more_centroids1) & torch.all(more_batch == more_batch1) & torch.all(more_type == more_type1)):
     #
@@ -375,12 +375,12 @@ def cluster_points(pos, batch, type, num_graphs,use_all_type
     #     print(torch.all(more_centroids == more_centroids1))
     #     print(torch.all(more_batch == more_batch1))
     #     print(torch.all(more_type == more_type1))
-    less_centroids,more_centroids,more_batch,more_type,centroids_b,centroids1_b, k_per_graph,k1_per_graph,step_idx,step_number=build_less_more_grouped(
-        pos,
-        type,
-        batch,
-        num_graphs
-        )
+    # less_centroids,more_centroids,more_batch,more_type,centroids_b,centroids1_b, k_per_graph,k1_per_graph,step_idx,step_number=build_less_more_grouped(
+    #     pos,
+    #     type,
+    #     batch,
+    #     num_graphs
+    #     )
 
     return less_centroids, more_batch, more_centroids, more_type,step_idx,step_number
 
