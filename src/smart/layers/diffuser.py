d2f02ac93d0ce423085694601e8a7835abb10729
Kguo-cs <u3006612@connect.hku.hk>
2026-03-04 17:46:11 +0800

    dit128_clusterscale_typesch_matchV

--- src/smart/layers/diffuser.py
+++ src/smart/layers/diffuser.py
@@ -226,10 +226,10 @@ class InitDiffusion(nn.Module):
 
                 x_pred = self.net(z, t, tokenized_agent, scene_enc, eval_mask)
 
-                fake_idx, real_idx= get_closest_sum_idx(x_pred[:,0],x[:,0],agent_batch,tokenized_agent["nonego_type_sorted"],all_state=True)
-
-                x_pred=x_pred[fake_idx]
-
+                # fake_idx, real_idx= get_closest_sum_idx(x_pred[:,0],x[:,0],agent_batch,tokenized_agent["nonego_type_sorted"],all_state=True)
+                #
+                # x_pred=x_pred[fake_idx]
+                #
                 denom = (1 - t[:, :, None]).clamp_min(self.t_eps)
 
 
