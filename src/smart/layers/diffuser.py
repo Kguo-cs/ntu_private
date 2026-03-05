9c12c98c89da2f6ded09aca805dd72fc95c58cc7
Kguo-cs <u3006612@connect.hku.hk>
2026-03-05 20:49:25 +0800

    dit128_noPose_cdist_predlocal_egoRel_a30

--- src/smart/layers/diffuser.py
+++ src/smart/layers/diffuser.py
@@ -858,7 +858,7 @@ class InitDenoiser(nn.Module):
                     batch_s=batch,  # [n_agent*n_step]
                     mask=None,  # [n_agent, n_step]
                     max_radius=60,
-                    max_num_neighbors=20,
+                    max_num_neighbors=30,
                     agent_train_mask=None,
                     layer_num=self.num_layers,
                     counter_feat_a=None,
