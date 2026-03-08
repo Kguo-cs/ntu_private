dedde4087bd73bf478e944e11b2a36d62a1c7af5
Kguo-cs <u3006612@connect.hku.hk>
2026-03-08 21:49:43 +0800

    dit256_init0_map80

--- src/smart/diffusion/denoiser.py
+++ src/smart/diffusion/denoiser.py
@@ -503,8 +503,8 @@ class InitDenoiser(nn.Module):
                     mask=None,  # [n_agent, n_step]
                     batch_s=batch,  # [n_agent,n_step]
                     batch_pl=batch_pl,  # [n_pl*n_step]
-                    pl2a_radius=40,
-                    max_num_neighbors=20,
+                    pl2a_radius=80,
+                    max_num_neighbors=80,
                     agent_train_mask=None,
                     layer_num=self.num_layers
                 )
