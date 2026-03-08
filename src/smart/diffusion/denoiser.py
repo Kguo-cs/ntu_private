0575c21af6765d4397cd3d156625d7d4cf86bfa7
Kguo-cs <u3006612@connect.hku.hk>
2026-03-08 23:10:40 +0800

    dit256_init0_nonorm_localv

--- src/smart/diffusion/denoiser.py
+++ src/smart/diffusion/denoiser.py
@@ -510,8 +510,8 @@ class InitDenoiser(nn.Module):
                     head_vector_s=head_vector_s,  # [n_agent, n_step, 2]
                     batch_s=batch,  # [n_agent*n_step]
                     mask=None,  # [n_agent, n_step]
-                    max_radius=60,
-                    max_num_neighbors=20,
+                    max_radius=100,
+                    max_num_neighbors=100,
                     agent_train_mask=None,
                     layer_num=self.num_layers,
                     counter_feat_a=None,
