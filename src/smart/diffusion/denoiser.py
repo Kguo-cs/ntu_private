5ae38c8c9ebb75f921bd32f60eca011a722dae22
Kguo-cs <u3006612@connect.hku.hk>
2026-03-09 11:10:41 +0800

    dit256_norel_noego

--- src/smart/diffusion/denoiser.py
+++ src/smart/diffusion/denoiser.py
@@ -125,10 +125,10 @@ class InitDenoiser(nn.Module):
                                                 )
 
 
-                self.ego_encoder = EdgeEncoder(hidden_dim,
-                                                num_freq_bands,
-                                                use_pl2a=True,
-                                                )
+                # self.ego_encoder = EdgeEncoder(hidden_dim,
+                #                                 num_freq_bands,
+                #                                 use_pl2a=True,
+                #                                 )
 
 
                 self.pt2a_attn_layers = nn.ModuleList(
@@ -159,20 +159,20 @@ class InitDenoiser(nn.Module):
                     ]
                 )
 
-                self.a2ego_attn_layers = nn.ModuleList(
-                    [
-                        AttentionLayer(
-                            hidden_dim=hidden_dim,
-                            num_heads=num_heads,
-                            head_dim=head_dim,
-                            dropout=dropout,
-                            bipartite=True,
-                            has_pos_emb=True,
-                        )
-                        for _ in range(num_layers)
-                    ]
-                )
-
+                # self.a2ego_attn_layers = nn.ModuleList(
+                #     [
+                #         AttentionLayer(
+                #             hidden_dim=hidden_dim,
+                #             num_heads=num_heads,
+                #             head_dim=head_dim,
+                #             dropout=dropout,
+                #             bipartite=True,
+                #             has_pos_emb=True,
+                #         )
+                #         for _ in range(num_layers)
+                #     ]
+                # )
+                #
 
             else:
                 module=RoFormerDecoder(hidden_dim=hidden_dim, num_heads=num_heads, dropout=0,
@@ -517,25 +517,25 @@ class InitDenoiser(nn.Module):
                     dis_edge_mask=None
                 )  # edge_index_a2a: [2, n_edge_a2a], r_a2a: [n_edge_a2a, hidden_dim]
 
-                ego_theta = torch.atan2(self.normal_mean[:, 3], self.normal_mean[:, 2]).repeat(num_graphs)
-                ego_pos=self.normal_mean[:,:2].repeat(num_graphs,1)
-
-                ego_batch=torch.arange(num_graphs).to(device)
-
-                edge_index_ego2a, r_ego2a = self.ego_encoder.build_map2agent_edge(
-                    pos_pl=ego_pos,  # [n_pl, 2]
-                    orient_pl=ego_theta,  # [n_pl]
-                    pos_a=pos_s,  # [n_agent, n_step, 2]
-                    head_a=theta,  # [n_agent, n_step]
-                    head_vector_a=head_vector_s,  # [n_agent, n_step, 2]
-                    mask=None,  # [n_agent, n_step]
-                    batch_s=batch,  # [n_agent,n_step]
-                    batch_pl=ego_batch,  # [n_pl*n_step]
-                    pl2a_radius=1000,
-                    max_num_neighbors=1,
-                    agent_train_mask=None,
-                    layer_num=self.num_layers
-                )
+                # ego_theta = torch.atan2(self.normal_mean[:, 3], self.normal_mean[:, 2]).repeat(num_graphs)
+                # ego_pos=self.normal_mean[:,:2].repeat(num_graphs,1)
+                #
+                # ego_batch=torch.arange(num_graphs).to(device)
+                #
+                # edge_index_ego2a, r_ego2a = self.ego_encoder.build_map2agent_edge(
+                #     pos_pl=ego_pos,  # [n_pl, 2]
+                #     orient_pl=ego_theta,  # [n_pl]
+                #     pos_a=pos_s,  # [n_agent, n_step, 2]
+                #     head_a=theta,  # [n_agent, n_step]
+                #     head_vector_a=head_vector_s,  # [n_agent, n_step, 2]
+                #     mask=None,  # [n_agent, n_step]
+                #     batch_s=batch,  # [n_agent,n_step]
+                #     batch_pl=ego_batch,  # [n_pl*n_step]
+                #     pl2a_radius=1000,
+                #     max_num_neighbors=1,
+                #     agent_train_mask=None,
+                #     layer_num=self.num_layers
+                # )
 
                 #
                 # rel_pos_a2ego = pos_s-ego_pos
@@ -557,7 +557,7 @@ class InitDenoiser(nn.Module):
 
                 for layer_i in range(self.num_layers):
 
-                    feat_a = self.a2ego_attn_layers[layer_i]((ego_embedding, feat_a), r_ego2a, edge_index_ego2a)
+                  #  feat_a = self.a2ego_attn_layers[layer_i]((ego_embedding, feat_a), r_ego2a, edge_index_ego2a)
 
                     feat_a = self.a2a_attn_layers[layer_i](feat_a, r_a2a, edge_index_a2a)
 
