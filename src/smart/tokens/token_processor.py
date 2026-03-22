d84387919976c7dad380a2ad390ba59b3cc8e7dc
Kguo-cs <u3006612@connect.hku.hk>
2026-03-22 19:14:04 +0800

    dit64_sample20_l1_allpos_v10

--- src/smart/tokens/token_processor.py
+++ src/smart/tokens/token_processor.py
@@ -275,13 +275,13 @@ class TokenProcessor(torch.nn.Module):
 
         if self.pred_init and not self.learn_init and self.training:
             first_valid=valid[:,0]
-            pos[first_valid,:6]=pos[first_valid,:6]+torch.randn_like(pos[first_valid,:6])*0.01
-            heading[first_valid,:6]=heading[first_valid,:6]+torch.randn_like(heading[first_valid,:6])*0.01
+            pos[first_valid,:6]=pos[first_valid,:6]+torch.randn_like(pos[first_valid,:6])*0.02
+            heading[first_valid,:6]=heading[first_valid,:6]+torch.randn_like(heading[first_valid,:6])*0.02
             first_nonvalid=~first_valid
             pos[first_nonvalid,:11]=pos[first_nonvalid,:11]+torch.randn_like(pos[first_nonvalid,:11])*0.01
-            heading[first_nonvalid,:11]=heading[first_nonvalid,:11]+torch.randn_like(heading[first_nonvalid,:11])*0.01
+            heading[first_nonvalid,:11]=heading[first_nonvalid,:11]+torch.randn_like(heading[first_nonvalid,:11])*0.02
 
-            shape=shape+torch.randn_like(shape)*0.01
+            shape=shape+torch.randn_like(shape)*0.02
 
         role_mask = data["agent"]["role"]
 
