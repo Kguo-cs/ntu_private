70894a8a3af5a0d62132977de38dedc96caa0c6c
Kguo-cs <u3006612@connect.hku.hk>
2026-03-23 18:44:53 +0800

    dit64_sample10_newmap_gailnoise1e2

--- src/smart/gan/discriminator.py
+++ src/smart/gan/discriminator.py
@@ -120,7 +120,7 @@ class InitDiscriminator(nn.Module):
                 )
 
         self.type_embedding = nn.Embedding(3, hidden_dim)
-        self.shape_embedding = MLPLayer(5, hidden_dim, hidden_dim)
+        self.shape_embedding = MLPLayer(9, hidden_dim, hidden_dim)
 
         self.score_decoder = MLPLayer(hidden_dim, hidden_dim, 1)
 
@@ -299,12 +299,12 @@ class InitDiscriminator(nn.Module):
 
     def forward(self,inputs, map_feature,  tokenized_agent,return_weight=False):
 
-        inputs[:,:-1]=inputs[:,:-1]+torch.randn_like(inputs[:,:-1])*1e-2
+        #inputs[:,:-1]=inputs[:,:-1]+torch.randn_like(inputs[:,:-1])*1e-2
 
         pos_a=inputs[...,:2]
         head_a=torch.atan2(inputs[...,3],inputs[...,2])
 
-        shape=inputs[...,4:]
+        shape=inputs#[...,4:]
 
         #if self.discriminator:
         # pos_a=pos_a+torch.randn_like(pos_a)*1e-2
