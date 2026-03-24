4e3597a9a6325ab6b17863eec6eb0d507e0ebbdc
Kguo-cs <u3006612@connect.hku.hk>
2026-03-24 22:27:41 +0800

    scenario_rollouts

--- src/smart/gan/discriminator.py
+++ src/smart/gan/discriminator.py
@@ -41,7 +41,7 @@ class InitDiscriminator(nn.Module):
 
         self.use_entry_former = False
         self.use_transformer=False
-        self.use_decompose = True
+        self.use_decompose = False
 
         self.dis_weight=10
         self.dist_decay=3
