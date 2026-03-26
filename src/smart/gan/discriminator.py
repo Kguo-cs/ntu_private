7b7af2d7bff46561a0296b0f730eb81d64b7f384
Kguo-cs <u3006612@connect.hku.hk>
2026-03-26 17:39:08 +0800

    scenario_rollouts

--- src/smart/gan/discriminator.py
+++ src/smart/gan/discriminator.py
@@ -133,7 +133,7 @@ class InitDiscriminator(nn.Module):
         if self.use_GAIL:
             self.return_meanstd = RunningMeanStdTorch(shape=(1))
 
-        self.Gamma=10
+        self.Gamma=1
 
         self.use_sde=False
 
