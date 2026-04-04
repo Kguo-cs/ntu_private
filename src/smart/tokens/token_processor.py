dac173b523cb471dbb191e8554c90eabd89947ec
Kguo-cs <u3006612@connect.hku.hk>
2026-04-04 10:21:12 +0800

    traj92_eps005_step1_firstvalidtoken

--- src/smart/tokens/token_processor.py
+++ src/smart/tokens/token_processor.py
@@ -243,13 +243,15 @@ class TokenProcessor(torch.nn.Module):
         shape=data["agent"]["shape"].clone()
 
         if self.pred_init and not self.learn_init and self.training and not self.traj_diffusion:
+            pd=torch.randn_like(pos[:,5]).clamp(min=-3,max=3)*0.1
+            hd=torch.randn_like(heading[:,5]).clamp(min=-3,max=3)*0.1
 
-            pos[:,5]=pos[:,5]+torch.randn_like(pos[:,5]).clamp(min=-3,max=3)*0.1
-            heading[:,5]=heading[:,5]+torch.randn_like(heading[:,5]).clamp(min=-3,max=3)*0.1
+            pos[:,5]=pos[:,5]+pd
+            heading[:,5]=heading[:,5]+hd
             shape=shape+torch.randn_like(shape).clamp(min=-3,max=3)*0.1
 
-            # pos[:,0]=+torch.randn_like(pos[:,0]).clamp(min=-3,max=3)*0.1
-            # heading[:,0]=+torch.randn_like(heading[:,0]).clamp(min=-3,max=3)*0.1
+            pos[:,0]=pos[:,0]+pd+torch.randn_like(pos[:,0]).clamp(min=-3,max=3)*0.1
+            heading[:,0]=heading[:,0]+hd+torch.randn_like(heading[:,0]).clamp(min=-3,max=3)*0.1
 
             error_dist=0.3
         else:
