6318405e7cc9b5da1d83b830d806956ee126a6b3
Kguo-cs <u3006612@connect.hku.hk>
2026-04-06 17:06:32 +0800

    con3_alldist_scale_nodiff

--- sd/models/direct_diffusion.py
+++ sd/models/direct_diffusion.py
@@ -50,7 +50,6 @@ class Direct_diffusion(pl.LightningModule):
         if self.use_latent:
             self.cfg_model = cfg_ldm.model
             self.cfg_ldm = cfg_ldm
-            print(self.cfg_model.autoencoder_path)
             # self.autoencoder = ScenarioDreamerAutoEncoder.load_from_checkpoint(self.cfg_model.autoencoder_path,
             #                                                                    cfg=cfg, map_location='cpu',
             #                                                                    weights_only=False)  # 🔥 key fix)
