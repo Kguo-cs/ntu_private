25678fc5c0a4284bd8f3a9515f0abc05c7f287bd
Kguo-cs <u3006612@connect.hku.hk>
2026-04-09 15:18:17 +0800

    1

--- sd/models/direct_diffusion.py
+++ sd/models/direct_diffusion.py
@@ -110,7 +110,7 @@ class Direct_diffusion(pl.LightningModule):
 
         self.scenarios={}
 
-        self.use_diffusion=False
+        self.use_diffusion=True
 
         if self.use_latent:
             self.cfg_model = cfg_ldm.model
