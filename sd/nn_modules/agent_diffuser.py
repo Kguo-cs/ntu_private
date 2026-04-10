1a17988aade7be1c8b6fef3a89888ee28ded30ee
Kguo-cs <u3006612@connect.hku.hk>
2026-04-10 10:19:36 +0800

    conr_rel256

--- sd/nn_modules/agent_diffuser.py
+++ sd/nn_modules/agent_diffuser.py
@@ -257,8 +257,8 @@ class AgentDecoder(nn.Module):
             head_vector_s=head_vector_s,  # [n_agent, n_step, 2]
             batch_s=batch,  # [n_agent*n_step]
             mask=None,  # [n_agent, n_step]
-            max_radius=100,
-            max_num_neighbors=60,
+            max_radius=60,
+            max_num_neighbors=20,
             agent_train_mask=None,
             layer_num=self.num_layers,
             counter_feat_a=None,
@@ -276,8 +276,8 @@ class AgentDecoder(nn.Module):
             mask=mask,  # [n_agent, n_step]
             batch_s=batch,  # [n_agent,n_step]
             batch_pl=batch_pl,  # [n_pl*n_step]
-            pl2a_radius=100,
-            max_num_neighbors=100,
+            pl2a_radius=40,
+            max_num_neighbors=20,
             agent_train_mask=None,
             layer_num=self.num_layers
         )
