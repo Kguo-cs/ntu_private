a4b0f6b97ac668e8ba100bd9182482907c494cf7
Kguo-cs <u3006612@connect.hku.hk>
2026-04-13 12:07:34 +0800

    conr_rel256

--- sd/train.py
+++ sd/train.py
@@ -139,7 +139,10 @@ def train_ldm(cfg, cfg_ae, save_dir=None):
 
 def train_autoencoder(cfg,cfg_ldm, save_dir=None):
     """ Train the Scenario Dreamer AutoEncoder model."""
+    print(cfg_ldm)
     cfg_ldm = set_latent_stats(cfg_ldm)
+    print(cfg_ldm)
+
     datamodule = instantiate(cfg.datamodule, dataset_cfg=cfg_ldm.dataset)
 
     model=Direct_diffusion(cfg,cfg_ldm)
