8c19183511c1adbba48ec004db01cc71a0a3930a
Kguo-cs <u3006612@connect.hku.hk>
2026-04-16 10:17:00 +0800

    1

--- src/smart/diffusion/dit/ldm.py
+++ src/smart/diffusion/dit/ldm.py
@@ -16,7 +16,7 @@ class LDM(nn.Module):
         hidden_dim=512
 
         self.model = DiT(hidden_dim)
-        self.ego_embedding= MLPLayer(19, hidden_dim, hidden_dim)
+        self.ego_embedding1= MLPLayer(19, hidden_dim, hidden_dim)
         self.lane_embed1= nn.Linear(128+4, hidden_dim)
 
         n_timesteps = 100
