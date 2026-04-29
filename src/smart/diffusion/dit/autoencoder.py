b5743c81c49a6b8f35988fe02d900e83a9885e36
Kguo-cs <u3006612@connect.hku.hk>
2026-04-29 17:08:10 +0800

    1

--- src/smart/diffusion/dit/autoencoder.py
+++ src/smart/diffusion/dit/autoencoder.py
@@ -409,7 +409,7 @@ class AutoEncoder(nn.Module):
         # agent_loss = static_err_weighted.mean()#_scene_mean(static_err_weighted, batch)
 
         # agent vector regression loss
-        agent_loss = self.agent_loss_fn(agent_states_pred/scale, x_agent/scale, batch)#=F.l1_loss(agent_states_pred/scale,x_agent/scale)#
+        agent_loss = F.l1_loss(agent_states_pred/scale,x_agent/scale)#self.agent_loss_fn(agent_states_pred/scale, x_agent/scale, batch)#=
 
         #agent_kl_loss = -0.5 * (1 + agent_log_var - agent_mu ** 2 - agent_log_var.exp())
         agent_kl_loss = self.kl_loss_fn(agent_mu, agent_log_var, batch)
