38f52242218ffab1eb9e376b76b4a9e55b73869c
Kguo-cs <u3006612@connect.hku.hk>
2026-04-29 21:50:19 +0800

    1

--- src/smart/diffusion/initial_diffusion.py
+++ src/smart/diffusion/initial_diffusion.py
@@ -37,7 +37,7 @@ class InitDiffusion(nn.Module):
         self.add_model_specific_args(parser)
         args = parser.parse_args()
 
-        self.learn_autoencoder = True
+        self.learn_autoencoder = False
         self.latent_diffusion = True
         self.sep_map=False
 
