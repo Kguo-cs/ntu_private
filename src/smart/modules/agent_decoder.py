e8033f23c78955ee8ddce3d81584bd64e6499129
Kguo-cs <u3006612@connect.hku.hk>
2026-05-01 17:03:32 +0800

    AIRL19_flow_vpred_rotateout_sde_eval

--- src/smart/modules/agent_decoder.py
+++ src/smart/modules/agent_decoder.py
@@ -95,10 +95,10 @@ class SMARTAgentDecoder(nn.Module):
 
     def predict_agent(self, sampled_idx,token_mask, mask_a ,pos_a,head_a,tokenized_agent, map_feature,shape, n_current=0):
 
-        # if self.discriminator and self.training:
-        #     pos_a=pos_a+torch.randn_like(pos_a)*1e-2
-        #     head_a=head_a+torch.randn_like(head_a)*1e-2
-        #     shape=shape+torch.randn_like(shape)*1e-2
+        if self.discriminator and self.training:
+            pos_a=pos_a+torch.randn_like(pos_a)*1e-2
+            head_a=head_a+torch.randn_like(head_a)*1e-2
+            shape=shape+torch.randn_like(shape)*1e-2
 
         n_agent, n_step = head_a.shape
 
