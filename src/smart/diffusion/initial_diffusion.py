ae4a930c6be80d4f6f4eb4661d38fac85b2d1e6a
Kguo-cs <u3006612@connect.hku.hk>
2026-05-03 22:10:10 +0800

    nft_collision

--- src/smart/diffusion/initial_diffusion.py
+++ src/smart/diffusion/initial_diffusion.py
@@ -200,6 +200,8 @@ class InitDiffusion(nn.Module):
 
                     advantage=(col_reward_agent==0).float() -0.5#col_reward <0 collision 0
 
+                    advantage=advantage-advantage.mean()
+
                   #  col_reward_agent=col_reward_agent.clamp_min(-20)
 
                     #advantage=(col_reward_agent-col_reward_agent.mean())/col_reward_agent.std()
