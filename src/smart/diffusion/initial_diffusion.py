8cfe782f38679322334c4caf26bf45e10373c878
Kguo-cs <u3006612@connect.hku.hk>
2026-05-05 11:08:05 +0800

    nft_collision

--- src/smart/diffusion/initial_diffusion.py
+++ src/smart/diffusion/initial_diffusion.py
@@ -219,7 +219,7 @@ class InitDiffusion(nn.Module):
 
                     col_reward_agent=torch.all(col_reward,dim=-1)
                     #
-                    advantage=(col_reward_agent==0).float()# -0.5#col_reward <0 collision 0
+                    advantage=(col_reward_agent==True).float()# -0.5#col_reward <0 collision 0
 
                    # advantage=advantage-advantage.mean()
 
