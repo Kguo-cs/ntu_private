5a36d50f08acf66905f22af2901f161332843bc6
Kguo-cs <u3006612@connect.hku.hk>
2026-05-06 16:18:37 +0800

    nft_collision

--- src/smart/diffusion/scale_flow.py
+++ src/smart/diffusion/scale_flow.py
@@ -143,13 +143,13 @@ class ScaleFlow(nn.Module):
 
         self.use_flux=False
 
-        self.use_sde=False
+        self.use_sde=True
 
         self.noise_level=0.7
 
         self.rationorm=False
 
-        self.use_nft=False
+        self.use_nft=True
 
         if self.use_nft:
             self.old_model = copy.deepcopy(self.model)
