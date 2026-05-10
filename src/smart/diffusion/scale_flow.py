58467276ba3c91013af2c353c27fcdb20ba65dbe
Kguo-cs <u3006612@connect.hku.hk>
2026-05-10 23:18:47 +0800

    nft_collision

--- src/smart/diffusion/scale_flow.py
+++ src/smart/diffusion/scale_flow.py
@@ -445,24 +445,33 @@ class ScaleFlow(nn.Module):
             denom = (1 - t).clamp_min(0.05)  # /t.clamp_min(self.t_eps)torch.ones_like(t) #
             e =x-(x - z) / denom #e_sampled
 
-        match_loss, pos_loss, heading_loss, shape_loss, vel_loss, collision_loss = get_matching_loss(
-            tokenized_agent,
-            x_pred[:,0],
-            x[:,0],
-            z[:,0],
-            e[:,0],
-            t[:,0],
-            use_col=False,
-            x_pred=self.x_pred
-        )
-        # with torch.no_grad():
-        #     weight_factor = (
-        #         torch.abs(x_pred.double() - x.double())
-        #         .mean(dim=tuple(range(1, x.ndim)), keepdim=True)
-        #         .clip(min=0.00001)
-        #     )
-        # # weight_factor=1
-        # match_loss = ((x_pred - x) ** 2 / weight_factor).mean(dim=tuple(range(1, x.ndim)))
+            # kl_loss = (
+            #     torch.mean((pred.float() - sample["preds"].detach().float()) ** 2)
+            #     / args.gradient_accumulation_steps
+            # )
+
+
+            with torch.no_grad():
+                weight_factor = (
+                    torch.abs(x_pred.double() - x.double())
+                    .mean(dim=tuple(range(1, x.ndim)), keepdim=True)
+                    .clip(min=0.00001)
+                )
+            match_loss = ((x_pred - x) ** 2 / weight_factor).mean(dim=tuple(range(1, x.ndim)))
+
+            collision_loss=pos_loss=heading_loss=shape_loss=vel_loss = torch.zeros_like(match_loss)
+        else:
+
+            match_loss, pos_loss, heading_loss, shape_loss, vel_loss, collision_loss = get_matching_loss(
+                tokenized_agent,
+                x_pred[:,0],
+                x[:,0],
+                z[:,0],
+                e[:,0],
+                t[:,0],
+                use_col=False,
+                x_pred=self.x_pred
+            )
 
         # print(policy_loss)
 
