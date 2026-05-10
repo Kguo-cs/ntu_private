1d8737c388ec163e4065bcafebe51c030e775034
Kguo-cs <u3006612@connect.hku.hk>
2026-05-10 11:33:02 +0800

    nft_collision

--- src/smart/diffusion/scale_flow.py
+++ src/smart/diffusion/scale_flow.py
@@ -396,7 +396,7 @@ class ScaleFlow(nn.Module):
 
                         per_sample_policy_loss=per_sample_policy_loss * sigma_t
 
-                    policy_loss = per_sample_policy_loss.mean()
+                    policy_loss = per_sample_policy_loss.mean()*0.1
 
                 #x_pred = self.model(z, t, tokenized_agent, initial_map_feature)
 
