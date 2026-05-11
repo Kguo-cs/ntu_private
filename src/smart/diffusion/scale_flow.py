c264f0fc00fe65f75c435ac4043951f3db7d6a29
Kguo-cs <u3006612@connect.hku.hk>
2026-05-11 13:30:33 +0800

    1

--- src/smart/diffusion/scale_flow.py
+++ src/smart/diffusion/scale_flow.py
@@ -153,7 +153,7 @@ class ScaleFlow(nn.Module):
 
         self.use_nft=False
 
-        self.use_kl=True
+        self.use_kl=False
 
         if self.use_nft:
             self.old_model = copy.deepcopy(self.model)
@@ -433,7 +433,7 @@ class ScaleFlow(nn.Module):
 
                         per_sample_policy_loss=per_sample_policy_loss * sigma_t
 
-                    policy_loss = per_sample_policy_loss.mean()*0.3
+                    policy_loss = per_sample_policy_loss.mean()*0.1
 
                 #x_pred = self.model(z, t, tokenized_agent, initial_map_feature)
 
