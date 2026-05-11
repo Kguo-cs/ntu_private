12555092b5d34cdfa1dc794582456c3b3d282b67
Kguo-cs <u3006612@connect.hku.hk>
2026-05-11 15:46:06 +0800

    1

--- src/smart/diffusion/scale_flow.py
+++ src/smart/diffusion/scale_flow.py
@@ -402,7 +402,7 @@ class ScaleFlow(nn.Module):
                             ((x_pred/scale - x_sampled/scale) ** 2).reshape(x_sampled.shape[0], -1),
                             dim=1,
                         )
-                        self_normalize=True
+                        self_normalize=False
                         if self_normalize:
                             match_loss = match_loss / torch.mean(
                                 torch.abs(
