7c0c983c07781da05c7ed58bbdf701c95fd753ee
Kguo-cs <u3006612@connect.hku.hk>
2026-05-11 10:58:00 +0800

    1

--- src/smart/diffusion/scale_flow.py
+++ src/smart/diffusion/scale_flow.py
@@ -431,7 +431,7 @@ class ScaleFlow(nn.Module):
 
                         per_sample_policy_loss=per_sample_policy_loss * sigma_t
 
-                    policy_loss = per_sample_policy_loss.mean()*0.2
+                    policy_loss = per_sample_policy_loss.mean()*0.02
 
                 #x_pred = self.model(z, t, tokenized_agent, initial_map_feature)
 
