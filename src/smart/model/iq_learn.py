94e95d034c1c71947f1b3bbaf76acf9380945344
Kguo-cs <u3006612@connect.hku.hk>
2026-05-13 11:22:36 +0800

    1

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -252,7 +252,7 @@ class IQ_SoftQ(LightningModule):
             return expert_nll
 
         for key in ["sampled_pos", "sampled_heading","sampled_idx","valid_mask","token_mask"]:
-            tokenized_agent[key]=tokenized_agent[key][:,:10]
+            tokenized_agent[key]=tokenized_agent[key][:,:5]
 
        # if self.pred_init:
         tokenized_agent["train_mask"] = None
