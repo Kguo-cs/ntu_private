25c9d0d2958dfc9e4ce30ade14139a563a6b1e88
Kguo-cs <u3006612@connect.hku.hk>
2026-05-15 10:17:42 +0800

    1

--- src/smart/gan/discriminator.py
+++ src/smart/gan/discriminator.py
@@ -129,7 +129,7 @@ class InitDiscriminator(nn.Module):
         if self.use_GAIL:
             self.return_meanstd = RunningMeanStdTorch(shape=(1))
 
-        self.Gamma=0
+        self.Gamma=0.1
 
         self.use_sde=False
 
@@ -231,7 +231,7 @@ class InitDiscriminator(nn.Module):
         g_loss, gen_rewards, r1, FakeLogits=self.get_d_loss(fake_samples,0,map_feature, tokenized_agent)
 
 
-        loss=match_loss*0.01-g_loss
+        loss=match_loss*0.1-g_loss
 
         logger("train/g_loss", g_loss.item(), on_step=True, batch_size=1)
 
