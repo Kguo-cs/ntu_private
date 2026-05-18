77c7b0eda479ddc1f86c5f93d8d5afc7d1a628ee
Kguo-cs <u3006612@connect.hku.hk>
2026-05-18 19:29:34 +0800

    1

--- src/smart/diffusion/scale_flow.py
+++ src/smart/diffusion/scale_flow.py
@@ -663,7 +663,7 @@ class ScaleFlow(nn.Module):
     def _euler_step(self, z, t, t_next, labels,noise_level,sde_inspired=True):
 
         if sde_inspired:
-            gamma=0.2
+            gamma=0.5
             h = t_next - t
 
             alpha = torch.clamp(
