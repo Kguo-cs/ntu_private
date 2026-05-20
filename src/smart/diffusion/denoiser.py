8bf57019a3b2696039e844ce46030f4721845381
Kguo-cs <u3006612@connect.hku.hk>
2026-05-20 10:36:22 +0800

    1

--- src/smart/diffusion/denoiser.py
+++ src/smart/diffusion/denoiser.py
@@ -405,7 +405,7 @@ class InitDenoiser(nn.Module):
                 #
                 #self.normal_scale[:,2:4]=self.normal_scale[:,2:4]*2
                # self.normal_scale[:,4:6]=self.normal_scale[:,4:6]*2
-               # self.normal_scale[:,:2]=self.normal_scale[:,:2]*0.5
+                self.normal_scale[:,:2]=self.normal_scale[:,:2]*0.5
                 # self.normal_scale[:,6:]=self.normal_scale[:,6:]*0.5
 
         return diff_input,diff_output
