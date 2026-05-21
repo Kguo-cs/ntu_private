6e5e866101a3004b82e935cde4b93e76ffacd67c
Kguo-cs <u3006612@connect.hku.hk>
2026-05-21 19:10:27 +0800

    1

--- src/smart/diffusion/denoiser.py
+++ src/smart/diffusion/denoiser.py
@@ -403,7 +403,7 @@ class InitDenoiser(nn.Module):
                 max_v=torch.amax(diff_output, dim=0, keepdim=True)
                 self.normal_mean.copy_((min_v+max_v)/2)
 
-                @#self.normal_mean[:,:2]=0
+                #self.normal_mean[:,:2]=0
 
                 #self.normal_mean.copy_(torch.mean(diff_output, dim=0, keepdim=True))
                 self.normal_scale.copy_(torch.std(diff_output, dim=0, keepdim=True))
