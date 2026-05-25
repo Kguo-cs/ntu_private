4723a03b92588aa7e12bf11522f19699cdf2b318
Kguo-cs <u3006612@connect.hku.hk>
2026-05-25 10:38:53 +0800

    1

--- src/smart/diffusion/scale_flow.py
+++ src/smart/diffusion/scale_flow.py
@@ -227,16 +227,15 @@ class ScaleFlow(nn.Module):
         fake_idx = torch.argsort(fake_pos, stable=True)
         fake_idx = fake_idx[torch.argsort(nonego_type[fake_idx], stable=True)]
         fake_idx = fake_idx[torch.argsort(agent_batch[fake_idx], stable=True)]
+
+        x = x[fake_idx]
+        tokenized_agent["nonego_type"] = tokenized_agent["nonego_type"][fake_idx]
         #
         # real_pos = e[:, 0, 0] + e[:, 0, 1]
         # real_idx = torch.argsort(real_pos, stable=True)
         # real_idx = real_idx[torch.argsort(nonego_type[real_idx], stable=True)]
         # real_idx = real_idx[torch.argsort(agent_batch[real_idx], stable=True)]
         # e = e[real_idx]
-
-        x = x[fake_idx]
-        tokenized_agent["nonego_type"] = tokenized_agent["nonego_type"][fake_idx]
-
         if self.learn_noise:
             t = torch.zeros((len(agent_batch)), device=x.device, dtype=torch.float32)[:,None,None]
 
