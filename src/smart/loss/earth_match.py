988dae7ba6e2e2c5db341af58ec011ce61857462
Kguo-cs <u3006612@connect.hku.hk>
2026-05-27 09:32:39 +0800

    1

--- src/smart/loss/earth_match.py
+++ src/smart/loss/earth_match.py
@@ -375,9 +375,9 @@ def get_matching_loss(
         #
         batch = tokenized_agent["nonego_batch"][t_mask]#[-len(fake_state):]
         #
-        col_loss=multi_circle_collision_loss_mem_efficient(fake_state[:,:2], torch.atan2(fake_state[:,3],fake_state[:,2]).detach(), fake_state[:,4].detach(),fake_state[:,5].detach(),batch)[0].mean()
+        col_loss=multi_circle_collision_loss_mem_efficient(fake_state[:,:2], torch.atan2(fake_state[:,3],fake_state[:,2]), fake_state[:,4].detach(),fake_state[:,5].detach(),batch)[0].mean()
     else:
-        col_loss = torch.zeros_like(match_loss.mean())  #.detach()
+        col_loss = torch.zeros_like(match_loss.mean())  #.detach().detach()
 
     # [03:08<13:55,  3.71it/s, v_num=4e1b]
 
