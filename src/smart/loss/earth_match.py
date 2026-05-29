f270319f18e3c6c02995ae4a2b8957857b19fa43
Kguo-cs <u3006612@connect.hku.hk>
2026-05-29 10:21:57 +0800

    1

--- src/smart/loss/earth_match.py
+++ src/smart/loss/earth_match.py
@@ -247,7 +247,7 @@ def multi_circle_collision_loss_mem_efficient(
 
     penetration = thresh - min_dist  #penatration>-0.1
 
-    loss = torch.relu(penetration).expm1()*100
+    loss = torch.relu(penetration).expm1()*20
 
     return loss,end_idx,start_idx#.mean() if reduction == "mean" else loss.sum()
 
