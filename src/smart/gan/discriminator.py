2f9b7137d2d1867ca8306063e5111377dc54c215
Kguo-cs <u3006612@connect.hku.hk>
2026-06-01 15:30:47 +0800

    1

--- src/smart/gan/discriminator.py
+++ src/smart/gan/discriminator.py
@@ -335,7 +335,7 @@ class InitDiscriminator(nn.Module):
         g_loss, gen_rewards, r1, FakeLogits=self.get_d_loss(fake_samples,0,tokenized_agent)
 
 
-        loss=match_loss#*0.1-g_loss
+        loss=match_loss*0.1-g_loss
 
        # logger("train/g_loss", g_loss.item(), on_step=True, batch_size=1)
 
