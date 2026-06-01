dd6c78d4c6502f81a28d1313fcdb08ca25f2ee0b
Kguo-cs <u3006612@connect.hku.hk>
2026-06-01 21:24:53 +0800

    1

--- src/smart/gan/discriminator.py
+++ src/smart/gan/discriminator.py
@@ -52,7 +52,7 @@ class InitDiscriminator(nn.Module):
 
        # if self.dis_vel:
         self.use_decompose = False
-        self.use_entry_former = False
+        self.use_entry_former = True
 
         if self.use_entry_former:
 
