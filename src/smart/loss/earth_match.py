57901f39586c028ac0f2dff7d72b4742aef6fae9
Kguo-cs <u3006612@connect.hku.hk>
2026-06-03 23:18:41 +0800

    1

--- src/smart/loss/earth_match.py
+++ src/smart/loss/earth_match.py
@@ -326,7 +326,10 @@ def get_matching_loss(
         fake_state=fake_state[fake_idx]
 
     if x_pred:
-        denom = (1 - t).clamp_min(t_eps) / t_dt#.detach()  # /t.clamp_min(self.t_eps)torch.ones_like(t) #
+        denom = (1 - t).clamp_min(t_eps) #/ t_dt#.detach()  # /t.clamp_min(self.t_eps)torch.ones_like(t) #
+
+        real_state=real_state*t_dt.detach()
+        fake_state=real_state*t_dt#.square()#.detach()
     else:
         real_state = (real_state - e)#*t_dt.detach()
 
