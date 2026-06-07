0083e0b7546f5ddca75d5f0217f5ef35199db0b9
Kguo-cs <u3006612@connect.hku.hk>
2026-06-07 22:23:48 +0800

    1

--- src/smart/diffusion/scale_flow.py
+++ src/smart/diffusion/scale_flow.py
@@ -144,7 +144,7 @@ class ScaleFlow(nn.Module):
             self.use_sde=False
 
         if self.learn_noise:
-            self.noise_model = DiT(128)
+            self.noise_model = DiT(256)
             # self.noise_model = InitDenoiser(
             #     token_processor,
             #     dataset=args.dataset,
