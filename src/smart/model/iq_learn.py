790e6f0ea2f2941420c58a86e40fd1e59a22abb0
Kguo-cs <u3006612@connect.hku.hk>
2026-06-12 13:52:45 +0800

    1

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -516,7 +516,7 @@ class IQ_SoftQ(LightningModule):
         self.log("train/advantages", advantages.mean().item(), on_step=True, batch_size=1)
         self.log("train/value_loss", value_loss.item(), on_step=True, batch_size=1)
 
-        policy_loss = expert_nll + 0.1*ppo_loss + 1e-3 * value_loss  # - 0.01 * agent_entropy.mean()
+        policy_loss = expert_nll + ppo_loss + 1e-2 * value_loss  # - 0.01 * agent_entropy.mean()
 
         actor_optimizer.zero_grad()
 
