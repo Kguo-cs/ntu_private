f37b08acd331d29bbf763309b8ccfda98414a6e5
Kguo-cs <u3006612@connect.hku.hk>
2026-06-13 15:29:19 +0800

    1

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -440,18 +440,19 @@ class IQ_SoftQ(LightningModule):
 
 
        # if self.pred_init:
-       #  tokenized_agent["train_mask"] = tokenized_agent["token_mask"].all(1)
-       #  tokenized_agent["pred_mask"] =tokenized_agent["token_mask"].all(1)
+        tokenized_agent["train_mask"] = tokenized_agent["token_mask"].all(1)
+        tokenized_agent["pred_mask"] =tokenized_agent["token_mask"].all(1)
         # else:
         #     tokenized_agent["train_mask"]=tokenized_agent["pred_mask"] #& tokenized_agent["token_mask"][:,self.start_step:].all(1)
-        tokenized_agent["train_mask"] = None
-        tokenized_agent["pred_mask"] =None
 
         if self.encoder.learn_dis:
             expert_dis_loss,_,_,_,expert_dis_mask = self.get_reward(tokenized_agent, "expert")
         else:
             expert_dis_loss=0
             expert_dis_mask=None
+
+        tokenized_agent["train_mask"] = None
+        tokenized_agent["pred_mask"] =None
 
         tokenized_agent_rollout = rollout(self.encoder, tokenized_map, tokenized_agent,  self.validation_rollout_sampling)
 
@@ -503,7 +504,7 @@ class IQ_SoftQ(LightningModule):
 
             value=torch.cat([intial_value[None],value],dim=0)
 
-        advantages, value_loss=compute_advantages(agent_rewards[-len(value):], value)#[max(0,1-self.gail_start_step):]
+        advantages, value_loss=compute_advantages(agent_rewards[-len(value)], value)#[max(0,1-self.gail_start_step):]
 
         #advantages = advantages[agent_train_mask]#t,a  # only train at expert valid
 
