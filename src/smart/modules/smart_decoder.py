de643f420109463d947904f6286278d15ba90fa4
Kguo-cs <u3006612@connect.hku.hk>
2026-06-15 09:49:45 +0800

    1

--- src/smart/modules/smart_decoder.py
+++ src/smart/modules/smart_decoder.py
@@ -229,16 +229,17 @@ class SMARTDecoder(nn.Module):
         else:
             if self.agent_encoder.learn_init :
                 map_feature=None
+                if self.pred_init:
+                    if self.sep_map:
+                        initial_map_feature = self.map_encoder1(tokenized_map,tokenized_agent=tokenized_agent)
+                    else:
+                        initial_map_feature = self.map_encoder(tokenized_map,tokenized_agent=tokenized_agent)
+                    tokenized_agent["initial_map_feature"] = initial_map_feature
+
             else:
                 map_feature = self.map_encoder(tokenized_map)
                 tokenized_agent["map_feature"] = map_feature
 
-            if self.pred_init:
-                if self.sep_map:
-                    initial_map_feature = self.map_encoder1(tokenized_map,tokenized_agent=tokenized_agent)
-                else:
-                    initial_map_feature = self.map_encoder(tokenized_map,tokenized_agent=tokenized_agent)
-                tokenized_agent["initial_map_feature"] = initial_map_feature
 
         if self.traj_diffusion:
             pred_dict=self.traj_diffuser(tokenized_agent,map_feature)
