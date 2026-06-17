6c0211791fd9c161f411f24d5513baa19104d7bd
Kguo-cs <u3006612@connect.hku.hk>
2026-06-17 13:01:13 +0800

    1

--- src/smart/model/smart.py
+++ src/smart/model/smart.py
@@ -124,7 +124,7 @@ class SMART(LightningModule):
                 self.para_num=32
             else:
                 self.para_num=1
-            self.n_rollout_closed_val=4
+            self.n_rollout_closed_val=2
         else:
             self.challenge_type=ChallengeType.SIM_AGENTS
             self.para_num=32
@@ -370,7 +370,7 @@ class SMART(LightningModule):
                     tfrecord_path=data["tfrecord_path"]
                     if self.n_vis_batch==0:
                         if self.challenge_type == ChallengeType.SCENARIO_GEN:
-                            scenario_rollouts=scenario_rollouts[:32]
+                            scenario_rollouts=scenario_rollouts[:64]
                         if len(scenario_rollouts) > self.para_num:
                             for i in range(np.ceil(len(scenario_rollouts) / self.para_num).astype(int)):  # 64
                                 print(i)# [05:45<00:00] para  [05:27<00:00] resample 64: [06:11<00:00,
