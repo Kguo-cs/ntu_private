e902542cfbcb884614362e50a4d952b0e060968e
Kguo-cs <u3006612@connect.hku.hk>
2026-06-20 16:55:02 +0800

    1

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -463,7 +463,7 @@ class IQ_SoftQ(LightningModule):
                 sampled_pos=sampled_pos,
                 sampled_heading=sampled_heading,
                 shape=shape,
-                critic_score=combined_logits.sum(),
+                critic_score=ego_logits.sum(),
                 valid_mask=tokenized_agent["valid_mask"],
                 gamma=gamma,
             )
