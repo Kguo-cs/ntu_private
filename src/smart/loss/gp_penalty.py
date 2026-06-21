a55c5b4a000d696a5e9366ece73fa080e9dda554
Kguo-cs <u3006612@connect.hku.hk>
2026-06-21 17:27:10 +0800

    1

--- src/smart/loss/gp_penalty.py
+++ src/smart/loss/gp_penalty.py
@@ -535,8 +535,8 @@ def _weighted_bce_with_logits(
     #     reduction="mean",
     # )
 
-    # elementwise_loss=(F.relu(1+(1-2*targets)*logits)*weight).mean()
-    elementwise_loss=(((2*targets-1)-logits).square()*weight).mean()
+    elementwise_loss=(F.relu(1+(1-2*targets)*logits)*weight).mean()
+   # elementwise_loss=(((2*targets-1)-logits).square()*weight).mean()
 
     return elementwise_loss #.sum() / weight.sum().clamp_min(eps)
 
