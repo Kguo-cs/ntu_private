90a898a43df9aa2c71d1634dc8a55b48e3621174
Kguo-cs <u3006612@connect.hku.hk>
2026-06-23 10:38:02 +0800

    1

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -382,7 +382,7 @@ class IQ_SoftQ(LightningModule):
             batch_size=1,
         )
         if use_gp_this_step:
-            gamma = 0.001 #l1 loss
+            gamma = 0.01 #l1 loss
 
             # critic_score = ego_logits.sum()
             # if has_interact_logits:
@@ -399,7 +399,7 @@ class IQ_SoftQ(LightningModule):
                 sampled_pos=sampled_pos,
                 sampled_heading=sampled_heading,
                 shape=shape,
-                critic_score=combined_logits[combined_logits.abs()>1].sum(),
+                critic_score=combined_logits.sum(),#[combined_logits.abs()>1]
                 valid_mask=tokenized_agent["valid_mask"],
                 gamma=gamma,
             )
