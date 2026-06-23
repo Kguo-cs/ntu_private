fdd6d5de26b2f4333de2acb7cf2346f72f02eed8
Kguo-cs <u3006612@connect.hku.hk>
2026-06-23 18:14:05 +0800

    1

--- src/smart/diffusion/diffusion_utils.py
+++ src/smart/diffusion/diffusion_utils.py
@@ -556,7 +556,7 @@ def get_matching_loss(
     scale=1 ,all_state=False,use_col=False,use_all_type=False,use_match=False,x_pred=False,
     t_eps=0.05, w_pos=0.1, w_heading=0.5, w_shape=0.2, w_vel=0.2,
     max_loss_weight=25.0,
-    use_huber=True,
+    use_huber=False,
     huber_beta=10,
     ):
 
