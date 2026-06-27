bc8b6d5489781ecd8b2193cd026d56378f158039
Kguo-cs <u3006612@connect.hku.hk>
2026-06-27 12:50:41 +0800

    1

--- src/smart/diffusion/diffusion_utils.py
+++ src/smart/diffusion/diffusion_utils.py
@@ -552,19 +552,26 @@ def get_diff_loss(
         edge_idx = torch.cat([start_idx, end_idx], dim=0)
         edge_loss = torch.cat([col_loss, col_loss], dim=0)
 
-        col_loss = scatter_sum(
-            edge_loss,
-            edge_idx,
-            dim=0,
-            dim_size=len(fake_state),
-        )/50#[0]
+        # col_loss = scatter_sum(
+        #     edge_loss,
+        #     edge_idx,
+        #     dim=0,
+        #     dim_size=len(fake_state),
+        # )/50#[0]
 
-        # col_loss1 = scatter_sum(
+        # col_loss1 = scatter_mean(
         #     edge_loss,
         #     edge_idx,
         #     dim=0,
         #     dim_size=len(fake_state),
         # )#[0]
+
+        col_loss = scatter_max(
+            edge_loss,
+            edge_idx,
+            dim=0,
+            dim_size=len(fake_state),
+        )[0]/40
 
 
         col_loss=(col_loss*w)
