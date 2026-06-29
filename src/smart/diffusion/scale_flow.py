d5dc99435adf5063e84391b57c56ccd418d2dfd2
Kguo-cs <u3006612@connect.hku.hk>
2026-06-29 11:26:16 +0800

    1

--- src/smart/diffusion/scale_flow.py
+++ src/smart/diffusion/scale_flow.py
@@ -508,7 +508,7 @@ class ScaleFlow(nn.Module):
 
                 #advantages_pg=advantages_pg.clamp_min(0.0)
 
-                logp_cur = -sampled_match_loss[non_ego]*0.01
+                logp_cur = -sampled_match_loss[non_ego]*0.1
 
                 tokenized_agent["sampled_match_loss"]=sampled_match_loss
 
@@ -520,7 +520,7 @@ class ScaleFlow(nn.Module):
 
                     sampled_match_loss = (mse_Loss /l1_Loss).mean(-1)
 
-                    logp_old=-sampled_match_loss[non_ego]*0.01
+                    logp_old=-sampled_match_loss[non_ego]*0.1
                 else:
                     logp_old = logp_cur.detach()
 
