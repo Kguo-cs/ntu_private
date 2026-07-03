865f0876160b66872a2bd47d7379862df45f1e2b
Kguo-cs <u3006612@connect.hku.hk>
2026-07-03 15:46:39 +0800

    1

--- src/smart/model/iq_learn.py
+++ src/smart/model/iq_learn.py
@@ -528,11 +528,6 @@ class IQ_SoftQ(LightningModule):
             self._log_train('train/shape_loss', shape_loss)
             self._log_train('train/vel_loss', vel_loss)
             self._log_train('train/g_loss', g_loss)
-            if "sampled_match_loss" in tokenized_agent.keys():
-                self._log_train('train/sampled_match_loss', tokenized_agent["sampled_match_loss"].mean())
-            self._log_train('train/policy_loss', tokenized_agent["policy_loss"])
-            if "clip_ratio" in tokenized_agent.keys():
-                self._log_train('train/clip_ratio', tokenized_agent["clip_ratio"].mean())
 
             init_loss=match_loss+g_loss
 
@@ -566,5 +561,10 @@ class IQ_SoftQ(LightningModule):
         loss = self.iq_update(tokenized_map, tokenized_agent)
 
         self._log_train("train/loss", loss)
+        if "sampled_match_loss" in tokenized_agent.keys():
+            self._log_train('train/sampled_match_loss', tokenized_agent["sampled_match_loss"].mean())
+        self._log_train('train/policy_loss', tokenized_agent["policy_loss"])
+        if "clip_ratio" in tokenized_agent.keys():
+            self._log_train('train/clip_ratio', tokenized_agent["clip_ratio"].mean())
 
         return loss
