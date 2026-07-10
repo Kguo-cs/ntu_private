c0eb0f894e73c9612ffa11acc04d5262216cacfc
Kguo-cs <u3006612@connect.hku.hk>
2026-07-10 16:45:48 +0800

    1

--- src/smart/diffusion/scale_flow.py
+++ src/smart/diffusion/scale_flow.py
@@ -525,9 +525,9 @@ class ScaleFlow(nn.Module):
         else:
             e = torch.randn_like(x)
 
-        fake_idx=get_closest_sum_idx(e[:,0],self.model.normalize(x)[:,0],tokenized_agent,all_state=True)
+        #fake_idx=get_closest_sum_idx(e[:,0],self.model.normalize(x)[:,0],tokenized_agent,all_state=True)
 
-        e=e[fake_idx]
+       # e=e[fake_idx]
 
         e = self.model.denormalize(e, init_agent_type)
 
