2611d068af742af4127003cd1058070e316c4552
Kguo-cs <u3006612@connect.hku.hk>
2026-07-11 15:54:39 +0800

    1

--- src/smart/diffusion/denoiser.py
+++ src/smart/diffusion/denoiser.py
@@ -104,7 +104,7 @@ class InitDenoiser(nn.Module):
         self.use_return_conditioned = False
         self.use_prev_condition = False
         self.label_drop_prob = 0.0
-        self.map_drop_prob=0.0
+        self.map_drop_prob=0.1
 
         if mean_flow:
             raise NotImplementedError("mean_flow=True was removed from the cleaned InitDenoiser.")
@@ -217,8 +217,8 @@ class InitDenoiser(nn.Module):
 
     def _maybe_init_normalizer(self, diff_output: torch.Tensor) -> None:
         if not torch.all(self.normal_mean == 0):
-            if self.normal_scale[0][2]>1.5:
-                self.normal_scale[:, 2:6] = self.normal_scale[:, 2:6] * 0.5
+            # if self.normal_scale[0][2]>1.5:
+            #     # self.normal_scale[:, 2:6] = self.normal_scale[:, 2:6] * 0.5
             #     # self.normal_scale[:, :2] = self.normal_scale[:, :2] * 2
             #     self.normal_mean = torch.tensor(
             #         [[
