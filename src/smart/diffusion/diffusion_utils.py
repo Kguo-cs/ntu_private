caeaec2d76f22b1b7291d532cce58ce701426281
Kguo-cs <u3006612@connect.hku.hk>
2026-07-12 15:20:35 +0800

    1

--- src/smart/diffusion/diffusion_utils.py
+++ src/smart/diffusion/diffusion_utils.py
@@ -324,7 +324,7 @@ def matching_loss(
 
 
     if fake_state.shape[-1]<16:
-        pos_loss = _robust_component_loss(fake_pos, real_pos, beta=huber_beta, use_huber=use_huber)
+        pos_loss = _robust_component_loss(fake_pos, real_pos, beta=0, use_huber=True)
         heading_loss = _robust_component_loss(fake_heading, real_heading, beta=0, use_huber=True)
         shape_loss = _robust_component_loss(fake_shape, real_shape, beta=huber_beta, use_huber=use_huber)
         vel_loss = _robust_component_loss(fake_vel, real_vel, beta=huber_beta, use_huber=use_huber)
