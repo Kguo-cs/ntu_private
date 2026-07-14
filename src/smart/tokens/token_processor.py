0187701547324b4cfa43e85a58e290256ba5e4bd
Kguo-cs <u3006612@connect.hku.hk>
2026-07-14 12:30:12 +0800

    1

--- src/smart/tokens/token_processor.py
+++ src/smart/tokens/token_processor.py
@@ -67,7 +67,7 @@ class TokenProcessor(torch.nn.Module):
         self.pred_2step=False
 
         self.traj_diffusion=False
-        self.use_gradient_penalty = True
+        self.use_gradient_penalty = False
 
         module_dir = os.path.dirname(__file__)
         self.init_agent_token(os.path.join(module_dir, agent_token_file))
