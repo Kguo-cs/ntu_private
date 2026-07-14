c0b988e59a5bd9b28fb34f475f68714c463863ce
Kguo-cs <u3006612@connect.hku.hk>
2026-07-14 12:43:30 +0800

    1

--- src/smart/diffusion/diffusion_utils.py
+++ src/smart/diffusion/diffusion_utils.py
@@ -710,7 +710,7 @@ def get_closest_sum_idx_fast(
 
 def get_diff_loss(
     tokenized_agent, fake_state,real_state,t,t_eps,scale=1 ,all_state=False,use_col=False,use_all_type=False,use_match=False,x_pred=False,
-    w_pos=0.1/ 5, w_heading=0.5/ 5, w_shape=0.2/ 5, w_vel=0.2/ 5,
+    w_pos=0.1/ 5, w_heading=0.5/ 5, w_shape=0.2/ 5, w_vel=5/ 5,
     #w_pos=0.1, w_heading=0.1, w_shape=0.1, w_vel=0.1,
     # w_pos = 0.0015,
     # w_heading = 0.5,
