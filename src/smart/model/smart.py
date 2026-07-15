a8d646def7e946ae132d3c517b28b93266cb6d0c
Kguo-cs <u3006612@connect.hku.hk>
2026-07-15 12:06:18 +0800

    1

--- src/smart/model/smart.py
+++ src/smart/model/smart.py
@@ -163,9 +163,9 @@ class SMART(LightningModule):
         return loss
 
     def validation_step(self, data, batch_idx):
-        if batch_idx < 136:
-            return None
-        #
+        # if batch_idx < 136:
+        #     return None
+        # #
         tokenized_map, tokenized_agent = self.token_processor(data)
 
         # # ! open-loop vlidation
