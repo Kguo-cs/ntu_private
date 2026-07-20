ff061b1fb39ef214c5f34e00aeeb57e01bdd4382
Kguo-cs <u3006612@connect.hku.hk>
2026-07-20 17:37:55 +0800

    1

--- src/smart/model/smart.py
+++ src/smart/model/smart.py
@@ -118,7 +118,7 @@ class SMART(LightningModule):
             finetune=model_config.finetune,
         )
 
-        self.training_rollout_len = int(_cfg(model_config, "training_rollout_len", 10))
+        self.training_rollout_len = int(_cfg(model_config, "training_rollout_len", 12))
         self._configure_finetuning(bool(model_config.finetune))
 
         self.n_vis_batch = int(model_config.n_vis_batch)
