f4dda506e0d334d40e9eecaa3a48a632e5732cfe
Kguo-cs <u3006612@connect.hku.hk>
2026-08-03 22:52:54 +0800

    1

--- src/smart/diffusion/diffusion_utils.py
+++ src/smart/diffusion/diffusion_utils.py
@@ -443,7 +443,7 @@ def get_diff_loss(
     w_pos: float = 0.1 / 5,
     w_heading: float = 0.5 / 5,
     w_shape: float = 0.2 / 5,
-    w_vel: float = 0.2 / 5,
+    w_vel: float = 1 / 5,
     max_loss_weight: float | None = None,
     use_huber: bool = False,
     huber_beta: float = 0.1,
