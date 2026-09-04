b7d83913de2f53df0743ba41c30a24469204f44a
Kguo-cs <u3006612@connect.hku.hk>
2026-09-04 19:47:00 +0800

    1

--- src/smart/diffusion/initial_diffusion.py
+++ src/smart/diffusion/initial_diffusion.py
@@ -76,7 +76,7 @@ class InitDiffusion(nn.Module):
             "init_logprob_clip": 50.0,
             "init_ppo_clip": 0.2,
             "num_branch_steps": 1,
-            #"branch_steps": [0,1,2,3,4,5,6,7,8],#5,6,71,3,5,7,4,4,1,2,3,4,5,10,11,12,13,
+            "branch_steps": [0,1,2,3,4,5,6,7,8],#5,6,71,3,5,7,4,4,1,2,3,4,5,10,11,12,13,
             "sampling_steps": 10,
             "use_rl": False,
         }
