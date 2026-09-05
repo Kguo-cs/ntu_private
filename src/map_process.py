9cb26db8f7bb8f5120217f7a9289fd78abe649ba
Kguo-cs <u3006612@connect.hku.hk>
2026-09-05 12:37:06 +0800

    1

--- src/map_process.py
+++ src/map_process.py
@@ -509,18 +509,18 @@ def batch_process9s_transformer(input_dir, output_dir, split, num_workers):
 
     input_dir = Path(input_dir) / split
     packages = sorted([p.as_posix() for p in input_dir.glob("*")])#[6:]
-    func = partial(
-        wm2argo,
-        split=split,
-        output_dir=output_dir,
-        output_dir_tfrecords_splitted=output_dir_tfrecords_splitted,
-    )
-
-    with multiprocessing.Pool(num_workers) as p:
-        r = list(tqdm(p.imap_unordered(func, packages), total=len(packages)))
-    # print(len(packages))
-    # for file_path in tqdm(packages):
-    #     wm2argo(file_path, split, output_dir, output_dir_tfrecords_splitted)
+    # func = partial(
+    #     wm2argo,
+    #     split=split,
+    #     output_dir=output_dir,
+    #     output_dir_tfrecords_splitted=output_dir_tfrecords_splitted,
+    # )
+    #
+    # with multiprocessing.Pool(num_workers) as p:
+    #     r = list(tqdm(p.imap_unordered(func, packages), total=len(packages)))
+    print(len(packages))
+    for file_path in tqdm(packages):
+        wm2argo(file_path, split, output_dir, output_dir_tfrecords_splitted)
 
 
 if __name__ == "__main__":
