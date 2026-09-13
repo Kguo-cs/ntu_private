405ee6e61417f258cd46233c2b904ed3a3b342c0
Kguo-cs <u3006612@connect.hku.hk>
2026-09-13 23:24:24 +0800

    1

--- src/smart/model/smart_gail.py
+++ src/smart/model/smart_gail.py
@@ -931,7 +931,7 @@ class SMART_GAIL(SMART):
 
             rl_loss = (
                     pg_loss
-                    + 0.02 * residual_loss
+                    + 0.1 * residual_loss
                     + 0.1 * std_loss
                     #+ 0.1* collision_loss
             )
