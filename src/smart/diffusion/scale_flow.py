1d7d4eae7260a98093828596cb2c9d20a5491686
Kguo-cs <u3006612@connect.hku.hk>
2026-09-15 23:16:51 +0800

    1

--- src/smart/diffusion/scale_flow.py
+++ src/smart/diffusion/scale_flow.py
@@ -292,7 +292,7 @@ class ScaleFlow(nn.Module):
         # delta_mu=prediction[:,:base.shape[-1]]
 
         #log_std=self.refiner_log_std
-        log_std = prediction[:, base.shape[-1]:].clamp(  math.log(0.05),  math.log(0.2)  )
+        log_std = prediction[:, base.shape[-1]:].clamp(  math.log(0.01),  math.log(1)  )
         # min_log_std = math.log(0.03)
         # max_log_std = math.log(0.3)
         #
