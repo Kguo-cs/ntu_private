5c12962749f24fdcc2ece7368bd53da6a0564bdf
Kguo-cs <u3006612@connect.hku.hk>
2026-09-16 19:30:29 +0800

    1

--- src/smart/diffusion/scale_flow.py
+++ src/smart/diffusion/scale_flow.py
@@ -1035,7 +1035,7 @@ class ScaleFlow(nn.Module):
                 # --------------------------------------
                 # normalized residual -> raw residual
                 # --------------------------------------
-                res =  delta *refiner_scale[None]#base + refiner_scale[None].to(delta.device)#
+                res =  delta *refiner_scale[None].to(delta.device)#
 
                 latent=base +res
 
