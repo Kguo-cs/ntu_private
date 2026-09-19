dce0129d84145f226d94ad979bdc109bd27a5d84
Kguo-cs <u3006612@connect.hku.hk>
2026-09-19 22:57:19 +0800

    1

--- src/smart/modules/smart_decoder.py
+++ src/smart/modules/smart_decoder.py
@@ -236,7 +236,7 @@ class SMARTDecoder(nn.Module):
             num_historical_steps=num_historical_steps,
             num_future_steps=num_future_steps,
             time_span=10,
-            pl2a_radius=pl2a_radius,
+            pl2a_radius=10,
             a2a_radius=dis_a2a_radius,
             num_freq_bands=num_freq_bands,
             num_layers=1,
