734e274205ffc931ccd0709c406e268db220da96
Kguo-cs <u3006612@connect.hku.hk>
2026-09-22 15:19:27 +0800

    1

--- src/smart/diffusion/scale_flow.py
+++ src/smart/diffusion/scale_flow.py
@@ -108,7 +108,7 @@ class ScaleFlow(nn.Module):
             #     # requires_grad=False
             # )
             # refiner mean 最大修正量，normalized space
-            self.refiner_delta_scale = 0.2
+            self.refiner_delta_scale = 0.1
 
         self.apply(weight_init)
 
