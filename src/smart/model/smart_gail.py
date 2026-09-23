2f41309e91ed363c8cbd50fc913bc19e7ac66b1a
Kguo-cs <u3006612@connect.hku.hk>
2026-09-23 15:19:53 +0800

    cone

--- src/smart/model/smart_gail.py
+++ src/smart/model/smart_gail.py
@@ -569,8 +569,8 @@ class SMART_GAIL(SMART):
             actor_optimizer=discriminator_optimizer=init_optimizer=None
 
         # if self.token_processor.learn_init :#== False
-        if self.token_processor.use_noise:
-            self._optimizer_step(discriminator_optimizer, critic_loss)
+       # if self.token_processor.use_noise:
+        self._optimizer_step(discriminator_optimizer, critic_loss)
 
         #if self.token_processor.learn_init:
 
