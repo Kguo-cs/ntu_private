e78cccc917280f80005bb8abf5f77e6febddc0e8
Kguo-cs <u3006612@connect.hku.hk>
2026-09-24 14:34:37 +0800

    cone

--- src/smart/model/smart_gail.py
+++ src/smart/model/smart_gail.py
@@ -673,8 +673,8 @@ class SMART_GAIL(SMART):
         advantages_2d, value_loss_elements = compute_advantages(
             rewards_pad,#[-len(value) :]
             value,
-            gamma=0.99,
-            lam=0.99
+            gamma=0.5,
+            lam=0.5
         )
 
         if "train_mask" in rollout_agent and rollout_agent["train_mask"] is not None:
